82d241150cd0b09ad2fa5f7569590534d3aae1ec
Erich Loftis <erichlof@swbell.net>
2017-12-11 01:43:58 -0600

    Update FirstPersonCameraControls.js

--- js/FirstPersonCameraControls.js
+++ js/FirstPersonCameraControls.js
@@ -1,7 +1,11 @@
 /**
+ * originally from https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/PointerLockControls.js
  * @author mrdoob / http://mrdoob.com/
- * adapted for virtualButtonJoystick.js by erichlof
+ *
+ * edited by Erich Loftis (erichlof on GitHub)
  * https://github.com/erichlof
+ * Btw, this is the most consice and elegant way to implement first person camera rotation/movement that I've ever seen -
+ * look at how short it is, without spaces/braces it would be around 30 lines!  Way to go, mrdoob!
  */
 
 var FirstPersonCameraControls = function ( camera ) {
@@ -14,7 +18,7 @@ var FirstPersonCameraControls = function ( camera ) {
 	this.yawObject = new THREE.Object3D();
 	this.yawObject.add( this.pitchObject );
 	
-	var that = this;
+	var scope = this;
 
 	var movementX = 0;
 	var movementY = 0;
@@ -24,10 +28,10 @@ var FirstPersonCameraControls = function ( camera ) {
 		movementX = event.movementX || event.mozMovementX || 0;
 		movementY = event.movementY || event.mozMovementY || 0;
 
-		that.yawObject.rotation.y -= movementX * 0.002;
-		that.pitchObject.rotation.x -= movementY * 0.002;
+		scope.yawObject.rotation.y -= movementX * 0.002;
+		scope.pitchObject.rotation.x -= movementY * 0.002;
 
-		that.pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, that.pitchObject.rotation.x ) );
+		scope.pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, scope.pitchObject.rotation.x ) );
 			
 	};
 
@@ -36,19 +40,19 @@ var FirstPersonCameraControls = function ( camera ) {
 
 	this.getObject = function () {
 
-		return that.yawObject;
+		return scope.yawObject;
 
 	};
 	
 	this.getYawObject = function () {
 
-		return that.yawObject;
+		return scope.yawObject;
 
 	};
 	
 	this.getPitchObject = function () {
 
-		return that.pitchObject;
+		return scope.pitchObject;
 
 	};
 /*	
@@ -61,7 +65,7 @@ var FirstPersonCameraControls = function ( camera ) {
 
 		return function( v ) {
 
-			rotation.set( that.pitchObject.rotation.x, that.yawObject.rotation.y, 0 );
+			rotation.set( scope.pitchObject.rotation.x, scope.yawObject.rotation.y, 0 );
 
 			v.copy( direction ).applyEuler( rotation );
 
@@ -73,7 +77,7 @@ var FirstPersonCameraControls = function ( camera ) {
 */	
 	this.getDirection = function() {
 
-		var te = that.pitchObject.matrixWorld.elements;
+		var te = camera.matrixWorld.elements;
 
 		return function( v ) {
 			
@@ -87,7 +91,7 @@ var FirstPersonCameraControls = function ( camera ) {
 	
 	this.getUpVector = function() {
 
-		var te = that.pitchObject.matrixWorld.elements;
+		var te = camera.matrixWorld.elements;
 
 		return function( v ) {
 			
@@ -101,7 +105,7 @@ var FirstPersonCameraControls = function ( camera ) {
 	
 	this.getRightVector = function() {
 
-		var te = that.pitchObject.matrixWorld.elements;
+		var te = camera.matrixWorld.elements;
 
 		return function( v ) {
 			
