137c072e34dabaf2105e83f1ee7a6da6051fb3bc
Erich Loftis <erichlof@swbell.net>
2019-02-01 23:26:18 -0600

    Add files via upload

---
+++ js/Compare_Uni-Directional_Approach.js
@@ -0,0 +1,190 @@
+// scene/demo-specific variables go here
+var sceneIsDynamic = false;
+var camFlightSpeed = 300;
+var tallBoxGeometry, tallBoxMaterial, tallBoxMesh;
+var shortBoxGeometry, shortBoxMaterial, shortBoxMesh;
+
+// called automatically from within initTHREEjs() function
+function initSceneData() {
+        
+        // scene/demo-specific three.js objects setup goes here
+        
+        // Boxes
+        tallBoxGeometry = new THREE.BoxGeometry(1,1,1);
+        tallBoxMaterial = new THREE.MeshPhysicalMaterial( {
+                color: new THREE.Color(0.95, 0.95, 0.95), //RGB, ranging from 0.0 - 1.0
+                roughness: 1.0 // ideal Diffuse material	
+        } );
+        
+        tallBoxMesh = new THREE.Mesh(tallBoxGeometry, tallBoxMaterial);
+        pathTracingScene.add(tallBoxMesh);
+        tallBoxMesh.visible = false; // disable normal Three.js rendering updates of this object: 
+        // it is just a data placeholder as well as an Object3D that can be transformed/manipulated by 
+        // using familiar Three.js library commands. It is then fed into the GPU path tracing renderer
+        // through its 'matrixWorld' matrix. See below:
+        tallBoxMesh.rotation.set(0, Math.PI * 0.1, 0);
+        tallBoxMesh.position.set(180, 170, -350);
+        tallBoxMesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
+        
+        
+        shortBoxGeometry = new THREE.BoxGeometry(1,1,1);
+        shortBoxMaterial = new THREE.MeshPhysicalMaterial( {
+                color: new THREE.Color(0.95, 0.95, 0.95), //RGB, ranging from 0.0 - 1.0
+                roughness: 1.0 // ideal Diffuse material	
+        } );
+        
+        shortBoxMesh = new THREE.Mesh(shortBoxGeometry, shortBoxMaterial);
+        pathTracingScene.add(shortBoxMesh);
+        shortBoxMesh.visible = false;
+        shortBoxMesh.rotation.set(0, -Math.PI * 0.09, 0);
+        shortBoxMesh.position.set(370, 85, -170);
+        shortBoxMesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
+        
+
+        // set camera's field of view
+        worldCamera.fov = 31;
+
+        // position and orient camera
+        cameraControlsObject.position.set(278, 270, 1050);
+        ///cameraControlsYawObject.rotation.y = 0.0;
+        // look slightly upward
+        cameraControlsPitchObject.rotation.x = 0.005;
+
+} // end function initSceneData()
+
+
+
+// called automatically from within initTHREEjs() function
+function initPathTracingShaders() {
+ 
+        // scene/demo-specific uniforms go here
+        pathTracingUniforms = {
+
+                tPreviousTexture: { type: "t", value: screenTextureRenderTarget.texture },
+		
+                uCameraIsMoving: { type: "b1", value: false },
+                uCameraJustStartedMoving: { type: "b1", value: false },
+
+                uTime: { type: "f", value: 0.0 },
+                uSampleCounter: { type: "f", value: 0.0 },
+                uFrameCounter: { type: "f", value: 1.0 },
+                uULen: { type: "f", value: 1.0 },
+                uVLen: { type: "f", value: 1.0 },
+                uApertureSize: { type: "f", value: 0.0 },
+                uFocusDistance: { type: "f", value: 1180.0 },
+                
+                uResolution: { type: "v2", value: new THREE.Vector2() },
+                
+                uRandomVector: { type: "v3", value: new THREE.Vector3() },
+        
+                uCameraMatrix: { type: "m4", value: new THREE.Matrix4() },
+                
+                uShortBoxInvMatrix: { type: "m4", value: new THREE.Matrix4() },
+                uShortBoxNormalMatrix: { type: "m3", value: new THREE.Matrix3() },
+                
+                uTallBoxInvMatrix: { type: "m4", value: new THREE.Matrix4() },
+                uTallBoxNormalMatrix: { type: "m3", value: new THREE.Matrix3() }
+        
+        };
+
+        pathTracingDefines = {
+        	//NUMBER_OF_TRIANGLES: total_number_of_triangles
+        };
+
+        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) {
+                pathTracingVertexShader = shaderText;
+
+                createPathTracingMaterial();
+        });
+
+} // end function initPathTracingShaders()
+
+
+// called automatically from within initPathTracingShaders() function above
+function createPathTracingMaterial() {
+
+        fileLoader.load('shaders/Compare_Uni-Directional_Approach_Fragment.glsl', function (shaderText) {
+                
+                pathTracingFragmentShader = shaderText;
+
+                pathTracingMaterial = new THREE.ShaderMaterial({
+                        uniforms: pathTracingUniforms,
+                        defines: pathTracingDefines,
+                        vertexShader: pathTracingVertexShader,
+                        fragmentShader: pathTracingFragmentShader,
+                        depthTest: false,
+                        depthWrite: false
+                });
+
+                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+                pathTracingScene.add(pathTracingMesh);
+
+                // the following keeps the large scene ShaderMaterial quad right in front 
+                //   of the camera at all times. This is necessary because without it, the scene 
+                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+                worldCamera.add(pathTracingMesh);
+                
+        });
+
+} // end function createPathTracingMaterial()
+
+
+
+// called automatically from within the animate() function
+function updateVariablesAndUniforms() {
+        
+        if (cameraIsMoving) {
+                sampleCounter = 1.0;
+                frameCounter += 1.0;
+
+                if (!cameraRecentlyMoving) {
+                        cameraJustStartedMoving = true;
+                        cameraRecentlyMoving = true;
+                }
+        }
+
+        if ( !cameraIsMoving ) {
+                sampleCounter += 1.0; // for progressive refinement of image
+                if (sceneIsDynamic)
+                        sampleCounter = 1.0; // reset for continuous updating of image
+                
+                frameCounter  += 1.0;
+                if (cameraRecentlyMoving)
+                        frameCounter = 1.0;
+
+                cameraRecentlyMoving = false;  
+        }
+
+        
+        pathTracingUniforms.uCameraIsMoving.value = cameraIsMoving;
+        pathTracingUniforms.uCameraJustStartedMoving.value = cameraJustStartedMoving;
+        pathTracingUniforms.uSampleCounter.value = sampleCounter;
+        pathTracingUniforms.uFrameCounter.value = frameCounter;
+        pathTracingUniforms.uRandomVector.value = randomVector.set(Math.random(), Math.random(), Math.random());
+        
+        // BOXES
+        pathTracingUniforms.uTallBoxInvMatrix.value.getInverse( tallBoxMesh.matrixWorld );
+        pathTracingUniforms.uTallBoxNormalMatrix.value.getNormalMatrix( tallBoxMesh.matrixWorld );
+        pathTracingUniforms.uShortBoxInvMatrix.value.getInverse( shortBoxMesh.matrixWorld );
+        pathTracingUniforms.uShortBoxNormalMatrix.value.getNormalMatrix( shortBoxMesh.matrixWorld );
+        
+        // CAMERA
+        cameraControlsObject.updateMatrixWorld(true);
+        pathTracingUniforms.uCameraMatrix.value.copy(worldCamera.matrixWorld);
+        screenOutputMaterial.uniforms.uOneOverSampleCounter.value = 1.0 / sampleCounter;
+
+        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+
+} // end function updateVariablesAndUniforms()
+
+
+
+initWindowAndControls(); // boilerplate: init handlers for window, mouse / mobile controls
+
+initTHREEjs(); // boilerplate: init necessary three.js items and scene/demo-specific objects
+
+onWindowResize(); // this 'jumpstarts' the initial dimensions and parameters for the window and renderer
+
+// everything is set up, now we can start animating
+animate();
