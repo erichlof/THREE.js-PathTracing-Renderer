a603dab205d25d844f294fa49ca441c2322d7096
Erich Loftis <erichlof@swbell.net>
2019-02-27 00:32:36 -0600

    Update CSG_Museum_4_Fragment.glsl

--- shaders/CSG_Museum_4_Fragment.glsl
+++ shaders/CSG_Museum_4_Fragment.glsl
@@ -1029,7 +1029,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
                         {
                                 // choose random Diffuse sample vector
 				r = Ray( x, randomCosWeightedDirectionInHemisphere(nl, seed) );
-				r.origin += r.direction;
+				r.origin += nl;
 				
 				continue;
                         }
@@ -1054,7 +1054,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			//bounceIsSpecular = (bounces < 2); 
 
 			r = Ray( x, reflect(r.direction, nl) );
-			r.origin += r.direction;
+			r.origin += nl;
 
 			continue;
 		}
@@ -1071,7 +1071,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			if (rand(seed) < Re) // reflect ray from surface
 			{
 				r = Ray( x, reflect(r.direction, nl) );
-				r.origin += r.direction;
+				r.origin += nl;
 			    	continue;	
 			}
 			else // transmit ray through surface
@@ -1079,7 +1079,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				mask *= intersec.color;
 				
 				r = Ray(x, tdir);
-				r.origin += r.direction;
+				r.origin -= nl;
 				continue;
 			}
 			
@@ -1116,7 +1116,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				vec3 reflectVec = reflect(r.direction, nl);
 				vec3 randVec = vec3(rand(seed) * 2.0 - 1.0, rand(seed) * 2.0 - 1.0, rand(seed) * 2.0 - 1.0);
 				r = Ray( x, mix( reflectVec, normalize(nl + randVec), roughness) );
-				r.origin += r.direction;
+				r.origin += nl;
 				continue;	
 			}
 
@@ -1130,7 +1130,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
                         {
                                 // choose random Diffuse sample vector
 				r = Ray( x, randomCosWeightedDirectionInHemisphere(nl, seed) );
-				r.origin += r.direction;
+				r.origin += nl;
 				continue;
                         }
                         else
