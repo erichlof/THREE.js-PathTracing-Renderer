6a2a2f6c52d6400ed1f25a2c852e0ee916ce5dda
Erich Loftis <erichlof@swbell.net>
2019-02-27 00:46:50 -0600

    Update Quadric_Geometry_Showcase_Fragment.glsl

--- shaders/Quadric_Geometry_Showcase_Fragment.glsl
+++ shaders/Quadric_Geometry_Showcase_Fragment.glsl
@@ -445,7 +445,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
                         {
                                 // choose random Diffuse sample vector
 				r = Ray( x, randomCosWeightedDirectionInHemisphere(nl, seed) );
-				r.origin += r.direction;
+				r.origin += nl;
 				continue;
                         }
                         else
@@ -466,7 +466,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			mask *= intersec.color;
 
 			r = Ray( x, reflect(r.direction, nl) );
-			r.origin += r.direction;
+			r.origin += nl;
 
 			//bounceIsSpecular = true; // turn on mirror caustics
 			continue;
@@ -481,7 +481,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			if (rand(seed) < Re) // reflect ray from surface
 			{
 				r = Ray( x, reflect(r.direction, nl) );
-				r.origin += r.direction;
+				r.origin += nl;
 
 				//bounceIsSpecular = true; // turn on reflecting caustics, useful for water
 			    	continue;	
@@ -491,7 +491,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				mask *= intersec.color;
 				
 				r = Ray(x, tdir);
-				r.origin += r.direction;
+				r.origin -= nl;
 
 				bounceIsSpecular = true; // turn on refracting caustics
 				continue;
@@ -509,7 +509,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			if( rand(seed) < Re )
 			{	
 				r = Ray( x, reflect(r.direction, nl) );
-				r.origin += r.direction;
+				r.origin += nl;
 				continue;	
 			}
 
@@ -523,7 +523,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
                         {
                                 // choose random Diffuse sample vector
 				r = Ray( x, randomCosWeightedDirectionInHemisphere(nl, seed) );
-				r.origin += r.direction;
+				r.origin += nl;
 				continue;
                         }
                         else
