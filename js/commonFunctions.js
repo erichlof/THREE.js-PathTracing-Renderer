73d332893160f1e53182038de3473b1f103ac1a6
Erich Loftis <erichlof@swbell.net>
2019-03-06 00:31:54 -0600

    Update commonFunctions.js

--- js/commonFunctions.js
+++ js/commonFunctions.js
@@ -376,8 +376,8 @@ function initTHREEjs() {
         screenOutputMesh = new THREE.Mesh(screenOutputGeometry, screenOutputMaterial);
         screenOutputScene.add(screenOutputMesh);
 
-
-        onWindowResize(); // this 'jumpstarts' the initial dimensions and parameters for the window and renderer
+        // this 'jumpstarts' the initial dimensions and parameters for the window and renderer
+        onWindowResize();
 
         // everything is set up, now we can start animating
         animate();
@@ -601,25 +601,28 @@ function animate() {
                 decreaseAperture = false;
         }
 
-
+        // update scene/demo-specific variables and uniforms every animation frame
         updateVariablesAndUniforms();
 
 
         // RENDERING in 3 steps
 
         // STEP 1
-        // Perform PathTracing and Render(save) into pathTracingRenderTarget
-        // Read previous screenTextureRenderTarget (inside fragment shader code) to use as a new starting point to blend with
-        renderer.render(pathTracingScene, worldCamera, pathTracingRenderTarget);
+        // Perform PathTracing and Render(save) into pathTracingRenderTarget, a full-screen texture.
+        // Read previous screenTextureRenderTarget(via texelFetch inside fragment shader) to use as a new starting point to blend with
+        renderer.setRenderTarget(pathTracingRenderTarget);
+        renderer.render(pathTracingScene, worldCamera);
 
         // STEP 2
-        // Render(copy) the final pathTracingScene output(above) into screenTextureRenderTarget
-        // This will be used as a new starting point for Step 1 above
-        renderer.render(screenTextureScene, quadCamera, screenTextureRenderTarget);
+        // Render(copy) the pathTracingScene output(pathTracingRenderTarget above) into screenTextureRenderTarget.
+        // This will be used as a new starting point for Step 1 above (essentially creating ping-pong buffers)
+        renderer.setRenderTarget(screenTextureRenderTarget);
+        renderer.render(screenTextureScene, quadCamera);
 
         // STEP 3
         // Render full screen quad with generated pathTracingRenderTarget in STEP 1 above.
-        // After the image is gamma corrected, it will be shown on the screen as the final accumulated output
+        // After the image is gamma-corrected, it will be shown on the screen as the final accumulated output
+        renderer.setRenderTarget(null);
         renderer.render(screenOutputScene, quadCamera);
 
         stats.update();
