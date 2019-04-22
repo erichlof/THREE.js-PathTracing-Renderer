99f5b780f25884b481124b4935fb8d470c38a68e
Erich Loftis <erichlof@swbell.net>
2019-04-22 11:34:42 -0500

    Update BVH_Spot_Light_Source.js

--- js/BVH_Spot_Light_Source.js
+++ js/BVH_Spot_Light_Source.js
@@ -75,8 +75,6 @@ function load_GLTF_Model() {
                 for (let i = 0; i < meshList.length; i++) {
                         geoList.push(meshList[i].geometry);
                 }
-                
-                modelMesh.geometry = THREE.BufferGeometryUtils.mergeBufferGeometries(geoList);
 
                 if (modelMesh.geometry.index)
                         modelMesh.geometry = modelMesh.geometry.toNonIndexed();
