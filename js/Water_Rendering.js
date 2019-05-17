964aed19cf99336d577e980098772f9e6f9e5aea
Erich Loftis <erichlof@swbell.net>
2019-05-17 17:06:48 -0500

    Update Water_Rendering.js

--- js/Water_Rendering.js
+++ js/Water_Rendering.js
@@ -47,6 +47,7 @@ function initSceneData() {
 
         // set camera's field of view
         worldCamera.fov = 50;
+        focusDistance = 500.0;
 
         // position and orient camera
         cameraControlsObject.position.set(96, 397, 278);
@@ -86,7 +87,7 @@ function initPathTracingShaders() {
                 uULen: { type: "f", value: 1.0 },
                 uVLen: { type: "f", value: 1.0 },
                 uApertureSize: { type: "f", value: 0.0 },
-                uFocusDistance: { type: "f", value: 1180.0 },
+                uFocusDistance: { type: "f", value: focusDistance },
                 
                 uResolution: { type: "v2", value: new THREE.Vector2() },
                 
