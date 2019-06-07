a75d591cbb5f871001557e5258249e37319ce856
Erich Loftis <erichlof@swbell.net>
2019-06-07 00:08:24 -0500

    Update Transforming_Quadric_Geometry_Showcase_Fragment.glsl

--- shaders/Transforming_Quadric_Geometry_Showcase_Fragment.glsl
+++ shaders/Transforming_Quadric_Geometry_Showcase_Fragment.glsl
@@ -668,23 +668,20 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed, inout bool rayHitIsDynamic )
 			Re = calcFresnelReflectance(n, nl, r.direction, nc, nt, tdir);
 			Tr = 1.0 - Re;
 
-			if (!firstTypeWasREFR && diffuseCount == 0)
+			if (bounces == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
 				firstMask = mask * Re;
 				firstRay = Ray( x, reflect(r.direction, nl) ); // create reflection ray from surface
 				firstRay.origin += nl * uEPS_intersect;
+				mask *= Tr;
 			}
-
-			if (bounces > 0 && bounceIsSpecular)
+			else if (rand(seed) < Re)
 			{
-				if (rand(seed) < Re)
-				{
-					r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
-					r.origin += nl * uEPS_intersect;
-					continue;
-				}
+				r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
+				r.origin += nl * uEPS_intersect;
+				continue;
 			}
 
 			// transmit ray through surface
@@ -698,10 +695,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed, inout bool rayHitIsDynamic )
 				sampleLight = true; // turn on refracting caustics
 			}
 			else
-			{
-				mask *= Tr;
 				mask *= intersec.color;
-			}
 				
 			continue;
 			
@@ -722,21 +716,17 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed, inout bool rayHitIsDynamic )
 				firstMask = mask * Re;
 				firstRay = Ray( x, reflect(r.direction, nl) ); // create reflection ray from surface
 				firstRay.origin += nl * uEPS_intersect;
+				mask *= Tr;
 			}
-			
-			if (bounces > 0 && bounceIsSpecular)
+			else if (rand(seed) < Re)
 			{
-				if (rand(seed) < Re)
-				{	
-					r = Ray( x, reflect(r.direction, nl) );
-					r.origin += nl * uEPS_intersect;
-					continue;	
-				}
+				r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
+				r.origin += nl * uEPS_intersect;
+				continue;
 			}
 			
 			diffuseCount++;
 
-			mask *= Tr;
 			mask *= intersec.color;
 			
 			bounceIsSpecular = false;
