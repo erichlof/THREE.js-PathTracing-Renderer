1a0355178c0029a35e2c6e0b249fbd3074d9e28e
Erich Loftis <erichlof@swbell.net>
2019-06-06 23:56:04 -0500

    Update BVH_Visualizer_Fragment.glsl

--- shaders/BVH_Visualizer_Fragment.glsl
+++ shaders/BVH_Visualizer_Fragment.glsl
@@ -364,20 +364,16 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				firstMask = mask * Re;
 				firstRay = Ray( x, reflect(r.direction, nl) ); // create reflection ray from surface
 				firstRay.origin += nl * uEPS_intersect;
+				mask *= Tr;
 			}
-
-			if (bounces > 0)
+			else if (rand(seed) < Re)
 			{
-				if (rand(seed) < Re)
-				{
-					r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
-					r.origin += nl * uEPS_intersect;
-					continue;
-				}
+				r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
+				r.origin += nl * uEPS_intersect;
+				continue;
 			}
 
 			// transmit ray through surface
-			mask *= Tr;
 			mask *= intersec.color;
 			
 			r = Ray(x, tdir);
@@ -402,25 +398,21 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				firstMask = mask * Re;
 				firstRay = Ray( x, reflect(r.direction, nl) ); // create reflection ray from surface
 				firstRay.origin += nl * uEPS_intersect;
+				mask *= Tr;
 			}
-
-			if (bounces > 0)
+			else if (rand(seed) < Re)
 			{
-				if (rand(seed) < Re)
-				{	
-					r = Ray( x, reflect(r.direction, nl) );
-					r.origin += nl * uEPS_intersect;
-					continue;	
-				}
+				r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
+				r.origin += nl * uEPS_intersect;
+				continue;
 			}
-
-			mask *= Tr;
+			
 			mask *= intersec.color;
+
 			//accumCol += calcDirectLightingSphere(mask, x, nl, spheres[0], seed);
 			r = Ray( x, randomCosWeightedDirectionInHemisphere(nl, seed) );
 			r.origin += nl * uEPS_intersect;
 			continue;
-			
 			
 		} //end if (intersec.type == COAT)
 		
