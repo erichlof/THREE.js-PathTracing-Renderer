4637e373f1b4f0d0a6bb105d0f1f4a51b3fa0e0c
Erich Loftis <erichlof@swbell.net>
2019-08-28 23:03:57 -0500

    Update Switching_Materials.js

--- js/Switching_Materials.js
+++ js/Switching_Materials.js
@@ -152,7 +152,7 @@ function MaterialObject() {
 function initSceneData() {
         
         // scene/demo-specific three.js objects setup goes here
-        EPS_intersect = mouseControl ? 0.1 : 1.0; // less precision on mobile
+        EPS_intersect = mouseControl ? 0.01 : 1.0; // less precision on mobile
 
         // set camera's field of view
         worldCamera.fov = 50;
