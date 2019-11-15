54c506263170a4701b7d6a5884b1a6ae83fec508
Erich Loftis <erichlof@swbell.net>
2019-11-15 13:14:22 -0600

    Update Water_Rendering_Fragment.glsl

--- shaders/Water_Rendering_Fragment.glsl
+++ shaders/Water_Rendering_Fragment.glsl
@@ -431,7 +431,9 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 		
 	} // end for (int bounces = 0; bounces < 5; bounces++)
 	
-	return max(vec3(0), accumCol); // prevents black spot artifacts appearing in the water     
+	
+	return max(vec3(0), accumCol); // prevents black spot artifacts appearing in the water 
+	     
 }
 
 
