baeb82c4d91dc7202bb17e5c6df0bc808197460b
Erich Loftis <erichlof@swbell.net>
2019-12-08 23:00:52 -0600

    Update Arctic_Circle_Fragment.glsl

--- shaders/Arctic_Circle_Fragment.glsl
+++ shaders/Arctic_Circle_Fragment.glsl
@@ -438,13 +438,6 @@ vec3 CalculateRadiance( Ray r, vec3 sunDirection, inout uvec2 seed )
 			Re = calcFresnelReflectance(r.direction, n, nc, nt, ratioIoR);
 			Tr = 1.0 - Re;
 			
-			if (Re > 0.99)
-			{
-				r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
-				r.origin += nl * uEPS_intersect;
-				continue;
-			}
-			
 			if (bounces == 0)
 			{	
 				// save intersection data for future reflection trace
