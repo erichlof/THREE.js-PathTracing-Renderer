ad974ea09d18c0dc5cc94fbac642086088030c19
Erich Loftis <erichlof@swbell.net>
2019-12-13 14:41:22 -0600

    Update Compare_Uni-Directional_Approach.js

--- js/Compare_Uni-Directional_Approach.js
+++ js/Compare_Uni-Directional_Approach.js
@@ -1,4 +1,5 @@
 // scene/demo-specific variables go here
+var EPS_intersect;
 var sceneIsDynamic = false;
 var camFlightSpeed = 300;
 var tallBoxGeometry, tallBoxMaterial, tallBoxMesh;
@@ -8,6 +9,7 @@ var shortBoxGeometry, shortBoxMaterial, shortBoxMesh;
 function initSceneData() {
         
         // scene/demo-specific three.js objects setup goes here
+        EPS_intersect = mouseControl ? 0.1 : 1.0; // less precision on mobile
         
         // Boxes
         tallBoxGeometry = new THREE.BoxGeometry(1,1,1);
@@ -66,6 +68,7 @@ function initPathTracingShaders() {
                 uCameraIsMoving: { type: "b1", value: false },
                 uCameraJustStartedMoving: { type: "b1", value: false },
 
+                uEPS_intersect: { type: "f", value: EPS_intersect },
                 uTime: { type: "f", value: 0.0 },
                 uSampleCounter: { type: "f", value: 0.0 },
                 uFrameCounter: { type: "f", value: 1.0 },
