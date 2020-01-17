03141f2fff100b447b5183e7f0683de423baae9b
Erich Loftis <erichlof@swbell.net>
2020-01-17 12:16:29 -0600

    Update Cornell_Box_Fragment.glsl

--- shaders/Cornell_Box_Fragment.glsl
+++ shaders/Cornell_Box_Fragment.glsl
@@ -39,6 +39,7 @@ float SceneIntersect( Ray r, inout Intersection intersec )
 	vec3 normal;
         float d;
 	float t = INFINITY;
+	bool isRayExiting = false;
 		
 	for (int i = 0; i < N_QUADS; i++)
         {
@@ -59,7 +60,7 @@ float SceneIntersect( Ray r, inout Intersection intersec )
 	// transform ray into Tall Box's object space
 	rObj.origin = vec3( uTallBoxInvMatrix * vec4(r.origin, 1.0) );
 	rObj.direction = vec3( uTallBoxInvMatrix * vec4(r.direction, 0.0) );
-	d = BoxIntersect( boxes[0].minCorner, boxes[0].maxCorner, rObj, normal );
+	d = BoxIntersect( boxes[0].minCorner, boxes[0].maxCorner, rObj, normal, isRayExiting );
 	
 	if (d < t)
 	{	
@@ -79,7 +80,7 @@ float SceneIntersect( Ray r, inout Intersection intersec )
 	// transform ray into Short Box's object space
 	rObj.origin = vec3( uShortBoxInvMatrix * vec4(r.origin, 1.0) );
 	rObj.direction = vec3( uShortBoxInvMatrix * vec4(r.direction, 0.0) );
-	d = BoxIntersect( boxes[1].minCorner, boxes[1].maxCorner, rObj, normal );
+	d = BoxIntersect( boxes[1].minCorner, boxes[1].maxCorner, rObj, normal, isRayExiting );
 	
 	if (d < t)
 	{	
@@ -314,4 +315,117 @@ void SetupScene(void)
 }
 
 
-#include <pathtracing_main>
+//#include <pathtracing_main>
+// tentFilter from Peter Shirley's 'Realistic Ray Tracing (2nd Edition)' book, pg. 60		
+float tentFilter(float x)
+{
+	return (x < 0.5) ? sqrt(2.0 * x) - 1.0 : 1.0 - sqrt(2.0 - (2.0 * x));
+}
+
+
+/* // cubicSplineFilter from Peter Shirley's 'Realistic Ray Tracing (2nd Edition)' book, pg. 58
+float solve(float r)
+{
+	float u = r;
+	for (int i = 0; i < 5; i++)
+	{
+		u = (11.0 * r + u * u * (6.0 + u * (8.0 - 9.0 * u))) /
+			(4.0 + 12.0 * u * (1.0 + u * (1.0 - u)));
+	}
+	return u;
+}
+
+float cubicFilter(float x)
+{
+	if (x < 1.0 / 24.0)
+		return pow(24.0 * x, 0.25) - 2.0;
+	else if (x < 0.5)
+		return solve(24.0 * (x - 1.0 / 24.0) / 11.0) - 1.0;
+	else if (x < 23.0 / 24.0)
+		return 1.0 - solve(24.0 * (23.0 / 24.0 - x) / 11.0);
+	else return 2.0 - pow(24.0 * (1.0 - x), 0.25);
+} */
+
+
+void main( void )
+{
+	// not needed, three.js has a built-in uniform named cameraPosition
+	//vec3 camPos     = vec3( uCameraMatrix[3][0],  uCameraMatrix[3][1],  uCameraMatrix[3][2]);
+	
+	vec3 camRight   = vec3( uCameraMatrix[0][0],  uCameraMatrix[0][1],  uCameraMatrix[0][2]);
+	vec3 camUp      = vec3( uCameraMatrix[1][0],  uCameraMatrix[1][1],  uCameraMatrix[1][2]);
+	vec3 camForward = vec3(-uCameraMatrix[2][0], -uCameraMatrix[2][1], -uCameraMatrix[2][2]);
+	
+	// seed for rand(seed) function
+	uvec2 seed = uvec2(uFrameCounter, uFrameCounter + 1.0) * uvec2(gl_FragCoord);
+
+	vec2 pixelPos = vec2(0);
+	vec2 pixelOffset = vec2(0);
+	
+	float x = rand(seed);
+	float y = rand(seed);
+
+	if (!uCameraIsMoving)
+	{
+		pixelOffset = vec2(tentFilter(x), tentFilter(y));
+		//pixelOffset = vec2(cubicFilter(x), cubicFilter(y));
+	}
+	
+	// pixelOffset ranges from -1.0 to +1.0, so only need to divide by half resolution
+	pixelOffset /= (uResolution * 0.5);
+
+	// we must map pixelPos into the range -1.0 to +1.0
+	pixelPos = (gl_FragCoord.xy / uResolution) * 2.0 - 1.0;
+	pixelPos += pixelOffset;
+
+	vec3 rayDir = normalize( pixelPos.x * camRight * uULen + pixelPos.y * camUp * uVLen + camForward );
+	
+	// depth of field
+	vec3 focalPoint = uFocusDistance * rayDir;
+	float randomAngle = rand(seed) * TWO_PI; // pick random point on aperture
+	float randomRadius = rand(seed) * uApertureSize;
+	vec3  randomAperturePos = ( cos(randomAngle) * camRight + sin(randomAngle) * camUp ) * sqrt(randomRadius);
+	// point on aperture to focal point
+	vec3 finalRayDir = normalize(focalPoint - randomAperturePos);
+	
+	Ray ray = Ray( cameraPosition + randomAperturePos , finalRayDir );
+
+	SetupScene();
+				
+	// perform path tracing and get resulting pixel color
+	vec3 pixelColor = CalculateRadiance( ray, seed );
+	
+	vec3 previousColor = texelFetch(tPreviousTexture, ivec2(gl_FragCoord.xy), 0).rgb;
+
+	
+	if (uFrameCounter == 1.0)
+	{
+		previousColor = vec3(0);
+	}
+	else if (uCameraIsMoving)
+	{
+		// vec3 interpColorX = vec3(0);
+		// vec3 interpColorY = vec3(0);
+		// vec3 dFdxColor = dFdx(pixelColor);
+		// interpColorX.r = pixelColor.r + dFdxColor.r;
+		// interpColorX.g = pixelColor.g + dFdxColor.g;
+		// interpColorX.b = pixelColor.b + dFdxColor.b;
+		
+		// vec3 dFdyColor = dFdy(pixelColor);
+		// interpColorY.r = pixelColor.r + dFdyColor.r;
+		// interpColorY.g = pixelColor.g + dFdyColor.g;
+		// interpColorY.b = pixelColor.b + dFdyColor.b;
+		// pixelColor = max(vec3(0), (pixelColor + interpColorX + pixelColor + interpColorY) * 0.25);
+
+		pixelColor += fwidth(pixelColor) * 0.25;
+		pixelColor += fwidth(previousColor) * 0.25;
+		
+
+		pixelColor *= 0.5;
+		previousColor *= 0.5;
+	}
+	
+	
+
+	out_FragColor = vec4( previousColor + pixelColor, 1.0 );
+}
