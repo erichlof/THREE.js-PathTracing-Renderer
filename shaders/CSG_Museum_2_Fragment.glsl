3e5abd4d8ab42d712c3a9689fa309a478ab31ca9
Erich Loftis <erichlof@swbell.net>
2020-02-21 14:37:51 -0600

    Update CSG_Museum_2_Fragment.glsl

--- shaders/CSG_Museum_2_Fragment.glsl
+++ shaders/CSG_Museum_2_Fragment.glsl
@@ -1163,7 +1163,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			Re = calcFresnelReflectance(r.direction, n, nc, nt, ratioIoR);
 			Tr = 1.0 - Re;
 			
-			if (bounces == 0)
+			if (!firstTypeWasREFR && diffuseCount == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
@@ -1222,7 +1222,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			vec3 glossyVec = normalize(randomDirectionInHemisphere(nl, seed));
 			
 			// clearCoat counts as refractive surface
-			if (bounces == 0)
+			if (!firstTypeWasREFR && diffuseCount == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
