4a5532b5e80bf75e85d092a8fe261890d6a650a2
Erich Loftis <erichlof@swbell.net>
2020-02-21 14:54:52 -0600

    Update Ray_Warping_Fragment.glsl

--- shaders/Ray_Warping_Fragment.glsl
+++ shaders/Ray_Warping_Fragment.glsl
@@ -491,7 +491,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			Re = calcFresnelReflectance(r.direction, n, nc, nt, ratioIoR);
 			Tr = 1.0 - Re;
 			
-			if (bounces == 0)
+			if (!firstTypeWasREFR && diffuseCount == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
@@ -535,7 +535,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			Tr = 1.0 - Re;
 
 			// clearCoat counts as refractive surface
-			if (bounces == 0)
+			if (!firstTypeWasREFR && diffuseCount == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
