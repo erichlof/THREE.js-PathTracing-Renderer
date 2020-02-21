eb95d7705d00da0e7d8103358c72d71b433c6253
Erich Loftis <erichlof@swbell.net>
2020-02-21 14:26:31 -0600

    Update BVH_Point_Light_Source_Fragment.glsl

--- shaders/BVH_Point_Light_Source_Fragment.glsl
+++ shaders/BVH_Point_Light_Source_Fragment.glsl
@@ -539,7 +539,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			Re = calcFresnelReflectance(r.direction, n, nc, nt, ratioIoR);
 			Tr = 1.0 - Re;
 			
-			if (bounces == 0)
+			if (!firstTypeWasREFR && diffuseCount == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
@@ -583,7 +583,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			Tr = 1.0 - Re;
 			
 			// clearCoat counts as refractive surface
-			if (bounces == 0)
+			if (!firstTypeWasREFR && diffuseCount == 0)
 			{	
 				// save intersection data for future reflection trace
 				firstTypeWasREFR = true;
