1f12bb83982f26e428680b62791cf2dae9aab5bc
Erich Loftis <erichlof@swbell.net>
2020-03-31 02:30:10 -0500

    Update CSG_Museum_3_Fragment.glsl

--- shaders/CSG_Museum_3_Fragment.glsl
+++ shaders/CSG_Museum_3_Fragment.glsl
@@ -1124,7 +1124,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				r.origin += nl * uEPS_intersect;
 				continue;
 			}
-			else if (firstTypeWasREFR && rand(seed) < 0.5)
+			else if ((firstTypeWasREFR || reflectionTime) && rand(seed) < 0.5)
 			{
 				r = Ray( x, normalize(randomCosWeightedDirectionInHemisphere(nl, seed)) );
 				r.origin += nl * uEPS_intersect;
@@ -1169,7 +1169,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				firstRay.origin += nl * uEPS_intersect;
 				mask *= Tr;
 			}
-			else if (firstTypeWasREFR && n == nl && rand(seed) < Re)
+			else if (bounceIsSpecular && n == nl && rand(seed) < Re)
 			{
 				r = Ray( x, reflect(r.direction, nl) ); // reflect ray from surface
 				r.origin += nl * uEPS_intersect;
@@ -1183,7 +1183,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 			r = Ray(x, normalize(tdir));
 			r.origin -= nl * uEPS_intersect;
 
-			if (diffuseCount == 1)
+			if (bounces == 1)
 				bounceIsSpecular = true; // turn on refracting caustics
 
 			continue;
@@ -1228,7 +1228,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				firstRay.origin += nl * uEPS_intersect;
 				mask *= Tr;
 			}
-			else if (firstTypeWasREFR && rand(seed) < Re)
+			else if (bounceIsSpecular && rand(seed) < Re)
 			{
 				mask *= maskFactor;
 				r = Ray( x, mix(reflectVec, glossyVec, roughness));
@@ -1256,7 +1256,7 @@ vec3 CalculateRadiance( Ray r, inout uvec2 seed )
 				r.origin += nl * uEPS_intersect;
 				continue;
                         }
-			else if (firstTypeWasREFR && rand(seed) < 0.5)
+			else if ((firstTypeWasREFR || reflectionTime) && rand(seed) < 0.5)
 			{
 				// choose random Diffuse sample vector
 				r = Ray( x, normalize(randomCosWeightedDirectionInHemisphere(nl, seed)) );
