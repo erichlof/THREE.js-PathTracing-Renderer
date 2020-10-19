36e8d6144cfd52a5e60514708611bd661ee35ea3
Erich Loftis <erichlof@swbell.net>
2020-10-19 00:05:48 -0500

    Update HDRI_Environment.js

--- js/HDRI_Environment.js
+++ js/HDRI_Environment.js
@@ -89,9 +89,7 @@ function init_GUI() {
                 ableToEngagePointerLock = true;
 
                 mobileJoystickControls = new MobileJoystickControls ({
-                        //showJoystick: true,
-                        //stationaryBase: true,
-                        guiDomElement: gui.domElement
+                        //showJoystick: true
                 });	
         }
 
