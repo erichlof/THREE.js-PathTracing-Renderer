0b539aa73eacab7c2dd6324aed93a3da06ba8073
Erich Loftis <erichlof@swbell.net>
2020-10-22 13:54:12 -0500

    Update BVH_Animated_Model.js

--- js/BVH_Animated_Model.js
+++ js/BVH_Animated_Model.js
@@ -348,7 +348,7 @@ function initPathTracingShaders() {
         // scene/demo-specific uniforms go here
         pathTracingUniforms = {
 
-                tPreviousTexture: { type: "t", value: screenTextureRenderTarget.texture },
+                tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },
                 tTriangleTexture: { type: "t", value: triangleDataTexture },
                 tAABBTexture: { type: "t", value: aabbDataTexture },
                 tAlbedoMap: { type: "t", value: albedoMap },
@@ -369,7 +369,6 @@ function initPathTracingShaders() {
 
                 uResolution: { type: "v2", value: new THREE.Vector2() },
 
-                uRandomVector: { type: "v3", value: new THREE.Vector3() },
                 uGLTF_Model_Position: { type: "v3", value: new THREE.Vector3() },
 
                 uCameraMatrix: {type: "m4", value: new THREE.Matrix4() },
@@ -426,49 +425,18 @@ function createPathTracingMaterial() {
 // called automatically from within the animate() function
 function updateVariablesAndUniforms() {
 
-        if ( !cameraIsMoving ) {
-                
-                if (sceneIsDynamic)
-                        sampleCounter = 1.0; // reset for continuous updating of image
-                else sampleCounter += 1.0; // for progressive refinement of image
-                
-                frameCounter += 1.0;
-
-                cameraRecentlyMoving = false;  
-        }
-
-        if (cameraIsMoving) {
-                sampleCounter = 1.0;
-                frameCounter += 1.0;
-
-                if (!cameraRecentlyMoving) {
-                        frameCounter = 1.0;
-                        cameraRecentlyMoving = true;
-                }
-        }
-
-        
-        pathTracingUniforms.uCameraIsMoving.value = cameraIsMoving;
-        pathTracingUniforms.uSampleCounter.value = sampleCounter;
-        pathTracingUniforms.uFrameCounter.value = frameCounter;
-        pathTracingUniforms.uRandomVector.value = randomVector.set(Math.random(), Math.random(), Math.random());
-        
         // GLTF Model
         animationTimer += 1.0 * frameTime;
         GLTF_Model_Mesh.rotateOnWorldAxis(animationAxis, 0.1 * frameTime);
         GLTF_Model_Mesh.position.set(100 * Math.cos(animationTimer * 0.05), 20 * (Math.sin(animationTimer * 0.2) + 1.5), 120 * Math.sin(animationTimer * 0.05) - 50 );
-        //GLTF_Model_Mesh.position.set(0,40,80);
+        ///GLTF_Model_Mesh.position.set(0,40,80);
 
         GLTF_Model_Mesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
         pathTracingUniforms.uGLTF_Model_Position.value.copy(GLTF_Model_Mesh.position);
         pathTracingUniforms.uGLTF_Model_InvMatrix.value.getInverse( GLTF_Model_Mesh.matrixWorld );
         pathTracingUniforms.uGLTF_Model_NormalMatrix.value.getNormalMatrix( GLTF_Model_Mesh.matrixWorld );
         
-        // CAMERA
-        cameraControlsObject.updateMatrixWorld(true);
-        pathTracingUniforms.uCameraMatrix.value.copy(worldCamera.matrixWorld);
-        screenOutputMaterial.uniforms.uOneOverSampleCounter.value = 1.0 / sampleCounter;
-
+        // INFO
         cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) +
                 " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
