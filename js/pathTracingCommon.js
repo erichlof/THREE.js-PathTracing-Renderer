08fdd5343736e2805238d211edc265a6a2ee4601
Erich Loftis <erichlof@swbell.net>
2020-10-22 13:49:56 -0500

    Update pathTracingCommon.js

--- js/pathTracingCommon.js
+++ js/pathTracingCommon.js
@@ -1,88 +1,6 @@
-var screenTextureShader = {
-
-        uniforms: THREE.UniformsUtils.merge( [
-		
-                {
-                        tPathTracedImageTexture: { type: "t", value: null }
-                }
-		
-        ] ),
-
-        vertexShader: [
-                'precision highp float;',
-		'precision highp int;',
-
-		'void main()',
-		'{',
-			'gl_Position = vec4( position, 1.0 );',
-		'}'
-		
-        ].join( '\n' ),
-
-        fragmentShader: [
-                'precision highp float;',
-		'precision highp int;',
-		'precision highp sampler2D;',
-
-                'uniform sampler2D tPathTracedImageTexture;',
-		
-		'void main()',
-		'{',	
-			'pc_fragColor = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy), 0);',	
-		'}'
-		
-        ].join( '\n' )
-
-};
-
-var screenOutputShader = {
-
-        uniforms: THREE.UniformsUtils.merge( [
-		
-                {
-                        uOneOverSampleCounter: { type: "f", value: 0.0 },
-			tPathTracedImageTexture: { type: "t", value: null }
-                }
-		
-        ] ),
-
-        vertexShader: [
-                'precision highp float;',
-		'precision highp int;',
-
-		'void main()',
-		'{',
-			'gl_Position = vec4( position, 1.0 );',
-		'}'
-
-        ].join( '\n' ),
-
-        fragmentShader: [
-                'precision highp float;',
-		'precision highp int;',
-		'precision highp sampler2D;',
-
-                'uniform float uOneOverSampleCounter;',
-		'uniform sampler2D tPathTracedImageTexture;',
-		
-		'void main()',
-		'{',
-			'vec3 pixelColor = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy), 0).rgb * uOneOverSampleCounter;',
-			'pixelColor = ReinhardToneMapping(pixelColor);',
-			'//pixelColor = Uncharted2ToneMapping(pixelColor);',
-			'//pixelColor = OptimizedCineonToneMapping(pixelColor);',
-			'//pixelColor = ACESFilmicToneMapping(pixelColor);',
-			'pc_fragColor = clamp(vec4( pow(pixelColor, vec3(0.4545)), 1.0 ), 0.0, 1.0);',	
-		'}'
-		
-        ].join( '\n' )
-
-};
-
-
 THREE.ShaderChunk[ 'pathtracing_uniforms_and_defines' ] = `
+
 uniform bool uCameraIsMoving;
-//uniform bool uCameraJustStartedMoving;
 uniform float uEPS_intersect;
 uniform float uTime;
 uniform float uSampleCounter;
@@ -132,10 +50,11 @@ in vec2 vUv;
 `;
 
 THREE.ShaderChunk[ 'pathtracing_skymodel_defines' ] = `
-#define TURBIDITY 0.2//0.3
-#define RAYLEIGH_COEFFICIENT 2.5//2.0
-#define MIE_COEFFICIENT 0.05//0.05
-#define MIE_DIRECTIONAL_G 0.76//0.76
+
+#define TURBIDITY 1.5
+#define RAYLEIGH_COEFFICIENT 2.0
+#define MIE_COEFFICIENT 0.02
+#define MIE_DIRECTIONAL_G 0.76
 // constants for atmospheric scattering
 #define THREE_OVER_SIXTEENPI 0.05968310365946075
 #define ONE_OVER_FOURPI 0.07957747154594767
@@ -151,9 +70,11 @@ THREE.ShaderChunk[ 'pathtracing_skymodel_defines' ] = `
 #define RAYLEIGH_ZENITH_LENGTH 8400.0
 #define MIE_ZENITH_LENGTH 1250.0
 #define UP_VECTOR vec3(0.0, 1.0, 0.0)
-#define SUN_POWER 50.0
-#define SUN_ANGULAR_DIAMETER_COS 0.99983194915 // 66 arc seconds -> degrees, and the cosine of that
-#define CUTOFF_ANGLE 1.6 
+#define SUN_POWER 1000.0
+// 66 arc seconds -> degrees, and the cosine of that
+#define SUN_ANGULAR_DIAMETER_COS 0.99985 //0.9999566769
+#define CUTOFF_ANGLE 1.7//1.6110731556870734
+#define STEEPNESS 1.0//1.5
 `;
 
 
@@ -1214,13 +1135,15 @@ float BVH_DoubleSidedTriangleIntersect( vec3 v0, vec3 v1, vec3 v2, Ray r, out fl
 	vec3 tvec = r.origin - v0;
 	u = dot(tvec, pvec) * det;
 	vec3 qvec = cross(tvec, edge1);
-	v = dot(r.direction, qvec) * det;
+	v = dot(r.direction, qvec) * det; 
 	float t = dot(edge2, qvec) * det;
 	return (u < 0.0 || u > 1.0 || v < 0.0 || u + v > 1.0 || t <= 0.0) ? INFINITY : t;
 }
 `;
 
 THREE.ShaderChunk[ 'pathtracing_physical_sky_functions' ] = `
+
+
 float RayleighPhase(float cosTheta)
 {
 	return THREE_OVER_SIXTEENPI * (1.0 + (cosTheta * cosTheta));
@@ -1238,18 +1161,18 @@ vec3 totalMie()
 }
 float SunIntensity(float zenithAngleCos)
 {
-	return SUN_POWER * max( 0.0, 1.0 - exp( -( CUTOFF_ANGLE - acos(zenithAngleCos) ) ) );
+	zenithAngleCos = clamp( zenithAngleCos, -1.0, 1.0 );
+	return SUN_POWER * max( 0.0, 1.0 - pow( E, -( ( CUTOFF_ANGLE - acos( zenithAngleCos ) ) / STEEPNESS ) ) );
 }
 vec3 Get_Sky_Color(Ray r, vec3 sunDirection)
 {
-	
-    	vec3 viewDir = normalize(r.direction);
+	vec3 viewDirection = normalize(r.direction);
 	
 	/* most of the following code is borrowed from the three.js shader file: SkyShader.js */
     	// Cosine angles
-	float cosViewSunAngle = dot(viewDir, sunDirection);
-    	float cosSunUpAngle = dot(sunDirection, UP_VECTOR); // allowed to be negative: + is daytime, - is nighttime
-    	float cosUpViewAngle = max(0.0001, dot(UP_VECTOR, viewDir)); // cannot be 0, used as divisor
+	float cosViewSunAngle = dot(viewDirection, normalize(sunDirection));
+    	float cosSunUpAngle = dot(UP_VECTOR, normalize(sunDirection)); // allowed to be negative: + is daytime, - is nighttime
+    	float cosUpViewAngle = dot(UP_VECTOR, viewDirection);
 	
         // Get sun intensity based on how high in the sky it is
     	float sunE = SunIntensity(cosSunUpAngle);
@@ -1262,30 +1185,50 @@ vec3 Get_Sky_Color(Ray r, vec3 sunDirection)
 	vec3 mieAtX = totalMie() * MIE_COEFFICIENT;  
     
 	// optical length
-	float zenithAngle = 1.0 / cosUpViewAngle;
-    
-	float rayleighOpticalLength = RAYLEIGH_ZENITH_LENGTH * zenithAngle;
-	float mieOpticalLength = MIE_ZENITH_LENGTH * zenithAngle;
+	float zenithAngle = acos( max( 0.0, dot( UP_VECTOR, viewDirection ) ) );
+	float inverse = 1.0 / ( cos( zenithAngle ) + 0.15 * pow( 93.885 - ( ( zenithAngle * 180.0 ) / PI ), -1.253 ) );
+
+	float rayleighOpticalLength = RAYLEIGH_ZENITH_LENGTH * inverse;
+	float mieOpticalLength = MIE_ZENITH_LENGTH * inverse;
 	// combined extinction factor	
 	vec3 Fex = exp(-(rayleighAtX * rayleighOpticalLength + mieAtX * mieOpticalLength));
 	// in scattering
-	vec3 rayleighXtoEye = rayleighAtX * RayleighPhase(cosViewSunAngle);
-	vec3 mieXtoEye = mieAtX * hgPhase(cosViewSunAngle, MIE_DIRECTIONAL_G);
-     
-    	vec3 totalLightAtX = rayleighAtX + mieAtX;
-    	vec3 lightFromXtoEye = rayleighXtoEye + mieXtoEye; 
-    
-    	vec3 somethingElse = sunE * (lightFromXtoEye / totalLightAtX);
-    
-    	vec3 sky = somethingElse * (1.0 - Fex);
-	float oneMinusCosSun = 1.0 - cosSunUpAngle;
-    	sky *= mix( vec3(1.0), pow(somethingElse * Fex,vec3(0.5)), 
-	    clamp(oneMinusCosSun * oneMinusCosSun * oneMinusCosSun * oneMinusCosSun * oneMinusCosSun, 0.0, 1.0) );
-	// composition + solar disk
-    	float sundisk = smoothstep(SUN_ANGULAR_DIAMETER_COS - 0.0001, SUN_ANGULAR_DIAMETER_COS, cosViewSunAngle);
-	vec3 sun = (sunE * SUN_POWER * Fex) * sundisk;
+	vec3 betaRTheta = rayleighAtX * RayleighPhase(cosViewSunAngle * 0.5 + 0.5);
+	vec3 betaMTheta = mieAtX * hgPhase(cosViewSunAngle, MIE_DIRECTIONAL_G);
 	
-	return sky + sun;
+	vec3 Lin = pow( sunE * ( ( betaRTheta + betaMTheta ) / ( rayleighAtX + mieAtX ) ) * ( 1.0 - Fex ), vec3( 1.5 ) );
+	Lin *= mix( vec3( 1.0 ), pow( sunE * ( ( betaRTheta + betaMTheta ) / ( rayleighAtX + mieAtX ) ) * Fex, vec3( 1.0 / 2.0 ) ), clamp( pow( 1.0 - cosSunUpAngle, 5.0 ), 0.0, 1.0 ) );
+
+	// nightsky
+	float theta = acos( viewDirection.y ); // elevation --> y-axis, [-pi/2, pi/2]
+	float phi = atan( viewDirection.z, viewDirection.x ); // azimuth --> x-axis [-pi/2, pi/2]
+	vec2 uv = vec2( phi, theta ) / vec2( 2.0 * PI, PI ) + vec2( 0.5, 0.0 );
+	vec3 L0 = vec3( 0.1 ) * Fex;
+
+	// composition + solar disc
+	float sundisk = smoothstep( SUN_ANGULAR_DIAMETER_COS, SUN_ANGULAR_DIAMETER_COS + 0.00002, cosViewSunAngle );
+	L0 += ( sunE * 19000.0 * Fex ) * sundisk;
+
+	vec3 texColor = ( Lin + L0 ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );
+	float sunfade = 1.0 - clamp( 1.0 - exp( ( sunDirection.y / 450000.0 ) ), 0.0, 1.0 );
+	vec3 retColor = pow( texColor, vec3( 1.0 / ( 1.2 + ( 1.2 * sunfade ) ) ) );
+
+	return retColor;
+     
+    	// vec3 totalLightAtX = rayleighAtX + mieAtX;
+    	// vec3 lightFromXtoEye = rayleighXtoEye + mieXtoEye; 
+    
+    	// vec3 somethingElse = sunE * (lightFromXtoEye / totalLightAtX);
+    
+    	// vec3 sky = somethingElse * (1.0 - Fex);
+	// float oneMinusCosSun = 1.0 - cosSunUpAngle;
+    	// sky *= mix( vec3(1.0), pow(somethingElse * Fex,vec3(0.5)), 
+	//     clamp(oneMinusCosSun * oneMinusCosSun * oneMinusCosSun * oneMinusCosSun * oneMinusCosSun, 0.0, 1.0) );
+	// // composition + solar disk
+    	// float sundisk = smoothstep(SUN_ANGULAR_DIAMETER_COS - 0.0001, SUN_ANGULAR_DIAMETER_COS, cosViewSunAngle);
+	// vec3 sun = (sunE * SUN_POWER * Fex) * sundisk;
+	
+	// return sky + sun;
 }
 `;
 
