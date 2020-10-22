0c31fb9f0d892b83f7b2ee3990071aefbf632ec6
Erich Loftis <erichlof@swbell.net>
2020-10-22 14:08:10 -0500

    Update Material_Roughness.js

--- js/Material_Roughness.js
+++ js/Material_Roughness.js
@@ -131,7 +131,7 @@ function initPathTracingShaders() {
         // scene/demo-specific uniforms go here
         pathTracingUniforms = {
 
-                tPreviousTexture: { type: "t", value: screenTextureRenderTarget.texture },
+                tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },
 
                 uCameraIsMoving: { type: "b1", value: false },
         
@@ -149,7 +149,6 @@ function initPathTracingShaders() {
                 uResolution: { type: "v2", value: new THREE.Vector2() },
         
                 uMaterialColor: { type: "v3", value: new THREE.Color(0.0, 1.0, 1.0) },
-                uRandomVector: { type: "v3", value: new THREE.Vector3() },
         
                 uCameraMatrix: { type: "m4", value: new THREE.Matrix4() }
         
@@ -250,40 +249,8 @@ function updateVariablesAndUniforms() {
                 cameraIsMoving = true;
                 changeMaterialColor = false;
         }
-        
-        if ( !cameraIsMoving ) {
-                
-                if (sceneIsDynamic)
-                        sampleCounter = 1.0; // reset for continuous updating of image
-                else sampleCounter += 1.0; // for progressive refinement of image
-                
-                frameCounter += 1.0;
 
-                cameraRecentlyMoving = false;  
-        }
-
-        if (cameraIsMoving) {
-                sampleCounter = 1.0;
-                frameCounter += 1.0;
-
-                if (!cameraRecentlyMoving) {
-                        frameCounter = 1.0;
-                        cameraRecentlyMoving = true;
-                }
-        }
-
-        
-        pathTracingUniforms.uCameraIsMoving.value = cameraIsMoving;
-        pathTracingUniforms.uSampleCounter.value = sampleCounter;
-        pathTracingUniforms.uFrameCounter.value = frameCounter;
-        pathTracingUniforms.uRandomVector.value = randomVector.set(Math.random(), Math.random(), Math.random());
-        
-        
-        // CAMERA
-        cameraControlsObject.updateMatrixWorld(true);
-        pathTracingUniforms.uCameraMatrix.value.copy(worldCamera.matrixWorld);
-        screenOutputMaterial.uniforms.uOneOverSampleCounter.value = 1.0 / sampleCounter;
-
+        // INFO
         cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
 } // end function updateUniforms()
