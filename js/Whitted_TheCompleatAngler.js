32a63b77c711bfd78766694c478a81345fdbba3c
Erich Loftis <erichlof@swbell.net>
2020-11-16 12:00:02 -0600

    Update Whitted_TheCompleatAngler.js

--- js/Whitted_TheCompleatAngler.js
+++ js/Whitted_TheCompleatAngler.js
@@ -1,5 +1,4 @@
 // scene/demo-specific variables go here
-var EPS_intersect;
 var sceneIsDynamic = true;
 var camFlightSpeed = 60;
 var tileNormalMapTexture;
@@ -38,27 +37,7 @@ function initSceneData() {
 function initPathTracingShaders() {
  
         // scene/demo-specific uniforms go here
-        pathTracingUniforms = {
-
-                tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },
-                tTileNormalMapTexture: { type: "t", value: tileNormalMapTexture },
-
-                uCameraIsMoving: { type: "b1", value: false },
-        
-                uEPS_intersect: { type: "f", value: EPS_intersect },
-                uTime: { type: "f", value: 0.0 },
-                uSampleCounter: { type: "f", value: 0.0 },
-                uFrameCounter: { type: "f", value: 1.0 },
-                uULen: { type: "f", value: 1.0 },
-                uVLen: { type: "f", value: 1.0 },
-                uApertureSize: { type: "f", value: 0.0 },
-                uFocusDistance: { type: "f", value: focusDistance },
-        
-                uResolution: { type: "v2", value: new THREE.Vector2() },
-        
-                uCameraMatrix: { type: "m4", value: new THREE.Matrix4() }
-        
-        };
+        pathTracingUniforms.tTileNormalMapTexture = { type: "t", value: tileNormalMapTexture };
 
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
