1edd0e4ec05c5bc7a09843722e370813618e2b29
Erich Loftis <erichlof@swbell.net>
2020-11-16 11:27:54 -0600

    Update BVH_Point_Light_Source.js

--- js/BVH_Point_Light_Source.js
+++ js/BVH_Point_Light_Source.js
@@ -342,29 +342,8 @@ function initSceneData() {
 function initPathTracingShaders() {
  
         // scene/demo-specific uniforms go here
-        pathTracingUniforms = {
-
-                tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },
-                tTriangleTexture: { type: "t", value: triangleDataTexture },
-                tAABBTexture: { type: "t", value: aabbDataTexture },
-                //tAlbedoTextures: { type: "t", value: uniqueMaterialTextures },
-
-                uCameraIsMoving: { type: "b1", value: false },
-
-                uEPS_intersect: { type: "f", value: EPS_intersect },
-                uTime: { type: "f", value: 0.0 },
-                uSampleCounter: { type: "f", value: 1.0 },
-                uFrameCounter: { type: "f", value: 1.0 },
-                uULen: { type: "f", value: 1.0 },
-                uVLen: { type: "f", value: 1.0 },
-                uApertureSize: { type: "f", value: 0.0 },
-                uFocusDistance: { type: "f", value: focusDistance },
-
-                uResolution: { type: "v2", value: new THREE.Vector2() },
-
-                uCameraMatrix: {type: "m4", value: new THREE.Matrix4() }
-        
-        };
+        pathTracingUniforms.tTriangleTexture = { type: "t", value: triangleDataTexture };
+        pathTracingUniforms.tAABBTexture = { type: "t", value: aabbDataTexture };
 
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
