14ffb6beda06dfb02fda0bf8b1d0a19f8c7b29e0
Erich Loftis <erichlof@swbell.net>
2020-11-16 11:39:34 -0600

    Update HDRI_Environment.js

--- js/HDRI_Environment.js
+++ js/HDRI_Environment.js
@@ -85,7 +85,7 @@ function init_GUI() {
                 mouseControl = false;
                 // if on mobile device, unpause the app because there is no ESC key and no mouse capture to do
                 isPaused = false;
-		
+                
                 ableToEngagePointerLock = true;
 
                 mobileJoystickControls = new MobileJoystickControls ({
@@ -479,35 +479,13 @@ function initSceneData() {
 function initPathTracingShaders() {
  
         // scene/demo-specific uniforms go here
-        pathTracingUniforms = {
-
-                tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },
-                tTriangleTexture: { type: "t", value: triangleDataTexture },
-                tAABBTexture: { type: "t", value: aabbDataTexture },
-                tHDRTexture: { type: "t", value: hdrTexture },
-
-                uCameraIsMoving: { type: "b1", value: false },
-
-		uMaterialType: { type: "i", value: 0 },
-
-                uEPS_intersect: { type: "f", value: EPS_intersect },
-                uTime: { type: "f", value: 0.0 },
-                uSampleCounter: { type: "f", value: 1.0 },
-                uFrameCounter: { type: "f", value: 1.0 },
-                uULen: { type: "f", value: 1.0 },
-                uVLen: { type: "f", value: 1.0 },
-                uApertureSize: { type: "f", value: 0.0 },
-                uFocusDistance: { type: "f", value: focusDistance },
-                uHDRI_Exposure: { type: "f", value: 1.0 },
-                uRoughness: { type: "f", value: 0.0 },
-
-                uResolution: { type: "v2", value: new THREE.Vector2() },
-
-                uMaterialColor: { type: "v3", value: new THREE.Color() },
-
-                uCameraMatrix: {type: "m4", value: new THREE.Matrix4() }
-        
-        };
+        pathTracingUniforms.tTriangleTexture = { type: "t", value: triangleDataTexture };
+        pathTracingUniforms.tAABBTexture = { type: "t", value: aabbDataTexture };
+        pathTracingUniforms.tHDRTexture = { type: "t", value: hdrTexture };
+        pathTracingUniforms.uMaterialType = { type: "i", value: 0 };
+        pathTracingUniforms.uHDRI_Exposure = { type: "f", value: 1.0 };
+        pathTracingUniforms.uRoughness = { type: "f", value: 0.0 };
+        pathTracingUniforms.uMaterialColor = { type: "v3", value: new THREE.Color() };
 
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
