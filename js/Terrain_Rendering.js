2816995d76ef86d0caa1da86ed1e944884ef5cbb
Erich Loftis <erichlof@swbell.net>
2020-11-16 11:44:48 -0600

    Update Terrain_Rendering.js

--- js/Terrain_Rendering.js
+++ js/Terrain_Rendering.js
@@ -39,31 +39,10 @@ function initSceneData() {
 function initPathTracingShaders() {
  
         // scene/demo-specific uniforms go here
-        pathTracingUniforms = {
-					
-                tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },	
-                t_PerlinNoise: { type: "t", value: PerlinNoiseTexture },
-                
-                uCameraIsMoving: { type: "b1", value: false },
-                
-                uEPS_intersect: { type: "f", value: EPS_intersect },
-                uCameraUnderWater: { type: "f", value: 0.0 },
-                uWaterLevel: { type: "f", value: 0.0 },
-                uTime: { type: "f", value: 0.0 },
-                uSampleCounter: { type: "f", value: 0.0 },
-                uFrameCounter: { type: "f", value: 1.0 },
-                uULen: { type: "f", value: 1.0 },
-                uVLen: { type: "f", value: 1.0 },
-                uApertureSize: { type: "f", value: 0.0 },
-                uFocusDistance: { type: "f", value: focusDistance },
-                
-                uResolution: { type: "v2", value: new THREE.Vector2() },
-                
-                uSunDirection: { type: "v3", value: new THREE.Vector3() },
-        
-                uCameraMatrix: { type: "m4", value: new THREE.Matrix4() }
-
-        };
+        pathTracingUniforms.t_PerlinNoise = { type: "t", value: PerlinNoiseTexture };      
+	pathTracingUniforms.uCameraUnderWater = { type: "f", value: 0.0 };
+	pathTracingUniforms.uWaterLevel = { type: "f", value: 0.0 };  
+	pathTracingUniforms.uSunDirection = { type: "v3", value: new THREE.Vector3() };
 
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
