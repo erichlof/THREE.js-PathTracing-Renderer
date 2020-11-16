0fb9405da1d5e9b899c11e4bfa71a8874d59101c
Erich Loftis <erichlof@swbell.net>
2020-11-16 12:44:22 -0600

    Update Grid_Acceleration_Fragment.glsl

--- shaders/Grid_Acceleration_Fragment.glsl
+++ shaders/Grid_Acceleration_Fragment.glsl
@@ -314,25 +314,21 @@ void main(void)
 	vec3 camUp = vec3(uCameraMatrix[1][0], uCameraMatrix[1][1], uCameraMatrix[1][2]);
 	vec3 camForward = vec3(-uCameraMatrix[2][0], -uCameraMatrix[2][1], -uCameraMatrix[2][2]);
 
-	// calculate unique seed for rand() function
-	seed = uvec2(uFrameCounter, uFrameCounter + 1.0) * uvec2(gl_FragCoord);
+	// calculate unique seed for rng() function
+	//seed = uvec2(uFrameCounter, uFrameCounter + 1.0) * uvec2(gl_FragCoord); // old way of generating random numbers
+
+	randVec4 = texture(tBlueNoiseTexture, (gl_FragCoord.xy + (uRandomVec2 * 256.0)) / 256.0 ); // new way of rand()
 
 	vec2 pixelPos = vec2(0);
 	vec2 pixelOffset = vec2(0);
-
+	
 	float x = rand();
 	float y = rand();
 
-	if (!uCameraIsMoving) 
-		pixelOffset = vec2(tentFilter(x), tentFilter(y));
-	
-
-	// pixelOffset ranges from -1.0 to +1.0, so only need to divide by half resolution
-	pixelOffset /= (uResolution * 0.5);
+	pixelOffset = vec2(tentFilter(x), tentFilter(y)) * 0.5;
 
 	// we must map pixelPos into the range -1.0 to +1.0
-	pixelPos = (gl_FragCoord.xy / uResolution) * 2.0 - 1.0;
-	pixelPos += pixelOffset;
+	pixelPos = ((gl_FragCoord.xy + pixelOffset) / uResolution) * 2.0 - 1.0;
 
 	vec3 rayDir = normalize(pixelPos.x * camRight * uULen + pixelPos.y * camUp * uVLen + camForward);
 
@@ -351,15 +347,16 @@ void main(void)
 
 	vec3 previousColor = texelFetch(tPreviousTexture, ivec2(gl_FragCoord.xy), 0).rgb;
 
-	if (uFrameCounter == 1.0) // camera just moved after being still
+	if (uCameraIsMoving)
 	{
-		previousColor = vec3(0); // clear rendering accumulation buffer
-	} 
-	else if (uCameraIsMoving) // camera is currently moving
+                previousColor *= 0.5; // motion-blur trail amount (old image)
+                pixelColor *= 0.5; // brightness of new image (noisy)
+        }
+	else
 	{
-		previousColor *= 0.5; // motion-blur trail amount (old image)
-		pixelColor *= 0.5; // brightness of new image (noisy)
-	}
+                previousColor *= 0.9; // motion-blur trail amount (old image)
+                pixelColor *= 0.1; // brightness of new image (noisy)
+        }
 
 	pc_fragColor = vec4(pixelColor + previousColor, 1.0);
 }
