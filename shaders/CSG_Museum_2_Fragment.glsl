e0db242c7999d402fae9380d27c4b216c235113e
Erich Loftis <erichlof@swbell.net>
2020-12-03 09:29:36 -0600

    Update CSG_Museum_2_Fragment.glsl

--- shaders/CSG_Museum_2_Fragment.glsl
+++ shaders/CSG_Museum_2_Fragment.glsl
@@ -1013,7 +1013,7 @@ vec3 CalculateRadiance(Ray r)
 				continue;
 			}
                         
-			dirToLight = sampleQuadLight(x, nl, lightChoice, dirToLight, weight);
+			dirToLight = sampleQuadLight(x, nl, lightChoice, weight);
 			mask *= weight * N_LIGHTS;
 
 			r = Ray( x, dirToLight );
@@ -1120,7 +1120,7 @@ vec3 CalculateRadiance(Ray r)
 				continue;
 			}
 
-			dirToLight = sampleQuadLight(x, nl, lightChoice, dirToLight, weight);
+			dirToLight = sampleQuadLight(x, nl, lightChoice, weight);
 			mask *= weight * N_LIGHTS;
 			
 			r = Ray( x, dirToLight );
