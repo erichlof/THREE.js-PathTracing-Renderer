44c350dc2e622cb9b5e0726bd51423e2642b3a15
Erich Loftis <erichlof@swbell.net>
2021-04-24 14:06:45 -0500

    Update Kajiya_TheRenderingEquation.js

--- js/Kajiya_TheRenderingEquation.js
+++ js/Kajiya_TheRenderingEquation.js
@@ -26,7 +26,9 @@ function initSceneData() {
 function initPathTracingShaders() {
  
         // scene/demo-specific uniforms go here
-
+        pathTracingUniforms.uColorEdgeSharpeningRate = { type: "f", value: 0.1 };
+        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 0.01 };
+        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 0.05 };
         
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
