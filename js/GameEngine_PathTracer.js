6abbcbd39a548e7ea3a0122dc851962bf02e4857
Erich Loftis <erichlof@swbell.net>
2021-04-24 14:03:06 -0500

    Update GameEngine_PathTracer.js

--- js/GameEngine_PathTracer.js
+++ js/GameEngine_PathTracer.js
@@ -7,6 +7,7 @@ var torusRotationAngle = 0;
 // called automatically from within initTHREEjs() function
 function initSceneData()
 {
+        //pixelRatio = 1.0;
 
         // scene/demo-specific three.js objects setup goes here
         EPS_intersect = mouseControl ? 0.01 : 1.0; // less precision on mobile
@@ -36,6 +37,10 @@ function initPathTracingShaders()
 {
         // scene/demo-specific uniforms go here
         pathTracingUniforms.uTorusInvMatrix = { type: "m4", value: new THREE.Matrix4() };
+        pathTracingUniforms.uColorEdgeSharpeningRate = { type: "f", value: 0.0 };
+        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 1.0 };
+        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 0.1 };
+
 
         pathTracingDefines = {
                 //NUMBER_OF_TRIANGLES: total_number_of_triangles
