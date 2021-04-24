7c86d8eab2355164ce852a22b0e2371520cc7d94
Erich Loftis <erichlof@swbell.net>
2021-04-24 14:04:45 -0500

    Update Geometry_Showcase.js

--- js/Geometry_Showcase.js
+++ js/Geometry_Showcase.js
@@ -6,7 +6,7 @@ var torusObject;
 // called automatically from within initTHREEjs() function
 function initSceneData()
 {
-
+        //pixelRatio = 1;
         // scene/demo-specific three.js objects setup goes here
         EPS_intersect = mouseControl ? 0.01 : 1.0; // less precision on mobile
 
@@ -36,6 +36,9 @@ function initPathTracingShaders()
 {
         // scene/demo-specific uniforms go here
         pathTracingUniforms.uTorusInvMatrix = { type: "m4", value: new THREE.Matrix4() };
+        pathTracingUniforms.uColorEdgeSharpeningRate = { type: "f", value: 0.08 };
+        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 1.0 };
+        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 0.05 };
         
         pathTracingDefines = {
                 //NUMBER_OF_TRIANGLES: total_number_of_triangles
