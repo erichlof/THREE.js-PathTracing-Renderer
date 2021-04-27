bd3e567b6528dc46128eeec071e4a9efd3adb393
Erich Loftis <erichlof@swbell.net>
2021-04-27 12:33:43 -0500

    Update Geometry_Showcase.js

--- js/Geometry_Showcase.js
+++ js/Geometry_Showcase.js
@@ -6,7 +6,8 @@ var torusObject;
 // called automatically from within initTHREEjs() function
 function initSceneData()
 {
-        //pixelRatio = 1;
+        //pixelRatio = 1; // for computers with the latest GPUs!
+
         // scene/demo-specific three.js objects setup goes here
         EPS_intersect = mouseControl ? 0.01 : 1.0; // less precision on mobile
 
@@ -37,8 +38,8 @@ function initPathTracingShaders()
         // scene/demo-specific uniforms go here
         pathTracingUniforms.uTorusInvMatrix = { type: "m4", value: new THREE.Matrix4() };
         pathTracingUniforms.uColorEdgeSharpeningRate = { type: "f", value: 0.1 };
-        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 1.0 };
-        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 0.1 };
+        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 0.1 };
+        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 1.0 };
         
         pathTracingDefines = {
                 //NUMBER_OF_TRIANGLES: total_number_of_triangles
@@ -90,7 +91,6 @@ function createPathTracingMaterial()
 // called automatically from within the animate() function
 function updateVariablesAndUniforms()
 {
-
         // TORUS
         torusObject.updateMatrixWorld(true); // 'true' forces immediate matrix update
         pathTracingUniforms.uTorusInvMatrix.value.copy(torusObject.matrixWorld).invert();
