40668b55d976eb871f99a0669ccc1d6a176f9e9d
Erich Loftis <erichlof@swbell.net>
2021-05-21 09:13:30 -0500

    Update ScreenOutput_Fragment.glsl

--- shaders/ScreenOutput_Fragment.glsl
+++ shaders/ScreenOutput_Fragment.glsl
@@ -8,227 +8,275 @@ uniform sampler2D tPathTracedImageTexture;
 
 void main()
 {
-        vec4 m[9];
-        m[0] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1,1)), 0);
-        m[1] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(0,1)), 0);
-        m[2] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(1,1)), 0);
+        
+        // 5x5 kernel
+        vec4 m25[25];
 
-        m[3] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1,0)), 0);
-        m[4] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(0,0)), 0);
-        m[5] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(1,0)), 0);
+        m25[ 0] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-2, 2)), 0);
+        m25[ 1] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1, 2)), 0);
+        m25[ 2] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 0, 2)), 0);
+        m25[ 3] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 1, 2)), 0);
+        m25[ 4] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 2, 2)), 0);
 
-        m[6] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1,-1)), 0);
-        m[7] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(0,-1)), 0);
-        m[8] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(1,-1)), 0);
+        m25[ 5] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-2, 1)), 0);
+        m25[ 6] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1, 1)), 0);
+        m25[ 7] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 0, 1)), 0);
+        m25[ 8] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 1, 1)), 0);
+        m25[ 9] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 2, 1)), 0);
 
-        vec4 centerPixel = m[4];
+        m25[10] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-2, 0)), 0);
+        m25[11] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1, 0)), 0);
+        m25[12] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 0, 0)), 0);
+        m25[13] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 1, 0)), 0);
+        m25[14] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 2, 0)), 0);
+
+        m25[15] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-2,-1)), 0);
+        m25[16] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1,-1)), 0);
+        m25[17] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 0,-1)), 0);
+        m25[18] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 1,-1)), 0);
+        m25[19] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 2,-1)), 0);
+
+        m25[20] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-2,-2)), 0);
+        m25[21] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2(-1,-2)), 0);
+        m25[22] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 0,-2)), 0);
+        m25[23] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 1,-2)), 0);
+        m25[24] = texelFetch(tPathTracedImageTexture, ivec2(gl_FragCoord.xy + vec2( 2,-2)), 0);
+        
+        vec4 centerPixel = m25[12];
         vec3 filteredPixelColor;
 	float threshold = 1.0;
         int count = 1;
 
-
-        // 3x3 kernel (good for half screen resolutions (pixelRatio = 0.5))
         // start with center pixel
-	filteredPixelColor = m[4].rgb;
-
+        filteredPixelColor = m25[12].rgb;
         // search left
-        if (m[3].a < threshold)
+        if (m25[11].a < threshold)
         {
-                filteredPixelColor += m[3].rgb;
+                filteredPixelColor += m25[11].rgb;
                 count++; 
+                if (m25[10].a < threshold)
+                {
+                        filteredPixelColor += m25[10].rgb;
+                        count++; 
+                }
+                if (m25[5].a < threshold)
+                {
+                        filteredPixelColor += m25[5].rgb;
+                        count++; 
+                }
         }
         // search right
-        if (m[5].a < threshold)
+        if (m25[13].a < threshold)
         {
-                filteredPixelColor += m[5].rgb;
+                filteredPixelColor += m25[13].rgb;
                 count++; 
+                if (m25[14].a < threshold)
+                {
+                        filteredPixelColor += m25[14].rgb;
+                        count++; 
+                }
+                if (m25[19].a < threshold)
+                {
+                        filteredPixelColor += m25[19].rgb;
+                        count++; 
+                }
         }
         // search above
-        if (m[1].a < threshold)
+        if (m25[7].a < threshold)
         {
-                filteredPixelColor += m[1].rgb;
+                filteredPixelColor += m25[7].rgb;
                 count++; 
+                if (m25[2].a < threshold)
+                {
+                        filteredPixelColor += m25[2].rgb;
+                        count++; 
+                }
+                if (m25[3].a < threshold)
+                {
+                        filteredPixelColor += m25[3].rgb;
+                        count++; 
+                }
         }
         // search below
-        if (m[7].a < threshold)
+        if (m25[17].a < threshold)
         {
-                filteredPixelColor += m[7].rgb;
+                filteredPixelColor += m25[17].rgb;
                 count++; 
+                if (m25[22].a < threshold)
+                {
+                        filteredPixelColor += m25[22].rgb;
+                        count++; 
+                }
+                if (m25[21].a < threshold)
+                {
+                        filteredPixelColor += m25[21].rgb;
+                        count++; 
+                }
         }
 
         // search upper-left
-        if (m[0].a < threshold)
+        if (m25[6].a < threshold)
         {
-                filteredPixelColor += m[0].rgb;
+                filteredPixelColor += m25[6].rgb;
                 count++; 
+                if (m25[0].a < threshold)
+                {
+                        filteredPixelColor += m25[0].rgb;
+                        count++; 
+                }
+                if (m25[1].a < threshold)
+                {
+                        filteredPixelColor += m25[1].rgb;
+                        count++; 
+                }
         }
         // search upper-right
-        if (m[2].a < threshold)
+        if (m25[8].a < threshold)
         {
-                filteredPixelColor += m[2].rgb;
+                filteredPixelColor += m25[8].rgb;
                 count++; 
+                if (m25[4].a < threshold)
+                {
+                        filteredPixelColor += m25[4].rgb;
+                        count++; 
+                }
+                if (m25[9].a < threshold)
+                {
+                        filteredPixelColor += m25[9].rgb;
+                        count++; 
+                }
         }
         // search lower-left
-        if (m[6].a < threshold)
+        if (m25[16].a < threshold)
         {
-                filteredPixelColor += m[6].rgb;
+                filteredPixelColor += m25[16].rgb;
                 count++; 
+                if (m25[15].a < threshold)
+                {
+                        filteredPixelColor += m25[15].rgb;
+                        count++; 
+                }
+                if (m25[20].a < threshold)
+                {
+                        filteredPixelColor += m25[20].rgb;
+                        count++; 
+                }
         }
         // search lower-right
-        if (m[8].a < threshold)
+        if (m25[18].a < threshold)
         {
-                filteredPixelColor += m[8].rgb;
+                filteredPixelColor += m25[18].rgb;
                 count++; 
+                if (m25[23].a < threshold)
+                {
+                        filteredPixelColor += m25[23].rgb;
+                        count++; 
+                }
+                if (m25[24].a < threshold)
+                {
+                        filteredPixelColor += m25[24].rgb;
+                        count++; 
+                }
         }
-
+        
         filteredPixelColor /= float(count);
 
-        if (uOneOverSampleCounter < 1.0)
-	        filteredPixelColor = mix(filteredPixelColor, centerPixel.rgb, clamp(centerPixel.a, 0.0, 1.0));
 
+        // 3x3 kernel
+        vec4 m9[9];
+        m9[0] = m25[6];
+        m9[1] = m25[7];
+        m9[2] = m25[8];
+
+        m9[3] = m25[11];
+        m9[4] = m25[12];
+        m9[5] = m25[13];
+
+        m9[6] = m25[16];
+        m9[7] = m25[17];
+        m9[8] = m25[18];
+
+        if (centerPixel.a > 0.0 || centerPixel.a == -1.0)
+        {
+                // reset variables
+                centerPixel = m9[4];
+                count = 1;
+
+                // start with center pixel
+                filteredPixelColor = m9[4].rgb;
+
+                // search left
+                if (m9[3].a < threshold)
+                {
+                        filteredPixelColor += m9[3].rgb;
+                        count++; 
+                }
+                // search right
+                if (m9[5].a < threshold)
+                {
+                        filteredPixelColor += m9[5].rgb;
+                        count++; 
+                }
+                // search above
+                if (m9[1].a < threshold)
+                {
+                        filteredPixelColor += m9[1].rgb;
+                        count++; 
+                }
+                // search below
+                if (m9[7].a < threshold)
+                {
+                        filteredPixelColor += m9[7].rgb;
+                        count++; 
+                }
+
+                // search upper-left
+                if (m9[0].a < threshold)
+                {
+                        filteredPixelColor += m9[0].rgb;
+                        count++; 
+                }
+                // search upper-right
+                if (m9[2].a < threshold)
+                {
+                        filteredPixelColor += m9[2].rgb;
+                        count++; 
+                }
+                // search lower-left
+                if (m9[6].a < threshold)
+                {
+                        filteredPixelColor += m9[6].rgb;
+                        count++; 
+                }
+                // search lower-right
+                if (m9[8].a < threshold)
+                {
+                        filteredPixelColor += m9[8].rgb;
+                        count++; 
+                }
+
+                filteredPixelColor /= float(count);
+
+                filteredPixelColor = mix(filteredPixelColor, centerPixel.rgb, 0.5);
+        } // end if (centerPixel.a > 0.0)
+
+
+        if ((centerPixel.a == 1.01 && uOneOverSampleCounter < 0.005) || uOneOverSampleCounter < 0.0002)
+        {
+                filteredPixelColor = centerPixel.rgb;
+        }
+
+
+        // final filteredPixelColor processing ////////////////////////////////////
+
+        // average accumulation buffer
         filteredPixelColor *= uOneOverSampleCounter;
 
+        // apply tone mapping (brings pixel into 0.0-1.0 rgb color range)
         filteredPixelColor = ReinhardToneMapping(filteredPixelColor);
         //filteredPixelColor = OptimizedCineonToneMapping(filteredPixelColor);
         //filteredPixelColor = ACESFilmicToneMapping(filteredPixelColor);
 
+        // lastly, apply gamma correction (gives more intensity/brightness range where it's needed)
         pc_fragColor = clamp(vec4( pow(filteredPixelColor, vec3(0.4545)), 1.0 ), 0.0, 1.0);
 }
-
-
-
-        /* // 5x5 kernel (good for full screen resolutions (pixelRatio = 1))
-        vec4 m[25];
-
-        // startwith center pixel
-        filteredPixelColor = m[12].rgb;
-        // search left
-        if (m[11].a < threshold)
-        {
-                filteredPixelColor += m[11].rgb;
-                count++; 
-                if (m[10].a < threshold)
-                {
-                        filteredPixelColor += m[10].rgb;
-                        count++; 
-                }
-                if (m[5].a < threshold)
-                {
-                        filteredPixelColor += m[5].rgb;
-                        count++; 
-                }
-        }
-        // search right
-        if (m[13].a < threshold)
-        {
-                filteredPixelColor += m[13].rgb;
-                count++; 
-                if (m[14].a < threshold)
-                {
-                        filteredPixelColor += m[14].rgb;
-                        count++; 
-                }
-                if (m[19].a < threshold)
-                {
-                        filteredPixelColor += m[19].rgb;
-                        count++; 
-                }
-        }
-        // search above
-        if (m[7].a < threshold)
-        {
-                filteredPixelColor += m[7].rgb;
-                count++; 
-                if (m[2].a < threshold)
-                {
-                        filteredPixelColor += m[2].rgb;
-                        count++; 
-                }
-                if (m[3].a < threshold)
-                {
-                        filteredPixelColor += m[3].rgb;
-                        count++; 
-                }
-        }
-        // search below
-        if (m[17].a < threshold)
-        {
-                filteredPixelColor += m[17].rgb;
-                count++; 
-                if (m[22].a < threshold)
-                {
-                        filteredPixelColor += m[22].rgb;
-                        count++; 
-                }
-                if (m[21].a < threshold)
-                {
-                        filteredPixelColor += m[21].rgb;
-                        count++; 
-                }
-        }
-
-        // search upper-left
-        if (m[6].a < threshold)
-        {
-                filteredPixelColor += m[6].rgb;
-                count++; 
-                if (m[0].a < threshold)
-                {
-                        filteredPixelColor += m[0].rgb;
-                        count++; 
-                }
-                if (m[1].a < threshold)
-                {
-                        filteredPixelColor += m[1].rgb;
-                        count++; 
-                }
-        }
-        // search upper-right
-        if (m[8].a < threshold)
-        {
-                filteredPixelColor += m[8].rgb;
-                count++; 
-                if (m[4].a < threshold)
-                {
-                        filteredPixelColor += m[4].rgb;
-                        count++; 
-                }
-                if (m[9].a < threshold)
-                {
-                        filteredPixelColor += m[9].rgb;
-                        count++; 
-                }
-        }
-        // search lower-left
-        if (m[16].a < threshold)
-        {
-                filteredPixelColor += m[16].rgb;
-                count++; 
-                if (m[15].a < threshold)
-                {
-                        filteredPixelColor += m[15].rgb;
-                        count++; 
-                }
-                if (m[20].a < threshold)
-                {
-                        filteredPixelColor += m[20].rgb;
-                        count++; 
-                }
-        }
-        // search lower-right
-        if (m[18].a < threshold)
-        {
-                filteredPixelColor += m[18].rgb;
-                count++; 
-                if (m[23].a < threshold)
-                {
-                        filteredPixelColor += m[23].rgb;
-                        count++; 
-                }
-                if (m[24].a < threshold)
-                {
-                        filteredPixelColor += m[24].rgb;
-                        count++; 
-                }
-        }
-        filteredPixelColor /= float(count); */
