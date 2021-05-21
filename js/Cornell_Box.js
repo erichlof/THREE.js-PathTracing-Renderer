e132cca36ab9e08b6d8a75847ba905fc115f26a1
Erich Loftis <erichlof@swbell.net>
2021-05-21 09:27:27 -0500

    Update Cornell_Box.js

--- js/Cornell_Box.js
+++ js/Cornell_Box.js
@@ -62,9 +62,6 @@ function initPathTracingShaders() {
         // scene/demo-specific uniforms go here     
         pathTracingUniforms.uTallBoxInvMatrix = { type: "m4", value: new THREE.Matrix4() };
         pathTracingUniforms.uShortBoxInvMatrix = { type: "m4", value: new THREE.Matrix4() };
-        pathTracingUniforms.uColorEdgeSharpeningRate = { type: "f", value: 1.0 };
-        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 0.05 };
-        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 1.0 };
         
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
