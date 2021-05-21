64a9f08693447febdac572c56069d3bea5a95584
Erich Loftis <erichlof@swbell.net>
2021-05-21 09:28:02 -0500

    Update GameEngine_PathTracer.js

--- js/GameEngine_PathTracer.js
+++ js/GameEngine_PathTracer.js
@@ -37,10 +37,6 @@ function initPathTracingShaders()
 {
         // scene/demo-specific uniforms go here
         pathTracingUniforms.uTorusInvMatrix = { type: "m4", value: new THREE.Matrix4() };
-        pathTracingUniforms.uColorEdgeSharpeningRate = { type: "f", value: 1.0 };
-        pathTracingUniforms.uNormalEdgeSharpeningRate = { type: "f", value: 1.0 };
-        pathTracingUniforms.uObjectEdgeSharpeningRate = { type: "f", value: 0.1 };
-
 
         pathTracingDefines = {
                 //NUMBER_OF_TRIANGLES: total_number_of_triangles
