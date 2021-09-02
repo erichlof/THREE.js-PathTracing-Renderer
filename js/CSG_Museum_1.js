5bfcb1665e172acbd811cbb979299e047bd5d6ba
Erich Loftis <erichlof@swbell.net>
2021-09-01 23:30:33 -0500

    Update CSG_Museum_1.js

--- js/CSG_Museum_1.js
+++ js/CSG_Museum_1.js
@@ -5,9 +5,11 @@ var camFlightSpeed = 70;
 // called automatically from within initTHREEjs() function
 function initSceneData() 
 {
-        //pixelRatio = 1; // for computers with the latest GPUs!
-
         // scene/demo-specific three.js objects setup goes here
+
+        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+
         EPS_intersect = mouseControl ? 0.1 : 1.0; // less precision on mobile
 
         // set camera's field of view
@@ -28,6 +30,7 @@ function initSceneData()
 function initPathTracingShaders() 
 {
         // scene/demo-specific uniforms go here
+        
 
         pathTracingDefines = {
         	//NUMBER_OF_TRIANGLES: total_number_of_triangles
