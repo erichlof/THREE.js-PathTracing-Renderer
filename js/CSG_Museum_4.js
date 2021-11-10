d22a8294a8bd03d7c86edf5214d88353d3cc0045
Erich Loftis <erichlof@swbell.net>
2021-11-09 22:43:51 -0600

    Update CSG_Museum_4.js

--- js/CSG_Museum_4.js
+++ js/CSG_Museum_4.js
@@ -5,22 +5,22 @@ var camFlightSpeed = 70;
 // called automatically from within initTHREEjs() function
 function initSceneData() 
 {
-        // scene/demo-specific three.js objects setup goes here
+	// scene/demo-specific three.js objects setup goes here
 
-        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
 
-        EPS_intersect = mouseControl ? 0.1 : 1.0; // less precision on mobile
+	EPS_intersect = 0.01;
 
-        // set camera's field of view
-        worldCamera.fov = 60;
-        focusDistance = 153.0;
+	// set camera's field of view
+	worldCamera.fov = 60;
+	focusDistance = 153.0;
 
-        // position and orient camera
-        cameraControlsObject.position.set(0, 20, 150);
-        ///cameraControlsYawObject.rotation.y = 0.0;
-        // look slightly downward
-        ///cameraControlsPitchObject.rotation.x = -0.4;
+	// position and orient camera
+	cameraControlsObject.position.set(0, 20, 150);
+	///cameraControlsYawObject.rotation.y = 0.0;
+	// look slightly downward
+	///cameraControlsPitchObject.rotation.x = -0.4;
 
 } // end function initSceneData()
 
@@ -29,20 +29,20 @@ function initSceneData()
 // called automatically from within initTHREEjs() function
 function initPathTracingShaders() 
 {
-        // scene/demo-specific uniforms go here
-        
+	// scene/demo-specific uniforms go here
+	
 
-        pathTracingDefines = {
-                //NUMBER_OF_TRIANGLES: total_number_of_triangles
-        };
+	pathTracingDefines = {
+		//NUMBER_OF_TRIANGLES: total_number_of_triangles
+	};
 
-        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
-        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) 
-        {
-                pathTracingVertexShader = shaderText;
+	// load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+	fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) 
+	{
+		pathTracingVertexShader = shaderText;
 
-                createPathTracingMaterial();
-        });
+		createPathTracingMaterial();
+	});
 
 } // end function initPathTracingShaders()
 
@@ -51,29 +51,29 @@ function initPathTracingShaders()
 function createPathTracingMaterial() 
 {
 
-        fileLoader.load('shaders/CSG_Museum_4_Fragment.glsl', function (shaderText) 
-        {
+	fileLoader.load('shaders/CSG_Museum_4_Fragment.glsl', function (shaderText) 
+	{
 
-                pathTracingFragmentShader = shaderText;
+		pathTracingFragmentShader = shaderText;
 
-                pathTracingMaterial = new THREE.ShaderMaterial({
-                        uniforms: pathTracingUniforms,
-                        defines: pathTracingDefines,
-                        vertexShader: pathTracingVertexShader,
-                        fragmentShader: pathTracingFragmentShader,
-                        depthTest: false,
-                        depthWrite: false
-                });
+		pathTracingMaterial = new THREE.ShaderMaterial({
+			uniforms: pathTracingUniforms,
+			defines: pathTracingDefines,
+			vertexShader: pathTracingVertexShader,
+			fragmentShader: pathTracingFragmentShader,
+			depthTest: false,
+			depthWrite: false
+		});
 
-                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
-                pathTracingScene.add(pathTracingMesh);
+		pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+		pathTracingScene.add(pathTracingMesh);
 
-                // the following keeps the large scene ShaderMaterial quad right in front 
-                //   of the camera at all times. This is necessary because without it, the scene 
-                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
-                worldCamera.add(pathTracingMesh);
+		// the following keeps the large scene ShaderMaterial quad right in front 
+		//   of the camera at all times. This is necessary because without it, the scene 
+		//   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+		worldCamera.add(pathTracingMesh);
 
-        });
+	});
 
 } // end function createPathTracingMaterial()
 
@@ -82,11 +82,11 @@ function createPathTracingMaterial()
 // called automatically from within the animate() function
 function updateVariablesAndUniforms() 
 {
-        // clamp camera height for walking on Museum floor
-        cameraControlsObject.position.y = 20;
+	// clamp camera height for walking on Museum floor
+	cameraControlsObject.position.y = 20;
 
-        // INFO
-        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
 } // end function updateUniforms()
 
