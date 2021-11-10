c647561807d8a85022a2c0b5389aac90048b68fc
Erich Loftis <erichlof@swbell.net>
2021-11-10 11:39:52 -0600

    Update Ray_Warping.js

--- js/Ray_Warping.js
+++ js/Ray_Warping.js
@@ -5,22 +5,22 @@ var camFlightSpeed = 300;
 // called automatically from within initTHREEjs() function
 function initSceneData() 
 {
-        // scene/demo-specific three.js objects setup goes here
+	// scene/demo-specific three.js objects setup goes here
 
-        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
 
-        EPS_intersect = mouseControl ? 0.01 : 1.0; // less precision on mobile
+	EPS_intersect = 0.01;
 
-        // set camera's field of view
-        worldCamera.fov = 50;
-        focusDistance = 750.0;
+	// set camera's field of view
+	worldCamera.fov = 50;
+	focusDistance = 750.0;
 
-        // position and orient camera
-        cameraControlsObject.position.set(278, 270, 550);
-        ///cameraControlsYawObject.rotation.y = 0.0;
-        // look slightly upward
-        cameraControlsPitchObject.rotation.x = 0.005;
+	// position and orient camera
+	cameraControlsObject.position.set(278, 270, 550);
+	///cameraControlsYawObject.rotation.y = 0.0;
+	// look slightly upward
+	cameraControlsPitchObject.rotation.x = 0.005;
 
 } // end function initSceneData()
 
@@ -30,20 +30,20 @@ function initSceneData()
 function initPathTracingShaders() 
 {
  
-        // scene/demo-specific uniforms go here
-        
+	// scene/demo-specific uniforms go here
+	
 
-        pathTracingDefines = {
-        	//NUMBER_OF_TRIANGLES: total_number_of_triangles
-        };
+	pathTracingDefines = {
+		//NUMBER_OF_TRIANGLES: total_number_of_triangles
+	};
 
-        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
-        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) 
-        {
-                pathTracingVertexShader = shaderText;
+	// load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+	fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) 
+	{
+		pathTracingVertexShader = shaderText;
 
-                createPathTracingMaterial();
-        });
+		createPathTracingMaterial();
+	});
 
 } // end function initPathTracingShaders()
 
@@ -52,29 +52,29 @@ function initPathTracingShaders()
 function createPathTracingMaterial() 
 {
 
-        fileLoader.load('shaders/Ray_Warping_Fragment.glsl', function (shaderText) 
-        {
-                
-                pathTracingFragmentShader = shaderText;
+	fileLoader.load('shaders/Ray_Warping_Fragment.glsl', function (shaderText) 
+	{
+		
+		pathTracingFragmentShader = shaderText;
 
-                pathTracingMaterial = new THREE.ShaderMaterial({
-                        uniforms: pathTracingUniforms,
-                        defines: pathTracingDefines,
-                        vertexShader: pathTracingVertexShader,
-                        fragmentShader: pathTracingFragmentShader,
-                        depthTest: false,
-                        depthWrite: false
-                });
+		pathTracingMaterial = new THREE.ShaderMaterial({
+			uniforms: pathTracingUniforms,
+			defines: pathTracingDefines,
+			vertexShader: pathTracingVertexShader,
+			fragmentShader: pathTracingFragmentShader,
+			depthTest: false,
+			depthWrite: false
+		});
 
-                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
-                pathTracingScene.add(pathTracingMesh);
+		pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+		pathTracingScene.add(pathTracingMesh);
 
-                // the following keeps the large scene ShaderMaterial quad right in front 
-                //   of the camera at all times. This is necessary because without it, the scene 
-                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
-                worldCamera.add(pathTracingMesh);
-                
-        });
+		// the following keeps the large scene ShaderMaterial quad right in front 
+		//   of the camera at all times. This is necessary because without it, the scene 
+		//   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+		worldCamera.add(pathTracingMesh);
+		
+	});
 
 } // end function createPathTracingMaterial()
 
@@ -83,8 +83,8 @@ function createPathTracingMaterial()
 // called automatically from within the animate() function
 function updateVariablesAndUniforms() 
 { 
-        // INFO
-        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 				
 } // end function updateVariablesAndUniforms()
 
