b8eae1cf5c7e43156dd1fed4b28d7b8e1f70b14a
Erich Loftis <erichlof@swbell.net>
2021-11-09 20:31:53 -0600

    Update BVH_Animated_Model_Fragment.glsl

--- shaders/BVH_Animated_Model_Fragment.glsl
+++ shaders/BVH_Animated_Model_Fragment.glsl
@@ -71,7 +71,7 @@ Box boxes[N_BOXES];
 vec3 perturbNormal(vec3 nl, vec2 normalScale, vec2 uv)
 {
 	
-        vec3 S = normalize( cross( abs(nl.y) < 0.9 ? vec3(0, 1, 0) : vec3(0, 0, 1), nl ) );
+        vec3 S = normalize( cross( abs(nl.y) < 0.9 ? vec3(0, 1, 0) : vec3(1, 0, 0), nl ) );
         vec3 T = cross(nl, S);
         vec3 N = normalize( nl );
 	// invert S, T when the UV direction is backwards (from mirrored faces),
