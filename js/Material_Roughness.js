5c62ecbfefcec5385fe75d0db3925f9374f92f47
Erich Loftis <erichlof@swbell.net>
2021-11-10 10:12:43 -0600

    Update Material_Roughness.js

--- js/Material_Roughness.js
+++ js/Material_Roughness.js
@@ -15,108 +15,108 @@ function init_GUI()
 {
 
 	material_TypeObject = {
-                Material_Preset: 'ClearCoat Diffuse'
-        };
-        material_ColorObject = {
-                Material_Color: [0, 255, 255]
-        };
-        
+		Material_Preset: 'ClearCoat Diffuse'
+	};
+	material_ColorObject = {
+		Material_Color: [0, 255, 255]
+	};
+	
 	function materialTypeChanger() 
-        {
-                changeMaterialType = true;
-        }
-        function materialColorChanger() 
-        {
-                changeMaterialColor = true;
-        }
-        gui = new dat.GUI();
-        
-        material_TypeController = gui.add( material_TypeObject, 'Material_Preset', [ 'ClearCoat Diffuse', 'Transparent Refractive', 
-                'Copper Metal', 'Aluminum Metal', 'Gold Metal', 'Silver Metal', 'ClearCoat Metal(Brass)' ] ).onChange( materialTypeChanger );
-        
-        material_ColorController = gui.addColor( material_ColorObject, 'Material_Color' ).onChange( materialColorChanger );
-        
+	{
+		changeMaterialType = true;
+	}
+	function materialColorChanger() 
+	{
+		changeMaterialColor = true;
+	}
+	gui = new dat.GUI();
+	
+	material_TypeController = gui.add( material_TypeObject, 'Material_Preset', [ 'ClearCoat Diffuse', 'Transparent Refractive', 
+		'Copper Metal', 'Aluminum Metal', 'Gold Metal', 'Silver Metal', 'ClearCoat Metal(Brass)' ] ).onChange( materialTypeChanger );
+	
+	material_ColorController = gui.addColor( material_ColorObject, 'Material_Color' ).onChange( materialColorChanger );
+	
 	materialTypeChanger();
-        materialColorChanger();
+	materialColorChanger();
 
-        gui.domElement.style.webkitUserSelect = "none";
-        gui.domElement.style.MozUserSelect = "none";
-        
-        window.addEventListener('resize', onWindowResize, false);
+	gui.domElement.style.webkitUserSelect = "none";
+	gui.domElement.style.MozUserSelect = "none";
+	
+	window.addEventListener('resize', onWindowResize, false);
 
-        if ( 'ontouchstart' in window ) 
-        {
-                mouseControl = false;
-                // if on mobile device, unpause the app because there is no ESC key and no mouse capture to do
-                isPaused = false;
-                
-                ableToEngagePointerLock = true;
+	if ( 'ontouchstart' in window ) 
+	{
+		mouseControl = false;
+		// if on mobile device, unpause the app because there is no ESC key and no mouse capture to do
+		isPaused = false;
+		
+		ableToEngagePointerLock = true;
 
-                mobileJoystickControls = new MobileJoystickControls ({
-                        //showJoystick: true
-                });	
-        }
+		mobileJoystickControls = new MobileJoystickControls ({
+			//showJoystick: true
+		});	
+	}
 
-        if (mouseControl) 
-        {
+	if (mouseControl) 
+	{
 
-                window.addEventListener( 'wheel', onMouseWheel, false );
+		window.addEventListener( 'wheel', onMouseWheel, false );
 
-                window.addEventListener("click", function(event) 
-                {
-                        event.preventDefault();	
-                }, false);
-                window.addEventListener("dblclick", function(event) 
-                {
-                        event.preventDefault();	
-                }, false);
-                
-                document.body.addEventListener("click", function(event) 
-                {
-                        if (!ableToEngagePointerLock)
-                                return;
-                        this.requestPointerLock = this.requestPointerLock || this.mozRequestPointerLock;
-                        this.requestPointerLock();
-                }, false);
+		window.addEventListener("click", function(event) 
+		{
+			event.preventDefault();	
+		}, false);
+		window.addEventListener("dblclick", function(event) 
+		{
+			event.preventDefault();	
+		}, false);
+		
+		document.body.addEventListener("click", function(event) 
+		{
+			if (!ableToEngagePointerLock)
+				return;
+			this.requestPointerLock = this.requestPointerLock || this.mozRequestPointerLock;
+			this.requestPointerLock();
+		}, false);
 
 
-                pointerlockChange = function (event)
-                {
-                        if (document.pointerLockElement === document.body ||
-                                document.mozPointerLockElement === document.body || document.webkitPointerLockElement === document.body)
-                        {
-                                document.addEventListener('keydown', onKeyDown, false);
-                                document.addEventListener('keyup', onKeyUp, false);
-                                isPaused = false;
-                        }
-                        else
-                        {
-                                document.removeEventListener('keydown', onKeyDown, false);
-                                document.removeEventListener('keyup', onKeyUp, false);
-                                isPaused = true;
-                        }
-                };
+		pointerlockChange = function (event)
+		{
+			if (document.pointerLockElement === document.body ||
+				document.mozPointerLockElement === document.body || document.webkitPointerLockElement === document.body)
+			{
+				document.addEventListener('keydown', onKeyDown, false);
+				document.addEventListener('keyup', onKeyUp, false);
+				isPaused = false;
+			}
+			else
+			{
+				document.removeEventListener('keydown', onKeyDown, false);
+				document.removeEventListener('keyup', onKeyUp, false);
+				isPaused = true;
+			}
+		};
 
-                // Hook pointer lock state change events
-                document.addEventListener( 'pointerlockchange', pointerlockChange, false );
-                document.addEventListener( 'mozpointerlockchange', pointerlockChange, false );
-                document.addEventListener( 'webkitpointerlockchange', pointerlockChange, false );
+		// Hook pointer lock state change events
+		document.addEventListener( 'pointerlockchange', pointerlockChange, false );
+		document.addEventListener( 'mozpointerlockchange', pointerlockChange, false );
+		document.addEventListener( 'webkitpointerlockchange', pointerlockChange, false );
 
-        }
+	}
 
-        if (mouseControl) 
-        {
-                gui.domElement.addEventListener("mouseenter", function(event) 
-                {
-                        ableToEngagePointerLock = false;	
-                }, false);
-                gui.domElement.addEventListener("mouseleave", function(event) 
-                {
-                        ableToEngagePointerLock = true;
-                }, false);
-        }
+	if (mouseControl) 
+	{
+		gui.domElement.addEventListener("mouseenter", function(event) 
+		{
+			ableToEngagePointerLock = false;	
+		}, false);
+		gui.domElement.addEventListener("mouseleave", function(event) 
+		{
+			ableToEngagePointerLock = true;
+		}, false);
+	}
 
-        initTHREEjs(); // boilerplate: init necessary three.js items and scene/demo-specific objects
+	initTHREEjs(); // boilerplate: init necessary three.js items and scene/demo-specific objects
 
 } // end function init_GUI()
 
@@ -125,22 +125,22 @@ function init_GUI()
 // called automatically from within initTHREEjs() function
 function initSceneData() 
 {
-        // scene/demo-specific three.js objects setup goes here
+	// scene/demo-specific three.js objects setup goes here
 
-        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
 
-        EPS_intersect = 0.01;
+	EPS_intersect = 0.01;
 
-        // set camera's field of view
-        worldCamera.fov = 60;
-        focusDistance = 130.0;
+	// set camera's field of view
+	worldCamera.fov = 60;
+	focusDistance = 130.0;
 
-        // position and orient camera
-        cameraControlsObject.position.set(0, 20, 120);
-        ///cameraControlsYawObject.rotation.y = 0.0;
-        // look slightly downward
-        ///cameraControlsPitchObject.rotation.x = -0.4;
+	// position and orient camera
+	cameraControlsObject.position.set(0, 20, 120);
+	///cameraControlsYawObject.rotation.y = 0.0;
+	// look slightly downward
+	///cameraControlsPitchObject.rotation.x = -0.4;
 
 } // end function initSceneData()
 
@@ -150,22 +150,22 @@ function initSceneData()
 function initPathTracingShaders() 
 {
  
-        // scene/demo-specific uniforms go here
-        pathTracingUniforms.uMaterialType = { type: "i", value: 4 };
-        pathTracingUniforms.uMaterialColor = { type: "v3", value: new THREE.Color(0.0, 1.0, 1.0) };
-        
+	// scene/demo-specific uniforms go here
+	pathTracingUniforms.uMaterialType = { type: "i", value: 4 };
+	pathTracingUniforms.uMaterialColor = { type: "v3", value: new THREE.Color(0.0, 1.0, 1.0) };
+	
 
-        pathTracingDefines = {
-        	//NUMBER_OF_TRIANGLES: total_number_of_triangles
-        };
+	pathTracingDefines = {
+		//NUMBER_OF_TRIANGLES: total_number_of_triangles
+	};
 
-        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
-        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) 
-        {
-                pathTracingVertexShader = shaderText;
+	// load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+	fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText) 
+	{
+		pathTracingVertexShader = shaderText;
 
-                createPathTracingMaterial();
-        });
+		createPathTracingMaterial();
+	});
 
 } // end function initPathTracingShaders()
 
@@ -174,29 +174,29 @@ function initPathTracingShaders()
 function createPathTracingMaterial() 
 {
 
-        fileLoader.load('shaders/Material_Roughness_Fragment.glsl', function (shaderText) 
-        {
-                
-                pathTracingFragmentShader = shaderText;
+	fileLoader.load('shaders/Material_Roughness_Fragment.glsl', function (shaderText) 
+	{
+		
+		pathTracingFragmentShader = shaderText;
 
-                pathTracingMaterial = new THREE.ShaderMaterial({
-                        uniforms: pathTracingUniforms,
-                        defines: pathTracingDefines,
-                        vertexShader: pathTracingVertexShader,
-                        fragmentShader: pathTracingFragmentShader,
-                        depthTest: false,
-                        depthWrite: false
-                });
+		pathTracingMaterial = new THREE.ShaderMaterial({
+			uniforms: pathTracingUniforms,
+			defines: pathTracingDefines,
+			vertexShader: pathTracingVertexShader,
+			fragmentShader: pathTracingFragmentShader,
+			depthTest: false,
+			depthWrite: false
+		});
 
-                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
-                pathTracingScene.add(pathTracingMesh);
+		pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+		pathTracingScene.add(pathTracingMesh);
 
-                // the following keeps the large scene ShaderMaterial quad right in front 
-                //   of the camera at all times. This is necessary because without it, the scene 
-                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
-                worldCamera.add(pathTracingMesh);
-                
-        });
+		// the following keeps the large scene ShaderMaterial quad right in front 
+		//   of the camera at all times. This is necessary because without it, the scene 
+		//   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+		worldCamera.add(pathTracingMesh);
+		
+	});
 
 } // end function createPathTracingMaterial()
 
@@ -206,65 +206,65 @@ function createPathTracingMaterial()
 function updateVariablesAndUniforms() 
 {
 
-        if (changeMaterialType) {
+	if (changeMaterialType) {
 
-                if (material_TypeController.getValue() == 'ClearCoat Diffuse') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 4;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(0.0, 1.0, 1.0);
-                }
-                else if (material_TypeController.getValue() == 'Transparent Refractive') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 2;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(0.1, 1.0, 0.6);
-                }
-                else if (material_TypeController.getValue() == 'Copper Metal') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 3;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(0.955008, 0.637427, 0.538163);
-                }
-                else if (material_TypeController.getValue() == 'Aluminum Metal') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 3;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(0.913183, 0.921494, 0.924524);
-                }
-                else if (material_TypeController.getValue() == 'Gold Metal') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 3;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(1.000000, 0.765557, 0.336057);
-                }
-                else if (material_TypeController.getValue() == 'Silver Metal') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 3;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(0.971519, 0.959915, 0.915324);
-                }
-                else if (material_TypeController.getValue() == 'ClearCoat Metal(Brass)') 
-                {
-                        pathTracingUniforms.uMaterialType.value = 18;
-                        pathTracingUniforms.uMaterialColor.value.setRGB(0.956863, 0.894118, 0.678431);
-                }
-                        
-                
-                material_ColorController.setValue([ pathTracingUniforms.uMaterialColor.value.r * 255,
-                                                    pathTracingUniforms.uMaterialColor.value.g * 255,
-                                                    pathTracingUniforms.uMaterialColor.value.b * 255 ]);
-                
-                cameraIsMoving = true;
-                changeMaterialType = false;
-        }
+		if (material_TypeController.getValue() == 'ClearCoat Diffuse') 
+		{
+			pathTracingUniforms.uMaterialType.value = 4;
+			pathTracingUniforms.uMaterialColor.value.setRGB(0.0, 1.0, 1.0);
+		}
+		else if (material_TypeController.getValue() == 'Transparent Refractive') 
+		{
+			pathTracingUniforms.uMaterialType.value = 2;
+			pathTracingUniforms.uMaterialColor.value.setRGB(0.1, 1.0, 0.6);
+		}
+		else if (material_TypeController.getValue() == 'Copper Metal') 
+		{
+			pathTracingUniforms.uMaterialType.value = 3;
+			pathTracingUniforms.uMaterialColor.value.setRGB(0.955008, 0.637427, 0.538163);
+		}
+		else if (material_TypeController.getValue() == 'Aluminum Metal') 
+		{
+			pathTracingUniforms.uMaterialType.value = 3;
+			pathTracingUniforms.uMaterialColor.value.setRGB(0.913183, 0.921494, 0.924524);
+		}
+		else if (material_TypeController.getValue() == 'Gold Metal') 
+		{
+			pathTracingUniforms.uMaterialType.value = 3;
+			pathTracingUniforms.uMaterialColor.value.setRGB(1.000000, 0.765557, 0.336057);
+		}
+		else if (material_TypeController.getValue() == 'Silver Metal') 
+		{
+			pathTracingUniforms.uMaterialType.value = 3;
+			pathTracingUniforms.uMaterialColor.value.setRGB(0.971519, 0.959915, 0.915324);
+		}
+		else if (material_TypeController.getValue() == 'ClearCoat Metal(Brass)') 
+		{
+			pathTracingUniforms.uMaterialType.value = 18;
+			pathTracingUniforms.uMaterialColor.value.setRGB(0.956863, 0.894118, 0.678431);
+		}
+			
+		
+		material_ColorController.setValue([ pathTracingUniforms.uMaterialColor.value.r * 255,
+						    pathTracingUniforms.uMaterialColor.value.g * 255,
+						    pathTracingUniforms.uMaterialColor.value.b * 255 ]);
+		
+		cameraIsMoving = true;
+		changeMaterialType = false;
+	}
 
-        if (changeMaterialColor) 
-        {
-                pathTracingUniforms.uMaterialColor.value.setRGB( material_ColorController.getValue()[0] / 255, 
-                                                                 material_ColorController.getValue()[1] / 255, 
-                                                                 material_ColorController.getValue()[2] / 255 );
-                
-                cameraIsMoving = true;
-                changeMaterialColor = false;
-        }
+	if (changeMaterialColor) 
+	{
+		pathTracingUniforms.uMaterialColor.value.setRGB( material_ColorController.getValue()[0] / 255, 
+								 material_ColorController.getValue()[1] / 255, 
+								 material_ColorController.getValue()[2] / 255 );
+		
+		cameraIsMoving = true;
+		changeMaterialColor = false;
+	}
 
-        // INFO
-        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
 } // end function updateUniforms()
 
