6d4f40bf778e37dca0bf4941a96e84f16aee73fc
Erich Loftis <erichlof@swbell.net>
2021-12-27 01:48:58 -0600

    Update HDRI_Environment_Fragment.glsl

--- shaders/HDRI_Environment_Fragment.glsl
+++ shaders/HDRI_Environment_Fragment.glsl
@@ -292,9 +292,8 @@ vec3 Get_HDR_Color(vec3 rDirection)
 	///sampleUV.x = (1.0 + atan(rDirection.x, -rDirection.z) * ONE_OVER_PI) * 0.5;
 	sampleUV.x = atan(rDirection.x, -rDirection.z) * ONE_OVER_TWO_PI + 0.5;
   	sampleUV.y = acos(rDirection.y) * ONE_OVER_PI;
-	vec4 texData = texture(tHDRTexture, sampleUV);
+	vec3 texColor = texture(tHDRTexture, sampleUV).rgb;
 
-	vec3 texColor = vec3(RGBEToLinear(texData));
 	// texColor = texData.a > 0.57 ? vec3(100) : vec3(0);
 	// return texColor;
 	return texColor * uHDRI_Exposure;
