0f6ec01fab61c2ed24fd79bf2a1e84e17698ee4c
Erich Loftis <erichlof@swbell.net>
2022-02-18 23:16:33 -0600

    Update Arctic_Circle.js

--- js/Arctic_Circle.js
+++ js/Arctic_Circle.js
@@ -1,6 +1,4 @@
 // scene/demo-specific variables go here
-var sceneIsDynamic = true;
-var camFlightSpeed = 1000;
 var sunAngle = 0;
 var sunDirection = new THREE.Vector3();
 var waterLevel = 0.0;
@@ -9,29 +7,31 @@ var cameraUnderWater = false;
 // called automatically from within initTHREEjs() function
 function initSceneData()
 {
-        // scene/demo-specific three.js objects setup goes here
+	// scene/demo-specific three.js objects setup goes here
+	sceneIsDynamic = true;
+	cameraFlightSpeed = 1000;
 
-        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
 
-        EPS_intersect = 1.0;
+	EPS_intersect = 1.0;
 
-        // set camera's field of view
-        worldCamera.fov = 60;
-        focusDistance = 2000.0;
+	// set camera's field of view
+	worldCamera.fov = 60;
+	focusDistance = 2000.0;
 
-        // position and orient camera
-        cameraControlsObject.position.set(-7134, 1979, -4422);
-        cameraControlsYawObject.rotation.y = 3.0;
-        cameraControlsPitchObject.rotation.x = 0.0;
+	// position and orient camera
+	cameraControlsObject.position.set(-7134, 1979, -4422);
+	cameraControlsYawObject.rotation.y = 3.0;
+	cameraControlsPitchObject.rotation.x = 0.0;
 
-        PerlinNoiseTexture = new THREE.TextureLoader().load('textures/perlin256.png');
-        PerlinNoiseTexture.wrapS = THREE.RepeatWrapping;
-        PerlinNoiseTexture.wrapT = THREE.RepeatWrapping;
-        PerlinNoiseTexture.flipY = false;
-        PerlinNoiseTexture.minFilter = THREE.LinearFilter;
-        PerlinNoiseTexture.magFilter = THREE.LinearFilter;
-        PerlinNoiseTexture.generateMipmaps = false;
+	PerlinNoiseTexture = new THREE.TextureLoader().load('textures/perlin256.png');
+	PerlinNoiseTexture.wrapS = THREE.RepeatWrapping;
+	PerlinNoiseTexture.wrapT = THREE.RepeatWrapping;
+	PerlinNoiseTexture.flipY = false;
+	PerlinNoiseTexture.minFilter = THREE.LinearFilter;
+	PerlinNoiseTexture.magFilter = THREE.LinearFilter;
+	PerlinNoiseTexture.generateMipmaps = false;
 
 } // end function initSceneData()
 
@@ -41,22 +41,22 @@ function initSceneData()
 function initPathTracingShaders()
 {
 
-        // scene/demo-specific uniforms go here
-        pathTracingUniforms.t_PerlinNoise = { type: "t", value: PerlinNoiseTexture };       
-        pathTracingUniforms.uWaterLevel = { type: "f", value: 0.0 };
-        pathTracingUniforms.uSunDirection = { type: "v3", value: new THREE.Vector3() };
+	// scene/demo-specific uniforms go here
+	pathTracingUniforms.t_PerlinNoise = { type: "t", value: PerlinNoiseTexture };       
+	pathTracingUniforms.uWaterLevel = { type: "f", value: 0.0 };
+	pathTracingUniforms.uSunDirection = { type: "v3", value: new THREE.Vector3() };
 
-        pathTracingDefines = {
-                //NUMBER_OF_TRIANGLES: total_number_of_triangles
-        };
+	pathTracingDefines = {
+		//NUMBER_OF_TRIANGLES: total_number_of_triangles
+	};
 
-        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
-        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText)
-        {
-                pathTracingVertexShader = shaderText;
+	// load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+	fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText)
+	{
+		pathTracingVertexShader = shaderText;
 
-                createPathTracingMaterial();
-        });
+		createPathTracingMaterial();
+	});
 
 } // end function initPathTracingShaders()
 
@@ -65,29 +65,29 @@ function initPathTracingShaders()
 function createPathTracingMaterial()
 {
 
-        fileLoader.load('shaders/Arctic_Circle_Fragment.glsl', function (shaderText)
-        {
+	fileLoader.load('shaders/Arctic_Circle_Fragment.glsl', function (shaderText)
+	{
 
-                pathTracingFragmentShader = shaderText;
+		pathTracingFragmentShader = shaderText;
 
-                pathTracingMaterial = new THREE.ShaderMaterial({
-                        uniforms: pathTracingUniforms,
-                        defines: pathTracingDefines,
-                        vertexShader: pathTracingVertexShader,
-                        fragmentShader: pathTracingFragmentShader,
-                        depthTest: false,
-                        depthWrite: false
-                });
+		pathTracingMaterial = new THREE.ShaderMaterial({
+			uniforms: pathTracingUniforms,
+			defines: pathTracingDefines,
+			vertexShader: pathTracingVertexShader,
+			fragmentShader: pathTracingFragmentShader,
+			depthTest: false,
+			depthWrite: false
+		});
 
-                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
-                pathTracingScene.add(pathTracingMesh);
+		pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+		pathTracingScene.add(pathTracingMesh);
 
-                // the following keeps the large scene ShaderMaterial quad right in front 
-                //   of the camera at all times. This is necessary because without it, the scene 
-                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
-                worldCamera.add(pathTracingMesh);
+		// the following keeps the large scene ShaderMaterial quad right in front 
+		//   of the camera at all times. This is necessary because without it, the scene 
+		//   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+		worldCamera.add(pathTracingMesh);
 
-        });
+	});
 
 } // end function createPathTracingMaterial()
 
@@ -97,21 +97,21 @@ function createPathTracingMaterial()
 function updateVariablesAndUniforms()
 {
 
-        // scene/demo-specific variables
-        if (cameraControlsObject.position.y < 0.0)
-                cameraUnderWater = true;
-        else cameraUnderWater = false;
+	// scene/demo-specific variables
+	if (cameraControlsObject.position.y < 0.0)
+		cameraUnderWater = true;
+	else cameraUnderWater = false;
 
-        sunAngle = ((elapsedTime * 0.04) + 0.5) % TWO_PI;
-        sunDirection.set(Math.cos(sunAngle), Math.cos(sunAngle) * 0.2 + 0.2, Math.sin(sunAngle));
-        sunDirection.normalize();
+	sunAngle = ((elapsedTime * 0.04) + 0.5) % TWO_PI;
+	sunDirection.set(Math.cos(sunAngle), Math.cos(sunAngle) * 0.2 + 0.2, Math.sin(sunAngle));
+	sunDirection.normalize();
 
-        // scene/demo-specific uniforms
-        pathTracingUniforms.uWaterLevel.value = waterLevel;
-        pathTracingUniforms.uSunDirection.value.copy(sunDirection);
+	// scene/demo-specific uniforms
+	pathTracingUniforms.uWaterLevel.value = waterLevel;
+	pathTracingUniforms.uSunDirection.value.copy(sunDirection);
 
-        // INFO
-        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
 } // end function updateUniforms()
 
