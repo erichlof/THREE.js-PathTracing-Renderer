9cfd2bec9b8c2a585e5ee2fbe49a13894cd92cce
Erich Loftis <erichlof@swbell.net>
2022-02-18 23:23:10 -0600

    Update Bi-Directional_PathTracing.js

--- js/Bi-Directional_PathTracing.js
+++ js/Bi-Directional_PathTracing.js
@@ -1,27 +1,28 @@
 // scene/demo-specific variables go here
-var sceneIsDynamic = false;
-var camFlightSpeed = 300;
+
 
 // called automatically from within initTHREEjs() function
 function initSceneData()
 {
-        // scene/demo-specific three.js objects setup goes here
+	// scene/demo-specific three.js objects setup goes here
+	sceneIsDynamic = false;
+	cameraFlightSpeed = 300;
 
-        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
 
-        EPS_intersect = 0.01;
+	EPS_intersect = 0.01;
 
-        // set camera's field of view
-        worldCamera.fov = 55;
-        focusDistance = 622.0;
+	// set camera's field of view
+	worldCamera.fov = 55;
+	focusDistance = 622.0;
 
-        // position and orient camera
-        cameraControlsObject.position.set(380, 350, 180);
-        // look slightly to the left
-        cameraControlsYawObject.rotation.y = 0.2;
-        // look slightly upward
-        cameraControlsPitchObject.rotation.x = -0.25;
+	// position and orient camera
+	cameraControlsObject.position.set(380, 350, 180);
+	// look slightly to the left
+	cameraControlsYawObject.rotation.y = 0.2;
+	// look slightly upward
+	cameraControlsPitchObject.rotation.x = -0.25;
 
 } // end function initSceneData()
 
@@ -30,20 +31,20 @@ function initSceneData()
 // called automatically from within initTHREEjs() function
 function initPathTracingShaders()
 {
-        // scene/demo-specific uniforms go here
-        
+	// scene/demo-specific uniforms go here
+	
 
-        pathTracingDefines = {
-                //NUMBER_OF_TRIANGLES: total_number_of_triangles
-        };
+	pathTracingDefines = {
+		//NUMBER_OF_TRIANGLES: total_number_of_triangles
+	};
 
-        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
-        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText)
-        {
-                pathTracingVertexShader = shaderText;
+	// load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+	fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText)
+	{
+		pathTracingVertexShader = shaderText;
 
-                createPathTracingMaterial();
-        });
+		createPathTracingMaterial();
+	});
 
 } // end function initPathTracingShaders()
 
@@ -52,29 +53,29 @@ function initPathTracingShaders()
 function createPathTracingMaterial()
 {
 
-        fileLoader.load('shaders/Bi-Directional_PathTracing_Fragment.glsl', function (shaderText)
-        {
+	fileLoader.load('shaders/Bi-Directional_PathTracing_Fragment.glsl', function (shaderText)
+	{
 
-                pathTracingFragmentShader = shaderText;
+		pathTracingFragmentShader = shaderText;
 
-                pathTracingMaterial = new THREE.ShaderMaterial({
-                        uniforms: pathTracingUniforms,
-                        defines: pathTracingDefines,
-                        vertexShader: pathTracingVertexShader,
-                        fragmentShader: pathTracingFragmentShader,
-                        depthTest: false,
-                        depthWrite: false
-                });
+		pathTracingMaterial = new THREE.ShaderMaterial({
+			uniforms: pathTracingUniforms,
+			defines: pathTracingDefines,
+			vertexShader: pathTracingVertexShader,
+			fragmentShader: pathTracingFragmentShader,
+			depthTest: false,
+			depthWrite: false
+		});
 
-                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
-                pathTracingScene.add(pathTracingMesh);
+		pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+		pathTracingScene.add(pathTracingMesh);
 
-                // the following keeps the large scene ShaderMaterial quad right in front 
-                //   of the camera at all times. This is necessary because without it, the scene 
-                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
-                worldCamera.add(pathTracingMesh);
+		// the following keeps the large scene ShaderMaterial quad right in front 
+		//   of the camera at all times. This is necessary because without it, the scene 
+		//   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+		worldCamera.add(pathTracingMesh);
 
-        });
+	});
 
 } // end function createPathTracingMaterial()
 
@@ -83,9 +84,9 @@ function createPathTracingMaterial()
 // called automatically from within the animate() function
 function updateVariablesAndUniforms()
 {
-        
-        // INFO
-        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+	
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
 } // end function updateUniforms()
 
