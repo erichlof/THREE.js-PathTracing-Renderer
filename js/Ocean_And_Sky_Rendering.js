0f70a3f58284cadb0ac81a99160d80360e0e4e9e
Erich Loftis <erichlof@swbell.net>
2022-02-18 23:43:43 -0600

    Update Ocean_And_Sky_Rendering.js

--- js/Ocean_And_Sky_Rendering.js
+++ js/Ocean_And_Sky_Rendering.js
@@ -1,6 +1,4 @@
 // scene/demo-specific variables go here
-var sceneIsDynamic = true;
-var camFlightSpeed = 300;
 var sunAngle = 0;
 var sunDirection = new THREE.Vector3();
 var tallBoxGeometry, tallBoxMaterial, tallBoxMesh;
@@ -9,62 +7,64 @@ var shortBoxGeometry, shortBoxMaterial, shortBoxMesh;
 // called automatically from within initTHREEjs() function
 function initSceneData()
 {
-        // scene/demo-specific three.js objects setup goes here
+	// scene/demo-specific three.js objects setup goes here
+	sceneIsDynamic = true;
+	cameraFlightSpeed = 300;
 
-        // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-        pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
 
-        EPS_intersect = 0.01;
+	EPS_intersect = 0.01;
 
-        // Boxes
-        tallBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-        tallBoxMaterial = new THREE.MeshPhysicalMaterial({
-                color: new THREE.Color(0.95, 0.95, 0.95), //RGB, ranging from 0.0 - 1.0
-                roughness: 1.0 // ideal Diffuse material	
-        });
+	// Boxes
+	tallBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	tallBoxMaterial = new THREE.MeshPhysicalMaterial({
+		color: new THREE.Color(0.95, 0.95, 0.95), //RGB, ranging from 0.0 - 1.0
+		roughness: 1.0 // ideal Diffuse material	
+	});
 
-        tallBoxMesh = new THREE.Mesh(tallBoxGeometry, tallBoxMaterial);
-        pathTracingScene.add(tallBoxMesh);
-        tallBoxMesh.visible = false; // disable normal Three.js rendering updates of this object: 
-        // it is just a data placeholder as well as an Object3D that can be transformed/manipulated by 
-        // using familiar Three.js library commands. It is then fed into the GPU path tracing renderer
-        // through its 'matrixWorld' matrix. See below:
-        tallBoxMesh.rotation.set(0, Math.PI * 0.1, 0);
-        tallBoxMesh.position.set(180, 170, -350);
-        tallBoxMesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
-
-
-        shortBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-        shortBoxMaterial = new THREE.MeshPhysicalMaterial({
-                color: new THREE.Color(0.95, 0.95, 0.95), //RGB, ranging from 0.0 - 1.0
-                roughness: 1.0 // ideal Diffuse material	
-        });
-
-        shortBoxMesh = new THREE.Mesh(shortBoxGeometry, shortBoxMaterial);
-        pathTracingScene.add(shortBoxMesh);
-        shortBoxMesh.visible = false;
-        shortBoxMesh.rotation.set(0, -Math.PI * 0.09, 0);
-        shortBoxMesh.position.set(370, 85, -170);
-        shortBoxMesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
+	tallBoxMesh = new THREE.Mesh(tallBoxGeometry, tallBoxMaterial);
+	pathTracingScene.add(tallBoxMesh);
+	tallBoxMesh.visible = false; // disable normal Three.js rendering updates of this object: 
+	// it is just a data placeholder as well as an Object3D that can be transformed/manipulated by 
+	// using familiar Three.js library commands. It is then fed into the GPU path tracing renderer
+	// through its 'matrixWorld' matrix. See below:
+	tallBoxMesh.rotation.set(0, Math.PI * 0.1, 0);
+	tallBoxMesh.position.set(180, 170, -350);
+	tallBoxMesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
 
 
-        // set camera's field of view
-        worldCamera.fov = 60;
-        focusDistance = 1180.0;
+	shortBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	shortBoxMaterial = new THREE.MeshPhysicalMaterial({
+		color: new THREE.Color(0.95, 0.95, 0.95), //RGB, ranging from 0.0 - 1.0
+		roughness: 1.0 // ideal Diffuse material	
+	});
 
-        // position and orient camera
-        cameraControlsObject.position.set(278, 270, 1050);
-        ///cameraControlsYawObject.rotation.y = 0.0;
-        // look slightly upward
-        cameraControlsPitchObject.rotation.x = 0.005;
+	shortBoxMesh = new THREE.Mesh(shortBoxGeometry, shortBoxMaterial);
+	pathTracingScene.add(shortBoxMesh);
+	shortBoxMesh.visible = false;
+	shortBoxMesh.rotation.set(0, -Math.PI * 0.09, 0);
+	shortBoxMesh.position.set(370, 85, -170);
+	shortBoxMesh.updateMatrixWorld(true); // 'true' forces immediate matrix update
 
-        PerlinNoiseTexture = new THREE.TextureLoader().load('textures/perlin256.png');
-        PerlinNoiseTexture.wrapS = THREE.RepeatWrapping;
-        PerlinNoiseTexture.wrapT = THREE.RepeatWrapping;
-        PerlinNoiseTexture.flipY = false;
-        PerlinNoiseTexture.minFilter = THREE.LinearFilter;
-        PerlinNoiseTexture.magFilter = THREE.LinearFilter;
-        PerlinNoiseTexture.generateMipmaps = false;
+
+	// set camera's field of view
+	worldCamera.fov = 60;
+	focusDistance = 1180.0;
+
+	// position and orient camera
+	cameraControlsObject.position.set(278, 270, 1050);
+	///cameraControlsYawObject.rotation.y = 0.0;
+	// look slightly upward
+	cameraControlsPitchObject.rotation.x = 0.005;
+
+	PerlinNoiseTexture = new THREE.TextureLoader().load('textures/perlin256.png');
+	PerlinNoiseTexture.wrapS = THREE.RepeatWrapping;
+	PerlinNoiseTexture.wrapT = THREE.RepeatWrapping;
+	PerlinNoiseTexture.flipY = false;
+	PerlinNoiseTexture.minFilter = THREE.LinearFilter;
+	PerlinNoiseTexture.magFilter = THREE.LinearFilter;
+	PerlinNoiseTexture.generateMipmaps = false;
 
 } // end function initSceneData()
 
@@ -74,25 +74,25 @@ function initSceneData()
 function initPathTracingShaders()
 {
 
-        // scene/demo-specific uniforms go here
-        pathTracingUniforms.t_PerlinNoise = { type: "t", value: PerlinNoiseTexture };
-        pathTracingUniforms.uCameraUnderWater = { type: "f", value: 0.0 };
-        pathTracingUniforms.uSunDirection = { type: "v3", value: new THREE.Vector3() };
-        pathTracingUniforms.uTallBoxInvMatrix = { type: "m4", value: new THREE.Matrix4() };
-        pathTracingUniforms.uShortBoxInvMatrix = { type: "m4", value: new THREE.Matrix4() };
+	// scene/demo-specific uniforms go here
+	pathTracingUniforms.t_PerlinNoise = { type: "t", value: PerlinNoiseTexture };
+	pathTracingUniforms.uCameraUnderWater = { type: "f", value: 0.0 };
+	pathTracingUniforms.uSunDirection = { type: "v3", value: new THREE.Vector3() };
+	pathTracingUniforms.uTallBoxInvMatrix = { type: "m4", value: new THREE.Matrix4() };
+	pathTracingUniforms.uShortBoxInvMatrix = { type: "m4", value: new THREE.Matrix4() };
 
 
-        pathTracingDefines = {
-                //NUMBER_OF_TRIANGLES: total_number_of_triangles
-        };
+	pathTracingDefines = {
+		//NUMBER_OF_TRIANGLES: total_number_of_triangles
+	};
 
-        // load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
-        fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText)
-        {
-                pathTracingVertexShader = shaderText;
+	// load vertex and fragment shader files that are used in the pathTracing material, mesh and scene
+	fileLoader.load('shaders/common_PathTracing_Vertex.glsl', function (shaderText)
+	{
+		pathTracingVertexShader = shaderText;
 
-                createPathTracingMaterial();
-        });
+		createPathTracingMaterial();
+	});
 
 } // end function initPathTracingShaders()
 
@@ -101,29 +101,29 @@ function initPathTracingShaders()
 function createPathTracingMaterial()
 {
 
-        fileLoader.load('shaders/Ocean_And_Sky_Rendering_Fragment.glsl', function (shaderText)
-        {
+	fileLoader.load('shaders/Ocean_And_Sky_Rendering_Fragment.glsl', function (shaderText)
+	{
 
-                pathTracingFragmentShader = shaderText;
+		pathTracingFragmentShader = shaderText;
 
-                pathTracingMaterial = new THREE.ShaderMaterial({
-                        uniforms: pathTracingUniforms,
-                        defines: pathTracingDefines,
-                        vertexShader: pathTracingVertexShader,
-                        fragmentShader: pathTracingFragmentShader,
-                        depthTest: false,
-                        depthWrite: false
-                });
+		pathTracingMaterial = new THREE.ShaderMaterial({
+			uniforms: pathTracingUniforms,
+			defines: pathTracingDefines,
+			vertexShader: pathTracingVertexShader,
+			fragmentShader: pathTracingFragmentShader,
+			depthTest: false,
+			depthWrite: false
+		});
 
-                pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
-                pathTracingScene.add(pathTracingMesh);
+		pathTracingMesh = new THREE.Mesh(pathTracingGeometry, pathTracingMaterial);
+		pathTracingScene.add(pathTracingMesh);
 
-                // the following keeps the large scene ShaderMaterial quad right in front 
-                //   of the camera at all times. This is necessary because without it, the scene 
-                //   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
-                worldCamera.add(pathTracingMesh);
+		// the following keeps the large scene ShaderMaterial quad right in front 
+		//   of the camera at all times. This is necessary because without it, the scene 
+		//   quad will fall out of view and get clipped when the camera rotates past 180 degrees.
+		worldCamera.add(pathTracingMesh);
 
-        });
+	});
 
 } // end function createPathTracingMaterial()
 
@@ -133,26 +133,26 @@ function createPathTracingMaterial()
 function updateVariablesAndUniforms()
 {
 
-        // scene/demo-specific variables
-        sunAngle = (elapsedTime * 0.035) % (Math.PI + 0.2) - 0.11;
-        sunDirection.set(Math.cos(sunAngle), Math.sin(sunAngle), -Math.cos(sunAngle) * 2.0);
-        sunDirection.normalize();
+	// scene/demo-specific variables
+	sunAngle = (elapsedTime * 0.035) % (Math.PI + 0.2) - 0.11;
+	sunDirection.set(Math.cos(sunAngle), Math.sin(sunAngle), -Math.cos(sunAngle) * 2.0);
+	sunDirection.normalize();
 
-        // scene/demo-specific uniforms
-        pathTracingUniforms.uSunDirection.value.copy(sunDirection);
+	// scene/demo-specific uniforms
+	pathTracingUniforms.uSunDirection.value.copy(sunDirection);
 
-        // BOXES
-        pathTracingUniforms.uTallBoxInvMatrix.value.copy(tallBoxMesh.matrixWorld).invert();
-        pathTracingUniforms.uShortBoxInvMatrix.value.copy(shortBoxMesh.matrixWorld).invert();
+	// BOXES
+	pathTracingUniforms.uTallBoxInvMatrix.value.copy(tallBoxMesh.matrixWorld).invert();
+	pathTracingUniforms.uShortBoxInvMatrix.value.copy(shortBoxMesh.matrixWorld).invert();
 
-        // CAMERA
-        if (cameraControlsObject.position.y < 2.0)
-                pathTracingUniforms.uCameraUnderWater.value = 1.0;
-        else
-                pathTracingUniforms.uCameraUnderWater.value = 0.0;
+	// CAMERA
+	if (cameraControlsObject.position.y < 2.0)
+		pathTracingUniforms.uCameraUnderWater.value = 1.0;
+	else
+		pathTracingUniforms.uCameraUnderWater.value = 0.0;
 
-        // INFO
-        cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
 
 } // end function updateUniforms()
 
