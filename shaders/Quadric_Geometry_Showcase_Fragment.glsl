4dd08efccffc7bd7192ece10fac6b0b88541155d
Erich Loftis <erichlof@swbell.net>
2022-02-24 16:36:57 -0600

    Update Quadric_Geometry_Showcase_Fragment.glsl

--- shaders/Quadric_Geometry_Showcase_Fragment.glsl
+++ shaders/Quadric_Geometry_Showcase_Fragment.glsl
@@ -399,7 +399,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 
-			if (rand() < P)
+			if (diffuseCount == 0 && rand() < P)
 			{
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
@@ -444,7 +444,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 			
-			if (rand() < P)
+			if (diffuseCount == 0 && rand() < P)
 			{
 				if (bounces == 0)
 					pixelSharpness = uFrameCounter > 200.0 ? 1.01 : -1.0;
