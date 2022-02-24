aa314185442175020c1ff747798bc226cfff0924
Erich Loftis <erichlof@swbell.net>
2022-02-24 16:29:42 -0600

    Update CSG_Museum_4_Fragment.glsl

--- shaders/CSG_Museum_4_Fragment.glsl
+++ shaders/CSG_Museum_4_Fragment.glsl
@@ -1092,7 +1092,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 			
-			if (rand() < P)
+			if (diffuseCount == 0 && rand() < P)
 			{
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
@@ -1147,9 +1147,9 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 			
-			if (rand() < P)
+			if (diffuseCount == 0 && rand() < P)
 			{
-				// if (diffuseCount == 0)
+				// if (bounces == 0)
 				// 	pixelSharpness = uFrameCounter > 200.0 ? 1.01 : -1.0;
 
 				mask *= RP;
