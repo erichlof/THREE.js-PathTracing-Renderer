4a684ca9d83c6dde9d32f101c20e0b4d0185aa15
Erich Loftis <erichlof@swbell.net>
2022-02-24 16:37:37 -0600

    Update Ray_Warping_Fragment.glsl

--- shaders/Ray_Warping_Fragment.glsl
+++ shaders/Ray_Warping_Fragment.glsl
@@ -341,7 +341,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 			
-			if (rand() < P)
+			if (bounces == 0 && rand() < P)
 			{
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
@@ -385,7 +385,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 
-			if (rand() < P)
+			if (diffuseCount == 0 && rand() < P)
 			{
 				if (bounces == 0)
 					pixelSharpness = uFrameCounter > 200.0 ? 1.01 : -1.0;
