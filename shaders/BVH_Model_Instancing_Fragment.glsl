c606f05cfe2135b8337a31595e3b329807f6a7ef
Erich Loftis <erichlof@swbell.net>
2022-02-24 16:24:44 -0600

    Update BVH_Model_Instancing_Fragment.glsl

--- shaders/BVH_Model_Instancing_Fragment.glsl
+++ shaders/BVH_Model_Instancing_Fragment.glsl
@@ -632,7 +632,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 			
-			if (rand() < P)
+			if (bounces == 0 && rand() < P)
 			{
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
@@ -675,7 +675,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
                 	TP = Tr / (1.0 - P);
 
 			
-			if (rand() < P)
+			if (bounces == 0 && rand() < P)
 			{	
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
