5d74f4cc56f57b241ad1218eb993994fa242557f
Erich Loftis <erichlof@swbell.net>
2022-02-25 14:44:01 -0600

    Update ScreenOutput_Fragment.glsl

--- shaders/ScreenOutput_Fragment.glsl
+++ shaders/ScreenOutput_Fragment.glsl
@@ -262,18 +262,19 @@ void main()
 	} // end if (centerPixel.a > 0.0)
 
 
-	// gets rid of jagged edges between surface areas of differing colors,
-	// and gives crisper edges/boundries to area light sources (spheres, quads, etc.)
-	if (centerPixel.a == 1.01)
+	// gives sharper edges between surface areas of differing colors,
+	// and to edges/boundries of area light sources (spheres, quads, etc.)
+	if (centerPixel.a == 1.01) 
 	{
-		filteredPixelColor = uSceneIsDynamic ? mix(centerPixel.rgb, filteredPixelColor, 0.5) : 
-					mix(centerPixel.rgb, filteredPixelColor, uOneOverSampleCounter);
+		filteredPixelColor = mix(centerPixel.rgb, filteredPixelColor, uOneOverSampleCounter);
+
+		filteredPixelColor = uSceneIsDynamic ? mix(centerPixel.rgb, filteredPixelColor, 0.75) : filteredPixelColor;
+
+		filteredPixelColor = (uOneOverSampleCounter < 0.01) ? centerPixel.rgb : filteredPixelColor;
 	}
 
-	if ((centerPixel.a == 1.01 && uOneOverSampleCounter < 0.005) || uOneOverSampleCounter < 0.0002)
-	{
-		filteredPixelColor = centerPixel.rgb;
-	}
+	// after 2000 samples, we can safely disable all noise filtering / blurring
+	filteredPixelColor = (uOneOverSampleCounter < 0.0005) ? centerPixel.rgb : filteredPixelColor;
 
 
 	// final filteredPixelColor processing ////////////////////////////////////
