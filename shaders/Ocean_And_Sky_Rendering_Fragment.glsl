85d2d43ccf76eacc3552ad1540d1893c1a9c4eae
Erich Loftis <erichlof@swbell.net>
2022-03-10 22:48:36 -0600

    Update Ocean_And_Sky_Rendering_Fragment.glsl

--- shaders/Ocean_And_Sky_Rendering_Fragment.glsl
+++ shaders/Ocean_And_Sky_Rendering_Fragment.glsl
@@ -662,9 +662,7 @@ vec3 CalculateRadiance(out vec3 objectNormal, out vec3 objectColor, out float ob
 			//previousIntersecType = REFR;
 			//above REFR setting must be placed under the if statement below that uses previousIntersecType
 
-			// use the following simplified check if scene is dynamic
-			if (diffuseCount == 0)
-				pixelSharpness = -1.0;
+			pixelSharpness = diffuseCount == 0 ? -1.0 : pixelSharpness;
 
 			checkOcean = false;
 			
@@ -912,24 +910,18 @@ void main( void )
 		currentPixel.rgb *= 0.5;
 	}
 
-	currentPixel.a = 0.0;
 	if (colorDifference >= 1.0 || normalDifference >= 1.0 || objectDifference >= 1.0)
 		pixelSharpness = 1.01;
 
-	
-	// Eventually, all edge-containing pixels' .a (alpha channel) values will converge to 1.01, which keeps them from getting blurred by the box-blur filter, thus retaining sharpness.
+	currentPixel.a = pixelSharpness;
+
+	// makes sharp edges more stable
 	if (previousPixel.a == 1.01)
 		currentPixel.a = 1.01;
-	// for dynamic scenes
+
+	// for dynamic scenes (to clear out old, dark, sharp pixel trails left behind from moving objects)
 	if (previousPixel.a == 1.01 && rng() < 0.05)
 		currentPixel.a = 1.0;
-	if (previousPixel.a == -1.0)
-		currentPixel.a = 0.0;
-
-	if (pixelSharpness == 1.01)
-		currentPixel.a = 1.01;
-	if (pixelSharpness == -1.0)
-		currentPixel.a = -1.0;
 
 	
 	pc_fragColor = vec4(previousPixel.rgb + currentPixel.rgb, currentPixel.a);
