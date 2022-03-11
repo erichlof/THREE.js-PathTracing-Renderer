cbb97e62b60072d563e2ecb327e526c494b3a893
Erich Loftis <erichlof@swbell.net>
2022-03-10 23:10:39 -0600

    Update BVH_Visualizer.js

--- js/BVH_Visualizer.js
+++ js/BVH_Visualizer.js
@@ -148,6 +148,7 @@ function initSceneData()
 
 	// scene/demo-specific three.js objects setup goes here
 	sceneIsDynamic = false;
+	
 	cameraFlightSpeed = 60;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
