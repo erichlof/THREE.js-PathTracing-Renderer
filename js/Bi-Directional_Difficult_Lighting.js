648fa2f6ea234c2318058c5ad83cb19643852584
Erich Loftis <erichlof@swbell.net>
2022-03-10 23:11:28 -0600

    Update Bi-Directional_Difficult_Lighting.js

--- js/Bi-Directional_Difficult_Lighting.js
+++ js/Bi-Directional_Difficult_Lighting.js
@@ -112,6 +112,9 @@ function initSceneData()
 
 	// scene/demo-specific three.js objects setup goes here
 	sceneIsDynamic = false;
+	
+	edgeSharpenSpeed = 0.0005;
+
 	cameraFlightSpeed = 200;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
