84ac6833c9a9e76e8931aa2626aadb830c6f7930
Erich Loftis <erichlof@swbell.net>
2022-03-10 23:40:45 -0600

    Update Terrain_Rendering.js

--- js/Terrain_Rendering.js
+++ js/Terrain_Rendering.js
@@ -12,6 +12,7 @@ function initSceneData()
 
 	// scene/demo-specific three.js objects setup goes here
 	sceneIsDynamic = true;
+	
 	cameraFlightSpeed = 300;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
