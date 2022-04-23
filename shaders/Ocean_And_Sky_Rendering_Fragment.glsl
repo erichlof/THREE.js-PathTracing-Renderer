b3810a415b02daf8f8fcb1b730407cb3039ce866
Erich Loftis <erichlof@swbell.net>
2022-04-23 01:00:56 -0500

    Update Ocean_And_Sky_Rendering_Fragment.glsl

--- shaders/Ocean_And_Sky_Rendering_Fragment.glsl
+++ shaders/Ocean_And_Sky_Rendering_Fragment.glsl
@@ -841,8 +841,9 @@ void main( void )
 	// we must map pixelPos into the range -1.0 to +1.0
 	vec2 pixelPos = ((gl_FragCoord.xy + pixelOffset) / uResolution) * 2.0 - 1.0;
 
-	vec3 rayDir = normalize( pixelPos.x * camRight * uULen + pixelPos.y * camUp * uVLen + camForward );
-	
+	vec3 rayDir = uUseOrthographicCamera ? camForward : 
+					       normalize( pixelPos.x * camRight * uULen + pixelPos.y * camUp * uVLen + camForward );
+
 	// depth of field
 	vec3 focalPoint = uFocusDistance * rayDir;
 	float randomAngle = rand() * TWO_PI; // pick random point on aperture
@@ -851,8 +852,10 @@ void main( void )
 	// point on aperture to focal point
 	vec3 finalRayDir = normalize(focalPoint - randomAperturePos);
 	
-	rayOrigin = cameraPosition + randomAperturePos;
+	rayOrigin = uUseOrthographicCamera ? cameraPosition + (camRight * pixelPos.x * uULen * 100.0) + (camUp * pixelPos.y * uVLen * 100.0) + randomAperturePos :
+					     cameraPosition + randomAperturePos;
 	rayDirection = finalRayDir;
+
 
         SetupScene(); 
 
