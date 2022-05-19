d4c2ff9fc1f2afac4611ad8274cce44c3c0d1634
Erich Loftis <erichlof@swbell.net>
2022-05-19 09:09:17 -0500

    Add files via upload

---
+++ js/Light_Shafts.js
@@ -0,0 +1,43 @@
+// scene/demo-specific variables go here
+
+
+// called automatically from within initTHREEjs() function (located in InitCommon.js file)
+function initSceneData() 
+{
+	demoFragmentShaderFileName = 'Light_Shafts_Fragment.glsl';
+
+	// scene/demo-specific three.js objects setup goes here
+	sceneIsDynamic = false;
+
+	cameraFlightSpeed = 50;
+
+	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+	pixelRatio = mouseControl ? 0.75 : 0.75; // less demanding on battery-powered mobile devices
+
+	EPS_intersect = 0.001;
+
+	// set camera's field of view
+	worldCamera.fov = 60;
+	focusDistance = 100;
+
+	// position and orient camera
+	cameraControlsObject.position.set(0, 0, 50);
+	///cameraControlsYawObject.rotation.y = 0.0;
+	// look slightly upward
+	cameraControlsPitchObject.rotation.x = 0.005;
+
+} // end function initSceneData()
+
+
+
+// called automatically from within the animate() function (located in InitCommon.js file)
+function updateVariablesAndUniforms() 
+{
+	// INFO
+	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
+
+} // end function updateUniforms()
+
+
+
+init(); // init app and start animating
