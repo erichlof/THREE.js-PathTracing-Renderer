f9aabef1d0550faee3d448f3ccebd66fe891d533
Erich Loftis <erichlof@swbell.net>
2022-08-23 15:55:47 -0500

    Update Cornell_Box.js

--- js/Cornell_Box.js
+++ js/Cornell_Box.js
@@ -13,7 +13,7 @@ function initSceneData()
 	cameraFlightSpeed = 300;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = 1.0;
+	pixelRatio = mouseControl ? 1.0 : 0.75;
 
 	EPS_intersect = 0.01;
 
