75819ac6acaf0692b00f13222b0aa869720d44bb
Erich Loftis <erichlof@swbell.net>
2022-11-03 23:25:39 -0500

    Update PathTracingCommon.js

--- js/PathTracingCommon.js
+++ js/PathTracingCommon.js
@@ -2532,6 +2532,41 @@ float BoxIntersect( vec3 minCorner, vec3 maxCorner, vec3 rayOrigin, vec3 rayDire
 }
 `;
 
+THREE.ShaderChunk[ 'pathtracing_box_interior_intersect' ] = `
+//--------------------------------------------------------------------------------------------------------------
+float BoxInteriorIntersect( vec3 minCorner, vec3 maxCorner, vec3 rayOrigin, vec3 rayDirection, out vec3 normal )
+//--------------------------------------------------------------------------------------------------------------
+{
+	vec3 invDir = 1.0 / rayDirection;
+	vec3 near = (minCorner - rayOrigin) * invDir;
+	vec3 far  = (maxCorner - rayOrigin) * invDir;
+	
+	vec3 tmin = min(near, far);
+	vec3 tmax = max(near, far);
+	
+	float t0 = max( max(tmin.x, tmin.y), tmin.z);
+	float t1 = min( min(tmax.x, tmax.y), tmax.z);
+	
+	if (t0 > t1) return INFINITY;
+
+	/*
+	if (t0 > 0.0) // if we are outside the box
+	{
+		normal = -sign(rayDirection) * step(tmin.yzx, tmin) * step(tmin.zxy, tmin);
+		return t0;	
+	}
+	*/
+
+	if (t1 > 0.0) // if we are inside the box
+	{
+		normal = -sign(rayDirection) * step(tmax, tmax.yzx) * step(tmax, tmax.zxy);
+		return t1;
+	}
+
+	return INFINITY;
+}
+`;
+
 THREE.ShaderChunk[ 'pathtracing_boundingbox_intersect' ] = `
 //--------------------------------------------------------------------------------------
 float BoundingBoxIntersect( vec3 minCorner, vec3 maxCorner, vec3 rayOrigin, vec3 invDir )
