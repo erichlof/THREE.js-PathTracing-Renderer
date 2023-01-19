06df1d30f6e2c05b9b8994f1d08cca905770d518
Erich Loftis <erichlof@swbell.net>
2023-01-19 00:52:57 -0600

    Update Kajiya_TheRenderingEquation.js

--- js/Kajiya_TheRenderingEquation.js
+++ js/Kajiya_TheRenderingEquation.js
@@ -19,6 +19,7 @@ function initSceneData()
 	// set camera's field of view
 	worldCamera.fov = 60;
 	focusDistance = 140.0;
+	apertureChangeSpeed = 5;
 
 	// position and orient camera
 	cameraControlsObject.position.set(20,30,130);
