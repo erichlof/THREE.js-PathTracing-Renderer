5b2ede991f6d8c64b426b241ebaa8a7d17f0f6a9
Erich Loftis <erichlof@swbell.net>
2023-03-01 22:25:58 -0600

    Update Ray_Warping.js

--- js/Ray_Warping.js
+++ js/Ray_Warping.js
@@ -12,7 +12,7 @@ function initSceneData()
 	cameraFlightSpeed = 300;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.75;
+	pixelRatio = mouseControl ? 1.0 : 0.75; // less demanding on battery-powered mobile devices
 
 	EPS_intersect = 0.01;
 
