e725dbbc9df648363201f99d22be51e1df0498f0
Erich Loftis <erichlof@swbell.net>
2023-04-05 00:10:10 -0500

    Update PathTracingCommon.js

--- js/PathTracingCommon.js
+++ js/PathTracingCommon.js
@@ -1246,16 +1246,13 @@ void Box_CSG_Intersect( vec3 ro, vec3 rd, out float t0, out float t1, out vec3 n
 }
 `;
 
-THREE.ShaderChunk[ 'pathtracing_convexpolyhedron_csg_intersect' ] = `
+THREE.ShaderChunk[ 'pathtracing_convexpolyhedron_intersect' ] = `
 // This convexPolyhedron routine works with any number of user-defined cutting planes - a plane is defined by its unit normal (vec3) and an offset distance (float) 
 //  from the plane origin to the shape's origin.  Examples of shapes that can be made from a list of pure convex cutting planes: cube, frustum, 
 //  triangular pyramid (tetrahedron), rectangular pyramid, triangular bipyramid (hexahedron), rectangular bipyramid (octahedron), and other polyhedra.
 
-const int numPlanes = 8;
-vec4 convex_planes[numPlanes];
-
 //------------------------------------------------------------------------------------------------------------
-float ConvexPolyhedron_Intersect( vec3 ro, vec3 rd, out vec3 n )
+float ConvexPolyhedron_4faces_Intersect( vec3 ro, vec3 rd, out vec3 n, vec4 planes[4] )
 //------------------------------------------------------------------------------------------------------------
 {
 	vec3 n0, n1;
@@ -1263,106 +1260,213 @@ float ConvexPolyhedron_Intersect( vec3 ro, vec3 rd, out vec3 n )
 	float t0 = -INFINITY;
 	float t1 = INFINITY;
 	float plane_dot_rayDir;
-
-	// for triangular pyramid (tetrahedron), set numPlanes = 4 above
-	// convex_planes[0] = vec4(normalize(vec3( 1, 0.45, 0.6)), -0.3);
-	// convex_planes[1] = vec4(normalize(vec3(-1, 0.45, 0.6)), -0.3);
-	// convex_planes[2] = vec4(normalize(vec3( 0,  0.3,  -1)), -0.3);
-	// convex_planes[3] = vec4(normalize(vec3( 0,-1, 0)), -1.0);
-
-	// for rectangular pyramid, set numPlanes = 5 above
-	// convex_planes[0] = vec4(normalize(vec3( 1, 0.5, 0)), -0.4);
-	// convex_planes[1] = vec4(normalize(vec3(-1, 0.5, 0)), -0.4);
-	// convex_planes[2] = vec4(normalize(vec3( 0, 0.5, 1)), -0.4);
-	// convex_planes[3] = vec4(normalize(vec3( 0, 0.5,-1)), -0.4);
-	// convex_planes[4] = vec4(normalize(vec3( 0,-1, 0)), -1.0);
-
-	// for triangular prism, set numPlanes = 5 above
-	// convex_planes[0] = vec4(normalize(vec3( 0, 0, 1)), -0.5);
-	// convex_planes[1] = vec4(normalize(vec3( 1, 0,-0.577)), -0.5);
-	// convex_planes[2] = vec4(normalize(vec3(-1, 0,-0.577)), -0.5);
-	// convex_planes[3] = vec4(normalize(vec3( 0, 1, 0)), -1.0);
-	// convex_planes[4] = vec4(normalize(vec3( 0,-1, 0)), -1.0);
-
-	// for cube, set numPlanes = 6 above
-	// convex_planes[0] = vec4((vec3( 1, 0, 0)), -1.0);
-	// convex_planes[1] = vec4((vec3(-1, 0, 0)), -1.0);
-	// convex_planes[2] = vec4((vec3( 0, 1, 0)), -1.0);
-	// convex_planes[3] = vec4((vec3( 0,-1, 0)), -1.0);
-	// convex_planes[4] = vec4((vec3( 0, 0, 1)), -1.0);
-	// convex_planes[5] = vec4((vec3( 0, 0,-1)), -1.0);
-
-	// for frustum (rectangular pyramid with apex cut off), set numPlanes = 6 above
-	// convex_planes[0] = vec4(normalize(vec3( 1, 0.35, 0)), -0.6);
-	// convex_planes[1] = vec4(normalize(vec3(-1, 0.35, 0)), -0.6);
-	// convex_planes[2] = vec4(normalize(vec3( 0, 0.35, 1)), -0.6);
-	// convex_planes[3] = vec4(normalize(vec3( 0, 0.35,-1)), -0.6);
-	// convex_planes[4] = vec4(normalize(vec3( 0,   1, 0)), -1.0);
-	// convex_planes[5] = vec4(normalize(vec3( 0,  -1, 0)), -1.0);
-
-	// for triangular bipyramid (hexahedron), set numPlanes = 6 above
-	// convex_planes[0] = vec4(normalize(vec3( 1, 0.7, 0.6)), -0.5);
-	// convex_planes[1] = vec4(normalize(vec3(-1, 0.7, 0.6)), -0.5);
-	// convex_planes[2] = vec4(normalize(vec3( 0, 0.6,  -1)), -0.5);
-	// convex_planes[3] = vec4(normalize(vec3( 1,-0.7, 0.6)), -0.5);
-	// convex_planes[4] = vec4(normalize(vec3(-1,-0.7, 0.6)), -0.5);
-	// convex_planes[5] = vec4(normalize(vec3( 0,-0.6,  -1)), -0.5);
-
-	// for pentagonal prism, set numPlanes = 7 above
-	// convex_planes[0] = vec4(normalize(vec3(cos(TWO_PI * 0.25), 0, sin(TWO_PI * 0.25))), -0.8);
-	// convex_planes[1] = vec4(normalize(vec3(cos(TWO_PI * 0.45), 0, sin(TWO_PI * 0.45))), -0.8);
-	// convex_planes[2] = vec4(normalize(vec3(cos(TWO_PI * 0.65), 0, sin(TWO_PI * 0.65))), -0.8);
-	// convex_planes[3] = vec4(normalize(vec3(cos(TWO_PI * 0.85), 0, sin(TWO_PI * 0.85))), -0.8);
-	// convex_planes[4] = vec4(normalize(vec3(cos(TWO_PI * 1.05), 0, sin(TWO_PI * 1.05))), -0.8);
-	// convex_planes[5] = vec4(normalize(vec3( 0, 1, 0)), -1.0);
-	// convex_planes[6] = vec4(normalize(vec3( 0,-1, 0)), -1.0);
-
-	// for rectangular bipyramid (octahedron), set numPlanes = 8 above
-	convex_planes[0] = vec4(normalize(vec3( 1, 0.75, 0)), -0.6);
-	convex_planes[1] = vec4(normalize(vec3(-1, 0.75, 0)), -0.6);
-	convex_planes[2] = vec4(normalize(vec3( 0, 0.75, 1)), -0.6);
-	convex_planes[3] = vec4(normalize(vec3( 0, 0.75,-1)), -0.6);
-	convex_planes[4] = vec4(normalize(vec3( 1,-0.75, 0)), -0.6);
-	convex_planes[5] = vec4(normalize(vec3(-1,-0.75, 0)), -0.6);
-	convex_planes[6] = vec4(normalize(vec3( 0,-0.75, 1)), -0.6);
-	convex_planes[7] = vec4(normalize(vec3( 0,-0.75,-1)), -0.6);
-
-	// for hexagonal prism, set numPlanes = 8 above
-	// convex_planes[0] = vec4(normalize(vec3( 0, 0,     1)), -0.9);
-	// convex_planes[1] = vec4(normalize(vec3( 0, 0,    -1)), -0.9);
-	// convex_planes[2] = vec4(normalize(vec3( 1, 0, 0.577)), -0.9);
-	// convex_planes[3] = vec4(normalize(vec3( 1, 0,-0.577)), -0.9);
-	// convex_planes[4] = vec4(normalize(vec3(-1, 0, 0.577)), -0.9);
-	// convex_planes[5] = vec4(normalize(vec3(-1, 0,-0.577)), -0.9);
-	// convex_planes[6] = vec4(normalize(vec3( 0, 1, 0)), -1.0);
-	// convex_planes[7] = vec4(normalize(vec3( 0,-1, 0)), -1.0);
-
 	
-	for (int i = 0; i < numPlanes; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		plane_dot_rayDir = dot(convex_planes[i].xyz, rd);
+		plane_dot_rayDir = dot(planes[i].xyz, rd);
 		if (plane_dot_rayDir == 0.0)
 			continue;
 
-		t = (-dot(convex_planes[i].xyz, ro) - convex_planes[i].w) / plane_dot_rayDir;
+		t = (-dot(planes[i].xyz, ro) - planes[i].w) / plane_dot_rayDir;
 
 		if (plane_dot_rayDir < 0.0 && t > t0)
 		{
 			t0 = t;
-			n0 = convex_planes[i].xyz;
+			n0 = planes[i].xyz;
 		}	
-	
 		if (plane_dot_rayDir > 0.0 && t < t1)
 		{
 			t1 = t;
-			n1 = convex_planes[i].xyz;
+			n1 = planes[i].xyz;
 		}	
-		
 	}
 
-	if (t0 > t1)
+	if (t0 > t1) // check for invalid t0/t1 intersection pair
 		return INFINITY;
+	if (t0 > 0.0)
+	{
+		n = n0;
+		return t0;
+	}
+	if (t1 > 0.0)
+	{
+		n = n1;
+		return t1;
+	}
 
+	return INFINITY;
+}
+
+//------------------------------------------------------------------------------------------------------------
+float ConvexPolyhedron_5faces_Intersect( vec3 ro, vec3 rd, out vec3 n, vec4 planes[5] )
+//------------------------------------------------------------------------------------------------------------
+{
+	vec3 n0, n1;
+	float t;
+	float t0 = -INFINITY;
+	float t1 = INFINITY;
+	float plane_dot_rayDir;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		plane_dot_rayDir = dot(planes[i].xyz, rd);
+		if (plane_dot_rayDir == 0.0)
+			continue;
+
+		t = (-dot(planes[i].xyz, ro) - planes[i].w) / plane_dot_rayDir;
+
+		if (plane_dot_rayDir < 0.0 && t > t0)
+		{
+			t0 = t;
+			n0 = planes[i].xyz;
+		}	
+		if (plane_dot_rayDir > 0.0 && t < t1)
+		{
+			t1 = t;
+			n1 = planes[i].xyz;
+		}	
+	}
+
+	if (t0 > t1) // check for invalid t0/t1 intersection pair
+		return INFINITY;
+	if (t0 > 0.0)
+	{
+		n = n0;
+		return t0;
+	}
+	if (t1 > 0.0)
+	{
+		n = n1;
+		return t1;
+	}
+
+	return INFINITY;
+}
+
+//------------------------------------------------------------------------------------------------------------
+float ConvexPolyhedron_6faces_Intersect( vec3 ro, vec3 rd, out vec3 n, vec4 planes[6] )
+//------------------------------------------------------------------------------------------------------------
+{
+	vec3 n0, n1;
+	float t;
+	float t0 = -INFINITY;
+	float t1 = INFINITY;
+	float plane_dot_rayDir;
+	
+	for (int i = 0; i < 6; i++)
+	{
+		plane_dot_rayDir = dot(planes[i].xyz, rd);
+		if (plane_dot_rayDir == 0.0)
+			continue;
+
+		t = (-dot(planes[i].xyz, ro) - planes[i].w) / plane_dot_rayDir;
+
+		if (plane_dot_rayDir < 0.0 && t > t0)
+		{
+			t0 = t;
+			n0 = planes[i].xyz;
+		}	
+		if (plane_dot_rayDir > 0.0 && t < t1)
+		{
+			t1 = t;
+			n1 = planes[i].xyz;
+		}	
+	}
+
+	if (t0 > t1) // check for invalid t0/t1 intersection pair
+		return INFINITY;
+	if (t0 > 0.0)
+	{
+		n = n0;
+		return t0;
+	}
+	if (t1 > 0.0)
+	{
+		n = n1;
+		return t1;
+	}
+
+	return INFINITY;
+}
+
+//------------------------------------------------------------------------------------------------------------
+float ConvexPolyhedron_7faces_Intersect( vec3 ro, vec3 rd, out vec3 n, vec4 planes[7] )
+//------------------------------------------------------------------------------------------------------------
+{
+	vec3 n0, n1;
+	float t;
+	float t0 = -INFINITY;
+	float t1 = INFINITY;
+	float plane_dot_rayDir;
+	
+	for (int i = 0; i < 7; i++)
+	{
+		plane_dot_rayDir = dot(planes[i].xyz, rd);
+		if (plane_dot_rayDir == 0.0)
+			continue;
+
+		t = (-dot(planes[i].xyz, ro) - planes[i].w) / plane_dot_rayDir;
+
+		if (plane_dot_rayDir < 0.0 && t > t0)
+		{
+			t0 = t;
+			n0 = planes[i].xyz;
+		}	
+		if (plane_dot_rayDir > 0.0 && t < t1)
+		{
+			t1 = t;
+			n1 = planes[i].xyz;
+		}	
+	}
+
+	if (t0 > t1) // check for invalid t0/t1 intersection pair
+		return INFINITY;
+	if (t0 > 0.0)
+	{
+		n = n0;
+		return t0;
+	}
+	if (t1 > 0.0)
+	{
+		n = n1;
+		return t1;
+	}
+
+	return INFINITY;
+}
+
+//------------------------------------------------------------------------------------------------------------
+float ConvexPolyhedron_8faces_Intersect( vec3 ro, vec3 rd, out vec3 n, vec4 planes[8] )
+//------------------------------------------------------------------------------------------------------------
+{
+	vec3 n0, n1;
+	float t;
+	float t0 = -INFINITY;
+	float t1 = INFINITY;
+	float plane_dot_rayDir;
+	
+	for (int i = 0; i < 8; i++)
+	{
+		plane_dot_rayDir = dot(planes[i].xyz, rd);
+		if (plane_dot_rayDir == 0.0)
+			continue;
+
+		t = (-dot(planes[i].xyz, ro) - planes[i].w) / plane_dot_rayDir;
+
+		if (plane_dot_rayDir < 0.0 && t > t0)
+		{
+			t0 = t;
+			n0 = planes[i].xyz;
+		}	
+		if (plane_dot_rayDir > 0.0 && t < t1)
+		{
+			t1 = t;
+			n1 = planes[i].xyz;
+		}	
+	}
+
+	if (t0 > t1) // check for invalid t0/t1 intersection pair
+		return INFINITY;
 	if (t0 > 0.0)
 	{
 		n = n0;
@@ -2938,12 +3042,12 @@ vec3 randomCosWeightedDirectionInHemisphere(vec3 nl)
     	return normalize(nl + vec3(r * cos(phi), r * sin(phi), z));
 }
 
-vec3 randomDirectionInSpecularLobe(vec3 reflectionDir, float roughness)
+vec3 randomDirectionInSpecularLobe(vec3 reflectionDir, vec3 nl, float roughness)
 {
 	float z = rng() * 2.0 - 1.0;
 	float phi = rng() * TWO_PI;
 	float r = sqrt(1.0 - z * z);
-    	vec3 cosDiffuseDir = normalize(reflectionDir + vec3(r * cos(phi), r * sin(phi), z));
+    	vec3 cosDiffuseDir = normalize(nl + vec3(r * cos(phi), r * sin(phi), z));
 	return normalize( mix(reflectionDir, cosDiffuseDir, roughness * roughness) );
 }
 
