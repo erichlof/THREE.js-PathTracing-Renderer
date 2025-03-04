8a174e276f6979a8d72b5dee5cc7e105f463d512
Erich Loftis <erichlof@swbell.net>
2025-03-04 12:09:45 -0600

    Update Light_Shafts_Fragment.glsl

--- shaders/Light_Shafts_Fragment.glsl
+++ shaders/Light_Shafts_Fragment.glsl
@@ -321,7 +321,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 		}
 		if (diffuseCount == 0) // handles reflections of light sources
 		{
-			objectNormal = nl; 
+			objectNormal += nl; 
 		}
 
 		// now do the normal path tracing routine with the camera ray
