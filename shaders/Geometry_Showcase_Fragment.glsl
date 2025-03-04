d74e3152feb6c1b2f8b47fd17e9edd1cc45267bc
Erich Loftis <erichlof@swbell.net>
2025-03-04 12:11:49 -0600

    Update Geometry_Showcase_Fragment.glsl

--- shaders/Geometry_Showcase_Fragment.glsl
+++ shaders/Geometry_Showcase_Fragment.glsl
@@ -361,13 +361,13 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 		}
 		if (isReflectionTime == FALSE && diffuseCount == 0 && hitObjectID != previousObjectID)
 		{
-			objectNormal = nl;
-			objectColor = hitColor;
+			objectNormal += nl;
+			objectColor += hitColor;
 		}
 		if (reflectionNeedsToBeSharp == TRUE && reflectionBounces == 0)
 		{
-			objectNormal = nl;
-			objectColor = hitColor;
+			objectNormal += nl;
+			objectColor += hitColor;
 		}
 		
 		
@@ -379,9 +379,9 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 
 			if (isReflectionTime == TRUE && bounceIsSpecular == TRUE)
 			{
-				objectNormal = nl;
+				objectNormal += nl;
 				//objectColor = hitColor;
-				objectID = hitObjectID;
+				objectID += hitObjectID;
 			}
 
 			if (bounceIsSpecular == TRUE || sampleLight == TRUE)
