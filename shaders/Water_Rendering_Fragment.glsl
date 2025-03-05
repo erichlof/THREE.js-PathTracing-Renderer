d982a37095f79006b70aaa0c35ff6dcb2e95397a
Erich Loftis <erichlof@swbell.net>
2025-03-04 19:29:02 -0600

    Update Water_Rendering_Fragment.glsl

--- shaders/Water_Rendering_Fragment.glsl
+++ shaders/Water_Rendering_Fragment.glsl
@@ -277,7 +277,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 		if (isReflectionTime == FALSE && diffuseCount == 0 && hitObjectID != previousObjectID)
 		{
 			objectNormal += nl;
-			objectColor = hitColor;
+			objectColor += hitColor;
 		}
 		/* if (reflectionNeedsToBeSharp == TRUE && reflectionBounces == 0)
 		{
