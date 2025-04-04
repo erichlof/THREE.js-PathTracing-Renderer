1e77e58a6839096b058cade5b76b269c233aac44
Erich Loftis <erichlof@swbell.net>
2025-04-04 13:53:49 -0500

    Add files via upload

---
+++ shaders/Quads_Only_Modeling_Fragment.glsl
@@ -0,0 +1,434 @@
+precision highp float;
+precision highp int;
+precision highp sampler2D;
+
+#include <pathtracing_uniforms_and_defines>
+
+uniform float uFrontLeftVertexHeight;
+uniform float uFrontMiddleVertexHeight;
+uniform float uFrontRightVertexHeight;
+uniform float uRearLeftVertexHeight;
+uniform float uRearMiddleVertexHeight;
+uniform float uRearRightVertexHeight;
+
+
+#define N_QUADS 1
+#define N_BOXES 1
+#define N_BILINEAR_PATCHES 2
+
+vec3 rayOrigin, rayDirection;
+// recorded intersection data:
+vec3 hitNormal, hitEmission, hitColor;
+vec2 hitUV;
+float hitObjectID = -INFINITY;
+int hitType = -100;
+
+
+struct Quad { vec3 normal; vec3 v0; vec3 v1; vec3 v2; vec3 v3; vec3 emission; vec3 color; int type; };
+struct Box { vec3 minCorner; vec3 maxCorner; vec3 emission; vec3 color; int type; };
+struct BiLinearPatch { vec3 p0; vec3 p1; vec3 p2; vec3 p3; vec3 emission; vec3 color; int type; };
+
+Quad quads[N_QUADS];
+Box boxes[N_BOXES];
+BiLinearPatch bilinearPatches[N_BILINEAR_PATCHES];
+
+
+#include <pathtracing_random_functions>
+
+#include <pathtracing_calc_fresnel_reflectance>
+
+#include <pathtracing_sphere_intersect>
+
+#include <pathtracing_bilinear_patch_intersect>
+
+#include <pathtracing_quad_intersect>
+
+#include <pathtracing_box_interior_intersect>
+
+#include <pathtracing_sample_quad_light>
+
+
+//---------------------------------------------------------------------------------------
+float SceneIntersect( )
+//---------------------------------------------------------------------------------------
+{
+	vec3 rObjOrigin, rObjDirection;
+	vec3 normal, hitPoint;
+	float d;
+	float t = INFINITY;
+	float u, v;
+	int objectCount = 0;
+	
+	hitObjectID = -INFINITY;
+
+	
+	d = QuadIntersect( quads[0].v0, quads[0].v1, quads[0].v2, quads[0].v3, rayOrigin, rayDirection, FALSE );
+	if (d < t)
+	{
+		t = d;
+		hitNormal = quads[0].normal;
+		hitEmission = quads[0].emission;
+		hitColor = quads[0].color;
+		hitType = quads[0].type;
+		hitObjectID = float(objectCount);
+	}
+	objectCount++;
+	
+	d = BoxInteriorIntersect( boxes[0].minCorner, boxes[0].maxCorner, rayOrigin, rayDirection, normal );
+	if (d < t && normal != vec3(0,0,-1))
+	{
+		t = d;
+		hitNormal = normal;
+		hitEmission = boxes[0].emission;
+		hitColor = vec3(1);
+		hitType = DIFF;
+
+		if (normal == vec3(1,0,0)) // left wall
+		{
+			hitColor = vec3(0.7, 0.05, 0.05);
+		}
+		else if (normal == vec3(-1,0,0)) // right wall
+		{
+			hitColor = vec3(0.05, 0.05, 0.7);
+		}
+		
+		hitObjectID = float(objectCount);
+	}
+	objectCount++;
+	
+
+	d = BilinearPatchIntersect( bilinearPatches[0].p0, bilinearPatches[0].p1, bilinearPatches[0].p2, bilinearPatches[0].p3, rayOrigin, rayDirection, TRUE, normal, u, v );
+	if (d < t)
+	{
+		t = d;
+		hitNormal = normal;
+		hitEmission = bilinearPatches[0].emission;
+		hitColor = bilinearPatches[0].color;
+		hitType = bilinearPatches[0].type;
+		hitObjectID = float(objectCount);
+	}
+	objectCount++;
+
+	d = BilinearPatchIntersect( bilinearPatches[1].p0, bilinearPatches[1].p1, bilinearPatches[1].p2, bilinearPatches[1].p3, rayOrigin, rayDirection, TRUE, normal, u, v );
+	if (d < t)
+	{
+		t = d;
+		hitNormal = normal;
+		hitEmission = bilinearPatches[1].emission;
+		hitColor = bilinearPatches[1].color;
+		hitType = bilinearPatches[1].type;
+		hitObjectID = float(objectCount);
+	}
+	objectCount++;
+
+	
+	return t;
+
+} // end float SceneIntersect( )
+
+
+//-----------------------------------------------------------------------------------------------------------------------------
+vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float objectID, out float pixelSharpness )
+//-----------------------------------------------------------------------------------------------------------------------------
+{
+	Quad light = quads[0];
+
+	vec3 accumCol = vec3(0);
+	vec3 mask = vec3(1);
+	vec3 reflectionMask = vec3(1);
+	vec3 reflectionRayOrigin = vec3(0);
+	vec3 reflectionRayDirection = vec3(0);
+	vec3 dirToLight;
+	vec3 x, n, nl;
+	vec3 absorptionCoefficient;
+	
+	float t;
+	float nc, nt, ratioIoR, Re, Tr;
+	float weight;
+	float thickness = 0.05;
+	float scatteringDistance;
+	float previousObjectID;
+
+	int reflectionBounces = -1;
+	int diffuseCount = 0;
+	int previousIntersecType = -100;
+	hitType = -100;
+
+	int bounceIsSpecular = TRUE;
+	int sampleLight = FALSE;
+	int willNeedReflectionRay = FALSE;
+	int isReflectionTime = FALSE;
+	int reflectionNeedsToBeSharp = FALSE;
+	
+
+	for (int bounces = 0; bounces < 6; bounces++)
+	{
+		if (isReflectionTime == TRUE)
+			reflectionBounces++;
+
+		previousIntersecType = hitType;
+		previousObjectID = hitObjectID;
+
+		t = SceneIntersect();
+		
+
+		if (t == INFINITY)
+		{
+			// this makes the object edges sharp against the black background
+			if (bounces == 0 || (bounces == 1 && previousIntersecType == SPEC))
+				pixelSharpness = 1.01;
+
+			if (willNeedReflectionRay == TRUE)
+			{
+				mask = reflectionMask;
+				rayOrigin = reflectionRayOrigin;
+				rayDirection = reflectionRayDirection;
+
+				willNeedReflectionRay = FALSE;
+				bounceIsSpecular = TRUE;
+				sampleLight = FALSE;
+				//diffuseCount = 0;
+				isReflectionTime = TRUE;
+				continue;
+			}
+
+			break;
+		}
+			
+
+		// useful data 
+		n = normalize(hitNormal);
+		nl = dot(n, rayDirection) < 0.0 ? n : -n;
+		x = rayOrigin + rayDirection * t;
+
+		if (bounces == 0)
+		{
+			objectID = hitObjectID;
+		}
+		if (isReflectionTime == FALSE && diffuseCount == 0 && hitObjectID != previousObjectID)
+		{
+			objectNormal += nl;
+			objectColor += hitColor;
+		}
+		if (reflectionNeedsToBeSharp == TRUE && reflectionBounces == 0)
+		{
+			objectNormal += nl;
+			objectColor += hitColor;
+		}
+		
+		
+		if (hitType == LIGHT)
+		{
+	
+			if (diffuseCount == 0 && isReflectionTime == FALSE)
+				pixelSharpness = 1.0;
+
+			if (isReflectionTime == TRUE && bounceIsSpecular == TRUE)
+			{
+				objectNormal += nl;
+				//objectColor = hitColor;
+				objectID += hitObjectID;
+			}
+			
+			if (bounceIsSpecular == TRUE || sampleLight == TRUE)
+				accumCol += mask * hitEmission;
+
+			if (willNeedReflectionRay == TRUE)
+			{
+				mask = reflectionMask;
+				rayOrigin = reflectionRayOrigin;
+				rayDirection = reflectionRayDirection;
+
+				willNeedReflectionRay = FALSE;
+				bounceIsSpecular = TRUE;
+				sampleLight = FALSE;
+				//diffuseCount = 0;
+				isReflectionTime = TRUE;
+				continue;
+			}
+			// reached a light, so we can exit
+			break;
+
+		} // end if (hitType == LIGHT)
+
+
+		// if we get here and sampleLight is still TRUE, shadow ray failed to find the light source 
+		// the ray hit an occluding object along its way to the light
+		if (sampleLight == TRUE)
+		{
+			if (willNeedReflectionRay == TRUE)
+			{
+				mask = reflectionMask;
+				rayOrigin = reflectionRayOrigin;
+				rayDirection = reflectionRayDirection;
+
+				willNeedReflectionRay = FALSE;
+				bounceIsSpecular = TRUE;
+				sampleLight = FALSE;
+				//diffuseCount = 0;
+				isReflectionTime = TRUE;
+				continue;
+			}
+
+			break;
+		}
+		
+
+		    
+		if (hitType == DIFF) // Ideal DIFFUSE reflection
+		{
+			diffuseCount++;
+
+			mask *= hitColor;
+
+			bounceIsSpecular = FALSE;
+
+			if (diffuseCount == 1 && rand() < 0.5)
+			{
+				mask *= 2.0;
+				// choose random Diffuse sample vector
+				rayDirection = randomCosWeightedDirectionInHemisphere(nl);
+				rayOrigin = x + nl * uEPS_intersect;
+				continue;
+			}
+			
+			dirToLight = sampleQuadLight(x, nl, quads[0], weight);
+			mask *= diffuseCount == 1 ? 2.0 : 1.0;
+			mask *= weight;
+
+			rayDirection = dirToLight;
+			rayOrigin = x + nl * uEPS_intersect;
+
+			sampleLight = TRUE;
+			continue;
+			
+		} // end if (hitType == DIFF)
+		
+		if (hitType == SPEC)  // Ideal SPECULAR reflection
+		{
+			mask *= hitColor;
+
+			rayDirection = reflect(rayDirection, nl);
+			rayOrigin = x + nl * uEPS_intersect;
+			continue;
+
+		} // end if (hitType == SPEC)
+
+		if (hitType == REFR)  // Ideal dielectric REFRACTION
+		{
+			nc = 1.0; // IOR of Air
+			nt = 1.5; // IOR of common Glass
+			Re = calcFresnelReflectance(rayDirection, n, nc, nt, ratioIoR);
+			Tr = 1.0 - Re;
+
+			if (Re == 1.0)
+			{
+				rayDirection = reflect(rayDirection, nl);
+				rayOrigin = x + nl * uEPS_intersect;
+				continue;
+			}
+
+			if (diffuseCount == 0 && hitObjectID != previousObjectID && n == nl)
+			{
+				reflectionMask = mask * Re;
+				reflectionRayDirection = reflect(rayDirection, nl); // reflect ray from surface
+				reflectionRayOrigin = x + nl * uEPS_intersect;
+				willNeedReflectionRay = TRUE;
+			}
+
+			// transmit ray through surface
+			
+			// is ray leaving a solid object from the inside? 
+			// If so, attenuate ray color with object color by how far ray has travelled through the medium
+			// if (distance(n, nl) > 0.1)
+			// {
+			// 	thickness = 0.01;
+			// 	mask *= exp( log(clamp(hitColor, 0.01, 0.99)) * thickness * t ); 
+			// }
+
+			mask *= Tr;
+			mask *= hitColor;
+
+			rayDirection = refract(rayDirection, nl, ratioIoR);
+			rayOrigin = x - nl * uEPS_intersect;
+
+			if (diffuseCount == 1 && isReflectionTime == FALSE)
+				bounceIsSpecular = TRUE; // turn on refracting caustics
+
+			continue;
+			
+		} // end if (hitType == REFR)
+		
+		if (hitType == COAT)  // Diffuse object underneath with ClearCoat on top
+		{
+			nc = 1.0; // IOR of Air
+			nt = 1.4; // IOR of Clear Coat
+			Re = calcFresnelReflectance(rayDirection, nl, nc, nt, ratioIoR);
+			Tr = 1.0 - Re;
+			
+			if (bounces == 0)
+			{
+				reflectionMask = mask * Re;
+				reflectionRayDirection = reflect(rayDirection, nl); // reflect ray from surface
+				reflectionRayOrigin = x + nl * uEPS_intersect;
+				willNeedReflectionRay = TRUE;
+				reflectionNeedsToBeSharp = TRUE;
+			}
+
+			diffuseCount++;
+
+			mask *= Tr;
+			mask *= hitColor;
+
+			bounceIsSpecular = FALSE;
+			
+			if (diffuseCount == 1 && rand() < 0.5)
+			{
+				mask *= 2.0;
+				// choose random Diffuse sample vector
+				rayDirection = randomCosWeightedDirectionInHemisphere(nl);
+				rayOrigin = x + nl * uEPS_intersect;
+				continue;
+			}
+
+			dirToLight = sampleQuadLight(x, nl, quads[0], weight);
+			mask *= diffuseCount == 1 ? 2.0 : 1.0;
+			mask *= weight;
+			
+			rayDirection = dirToLight;
+			rayOrigin = x + nl * uEPS_intersect;
+
+			sampleLight = TRUE;
+			continue;
+			
+		} //end if (hitType == COAT)
+
+		
+	} // end for (int bounces = 0; bounces < 6; bounces++)
+	
+	
+	return max(vec3(0), accumCol);
+
+} // end vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float objectID, out float pixelSharpness )
+
+
+
+//-----------------------------------------------------------------------
+void SetupScene(void)
+//-----------------------------------------------------------------------
+{
+	vec3 z  = vec3(0);// No color value, Black        
+	vec3 L1 = vec3(1.0, 1.0, 1.0) * 5.0;// Bright light
+	float wallRadius = 50.0;
+	float lightRadius = 10.0;
+	
+	quads[0] = Quad( vec3(0,-1, 0), vec3(-lightRadius, wallRadius-1.0,-lightRadius), vec3(lightRadius, wallRadius-1.0,-lightRadius), vec3(lightRadius, wallRadius-1.0, lightRadius), vec3(-lightRadius, wallRadius-1.0, lightRadius), L1, z, LIGHT);// Quad Area Light on ceiling
+
+	boxes[0] = Box( vec3(-wallRadius), vec3(wallRadius), z, vec3(1), DIFF);// the Cornell Box interior
+
+	bilinearPatches[0] = BiLinearPatch(vec3(-40,uFrontLeftVertexHeight,10), vec3(-20,uFrontMiddleVertexHeight,10), vec3(-20,uRearMiddleVertexHeight,-10), vec3(-40,uRearLeftVertexHeight,-10), z, vec3(0,1,1), COAT);// cyan BiLinear Patch
+	bilinearPatches[1] = BiLinearPatch(vec3(-20,uFrontMiddleVertexHeight,10), vec3(0,uFrontRightVertexHeight,10), vec3(0,uRearRightVertexHeight,-10), vec3(-20,uRearMiddleVertexHeight,-10), z, vec3(1,1,0), COAT);// yellow BiLinear Patch
+}
+
+
+#include <pathtracing_main>
