aaabf15f4006dfba00171e4c8cca06f650f8d552
Erich Loftis <erichlof@swbell.net>
2025-04-24 16:07:41 -0500

    Update Bi-Directional_Difficult_Lighting_Fragment.glsl

--- shaders/Bi-Directional_Difficult_Lighting_Fragment.glsl
+++ shaders/Bi-Directional_Difficult_Lighting_Fragment.glsl
@@ -654,7 +654,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 		}
 		if (isReflectionTime == FALSE && diffuseCount == 0 && hitObjectID != previousObjectID)
 		{
-			objectNormal += nl;
+			objectNormal += n;
 			objectColor += hitColor;
 		}
 
