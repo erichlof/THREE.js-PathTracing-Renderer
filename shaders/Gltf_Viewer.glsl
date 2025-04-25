e9602e440ba84dfb2aa7d0ca5314bee3a16c4edc
Erich Loftis <erichlof@swbell.net>
2025-04-24 20:04:33 -0500

    Update Gltf_Viewer.glsl

--- shaders/Gltf_Viewer.glsl
+++ shaders/Gltf_Viewer.glsl
@@ -374,13 +374,13 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 
 		if (isReflectionTime == FALSE && diffuseCount == 0)// && hitObjectID != previousObjectID)
 		{
-			objectNormal += nl;
+			objectNormal += n;
 			objectColor += hitColor;
 			objectID += hitObjectID;
 		}
 		if (reflectionNeedsToBeSharp == TRUE && reflectionBounces == 0)
 		{
-			objectNormal += nl;
+			objectNormal += n;
 			//objectColor += hitColor;
 			//objectID += hitObjectID;
 		}
