86d221a52776ab195802276326b4ad2b249a8fb8
Erich Loftis <erichlof@swbell.net>
2025-04-25 22:34:43 -0500

    Update Bi-Directional_Difficult_Lighting.js

--- js/Bi-Directional_Difficult_Lighting.js
+++ js/Bi-Directional_Difficult_Lighting.js
@@ -120,7 +120,7 @@ function initSceneData()
 	cameraFlightSpeed = 200;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.7;
+	pixelRatio = mouseControl ? 0.8 : 0.7;
 
 	EPS_intersect = 0.01;
 
