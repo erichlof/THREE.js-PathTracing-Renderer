ed6b1cc5a2ebb97637e021e82410a4bda7b15cc1
Erich Loftis <erichlof@swbell.net>
2025-04-25 22:41:52 -0500

    Update Cheap_Torus.js

--- js/Cheap_Torus.js
+++ js/Cheap_Torus.js
@@ -94,7 +94,7 @@ function initSceneData()
 	cameraFlightSpeed = 50;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 1.0 : 0.75;
+	pixelRatio = mouseControl ? 1.0 : 1.0;
 
 	EPS_intersect = 0.001;
 
