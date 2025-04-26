8c03bf48d61a946f73c011062724ba4b2adcc539
Erich Loftis <erichlof@swbell.net>
2025-04-25 22:33:59 -0500

    Update Arctic_Circle.js

--- js/Arctic_Circle.js
+++ js/Arctic_Circle.js
@@ -16,7 +16,7 @@ function initSceneData()
 	cameraFlightSpeed = 1000;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.7 : 0.75;
+	pixelRatio = mouseControl ? 0.8 : 0.75;
 
 	EPS_intersect = 1.0;
 
