4ad5365e9fc282b059e34326b7708e328ed71266
Erich Loftis <erichlof@swbell.net>
2025-04-25 22:50:06 -0500

    Update Fractal3D.js

--- js/Fractal3D.js
+++ js/Fractal3D.js
@@ -192,7 +192,7 @@ function initSceneData()
 	cameraFlightSpeed = 200;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.75;
+	pixelRatio = mouseControl ? 0.8 : 0.75;
 
 	EPS_intersect = 0.01;
 
