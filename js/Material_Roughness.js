20d9ce4c1363ec5fe623c9e4ca434d7d1d01ae1e
Erich Loftis <erichlof@swbell.net>
2025-05-11 00:48:26 -0500

    Update Material_Roughness.js

--- js/Material_Roughness.js
+++ js/Material_Roughness.js
@@ -18,7 +18,7 @@ function initSceneData()
 	cameraFlightSpeed = 60;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.8 : 0.75;
+	pixelRatio = mouseControl ? 1.0 : 0.75;
 
 	EPS_intersect = 0.01;
 
