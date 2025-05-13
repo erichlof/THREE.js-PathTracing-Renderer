52d7175d25365c13da6ea423ebe3ae16f8bf143c
Erich Loftis <erichlof@swbell.net>
2025-05-12 23:05:47 -0500

    Update CSG_Museum_4_Fragment.glsl

--- shaders/CSG_Museum_4_Fragment.glsl
+++ shaders/CSG_Museum_4_Fragment.glsl
@@ -966,20 +966,19 @@ float SceneIntersect( )
 vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float objectID, out float pixelSharpness )
 //-----------------------------------------------------------------------------------------------------------------------------
 {
-	Quad lightChoice;
-
+	
 	vec3 accumCol = vec3(0);
 	vec3 mask = vec3(1);
 	vec3 reflectionMask = vec3(1);
 	vec3 reflectionRayOrigin = vec3(0);
 	vec3 reflectionRayDirection = vec3(0);
-	vec3 randPointOnLight, dirToLight;
 	vec3 x, n, nl;
         
 	float t;
 	float weight;
 	float nc, nt, ratioIoR, Re, Tr;
 	float previousObjectID;
+	float newRandom = rand();
 
 	int reflectionBounces = -1;
 	int diffuseCount = 0;
@@ -990,9 +989,6 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 	int willNeedReflectionRay = FALSE;
 	int isReflectionTime = FALSE;
 	int reflectionNeedsToBeSharp = FALSE;
-
-
-	lightChoice = quads[int(rand() * 2.0)];
 
 
 
@@ -1106,11 +1102,14 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 				continue;
 			}
                         
-			dirToLight = sampleQuadLight(x, nl, lightChoice, weight);
+			if (newRandom < 0.5)
+				rayDirection = sampleQuadLight(x, nl, quads[0], weight);
+			else
+				rayDirection = sampleQuadLight(x, nl, quads[1], weight);
+				
 			mask *= diffuseCount == 1 ? 2.0 : 1.0;
 			mask *= weight * N_LIGHTS;
 
-			rayDirection = dirToLight;
 			rayOrigin = x + nl * uEPS_intersect;
 
 			sampleLight = TRUE;
@@ -1219,11 +1218,14 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 				continue;
 			}
 
-			dirToLight = sampleQuadLight(x, nl, lightChoice, weight);
+			if (newRandom < 0.5)
+				rayDirection = sampleQuadLight(x, nl, quads[0], weight);
+			else
+				rayDirection = sampleQuadLight(x, nl, quads[1], weight);
+				
 			mask *= diffuseCount == 1 ? 2.0 : 1.0;
 			mask *= weight * N_LIGHTS;
-			
-			rayDirection = dirToLight;
+
 			rayOrigin = x + nl * uEPS_intersect;
 
 			sampleLight = TRUE;
@@ -1258,18 +1260,21 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 
 			if (diffuseCount == 1 && rand() < 0.5)
                         {
-				//mask *= 2.0;
+				mask *= 2.0;
                                 // choose random scattering direction vector
 				rayDirection = randomSphereDirection();
 				rayOrigin = x + rayDirection * scatteringDistance;
 				continue;
                         }
                         
-			dirToLight = sampleQuadLight(x, nl, lightChoice, weight);
-			mask *= 2.0;
+			if (newRandom < 0.5)
+				rayDirection = sampleQuadLight(x, nl, quads[0], weight);
+			else
+				rayDirection = sampleQuadLight(x, nl, quads[1], weight);
+				
+			mask *= diffuseCount == 1 ? 2.0 : 1.0;
 			mask *= weight * N_LIGHTS;
 
-			rayDirection = dirToLight;
 			rayOrigin = x + rayDirection * scatteringDistance;
 			
 			sampleLight = TRUE;
