723924c3955a5ab371f5746223bbf3a8c3f15adc
Erich Loftis <erichlof@swbell.net>
2025-05-28 13:31:08 -0500

    Update Fractal3D_Fragment.glsl

--- shaders/Fractal3D_Fragment.glsl
+++ shaders/Fractal3D_Fragment.glsl
@@ -291,10 +291,9 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 {
 	vec3 accumCol = vec3(0);
 	vec3 mask = vec3(1);
-	vec3 reflectionMask = vec3(1);
-	vec3 reflectionRayOrigin = vec3(0);
-	vec3 reflectionRayDirection = vec3(0);
-	vec3 dirToLight;
+	vec3 diffuseBounceMask = vec3(1);
+	vec3 diffuseBounceRayOrigin = vec3(0);
+	vec3 diffuseBounceRayDirection = vec3(0);
 	vec3 x, n, nl;
 	vec3 absorptionCoefficient;
 	vec3 skyColor;
@@ -312,11 +311,12 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 	int bounceIsSpecular = TRUE;
 	int sampleLight = FALSE;
 	int isRayExiting = FALSE;
-	int willNeedReflectionRay = FALSE;
+	int willNeedDiffuseBounceRay = FALSE;
+	int isDiffuseBounceTime = FALSE;
 	
 
 	
-	for (int bounces = 0; bounces < 6; bounces++)
+	for (int bounces = 0; bounces < 10; bounces++)
 	{
 		previousIntersecType = hitType;
 
@@ -344,7 +344,20 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 				accumCol += mask * skyColor * weight;
 			}
 
-					
+			if (willNeedDiffuseBounceRay == TRUE)
+			{
+				mask = diffuseBounceMask;
+				rayOrigin = diffuseBounceRayOrigin;
+				rayDirection = diffuseBounceRayDirection;
+
+				willNeedDiffuseBounceRay = FALSE;
+				bounceIsSpecular = FALSE;
+				sampleLight = FALSE;
+				isDiffuseBounceTime = TRUE;
+				diffuseCount = 1;
+				continue;
+			}
+	
 			// reached the sky light, so we can exit
 			break;
 		} // end if (t == INFINITY)
@@ -367,6 +380,20 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 		// the ray hit an occluding object along its way to the light
 		if (sampleLight == TRUE)
 		{
+			if (willNeedDiffuseBounceRay == TRUE)
+			{
+				mask = diffuseBounceMask;
+				rayOrigin = diffuseBounceRayOrigin;
+				rayDirection = diffuseBounceRayDirection;
+
+				willNeedDiffuseBounceRay = FALSE;
+				bounceIsSpecular = FALSE;
+				sampleLight = FALSE;
+				isDiffuseBounceTime = TRUE;
+				diffuseCount = 1;
+				continue;
+			}
+
 			break;
 		}
 		
@@ -380,22 +407,19 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 
 			bounceIsSpecular = FALSE;
 
-			if (diffuseCount == 1 && rand() < 0.5)
+			rayOrigin = x + nl * uEPS_intersect;
+
+			if (diffuseCount == 1)
 			{
-				mask *= 2.0;
-				// choose random Diffuse sample vector
-				rayDirection = randomCosWeightedDirectionInHemisphere(nl);
-				rayOrigin = x + nl * uEPS_intersect;
-				continue;
+				diffuseBounceMask = mask;
+				diffuseBounceRayOrigin = rayOrigin;
+				diffuseBounceRayDirection = randomCosWeightedDirectionInHemisphere(nl);
+				willNeedDiffuseBounceRay = TRUE;
 			}
 			
 			rayDirection = randomDirectionInSpecularLobe(uSunDirection, 0.15); // create shadow ray pointed towards light
-			rayOrigin = x + nl * uEPS_intersect;
-			
 			weight = max(0.0, dot(rayDirection, nl)) * 0.03; // down-weight directSunLight contribution
-			mask *= diffuseCount == 1 ? 2.0 : 1.0;
 			mask *= weight;
-
 			sampleLight = TRUE;
 			continue;
 			
@@ -403,7 +427,7 @@ vec3 CalculateRadiance( out vec3 objectNormal, out vec3 objectColor, out float o
 		
 		
 		
-	} // end for (int bounces = 0; bounces < 6; bounces++)
+	} // end for (int bounces = 0; bounces < 10; bounces++)
 	
 	
 	return max(vec3(0), accumCol);
