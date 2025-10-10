22b296edf3e40f8c94e2f9a2f3fafd5382406942
Erich Loftis <erichlof@swbell.net>
2025-10-09 22:06:45 -0500

    Update RayTracing_InOneWeekend_Fragment.glsl

--- shaders/RayTracing_InOneWeekend_Fragment.glsl
+++ shaders/RayTracing_InOneWeekend_Fragment.glsl
@@ -187,29 +187,32 @@ float SceneIntersect( )
 		uv1 = ivec2( mod(id + 1.0, 2048.0), (id + 1.0) * INV_TEXTURE_WIDTH );
 		uv2 = ivec2( mod(id + 2.0, 2048.0), (id + 2.0) * INV_TEXTURE_WIDTH );
 		uv3 = ivec2( mod(id + 3.0, 2048.0), (id + 3.0) * INV_TEXTURE_WIDTH );
-		uv4 = ivec2( mod(id + 4.0, 2048.0), (id + 4.0) * INV_TEXTURE_WIDTH );
+		//uv4 = ivec2( mod(id + 4.0, 2048.0), (id + 4.0) * INV_TEXTURE_WIDTH );
 		
 		invTransformMatrix = mat4( texelFetch(tShape_DataTexture, uv0, 0),
 		 			   texelFetch(tShape_DataTexture, uv1, 0), 
 		 			   texelFetch(tShape_DataTexture, uv2, 0), 
 		 			   texelFetch(tShape_DataTexture, uv3, 0) );
 
-		sd4 = texelFetch(tShape_DataTexture, uv4, 0);
+		//sd4 = texelFetch(tShape_DataTexture, uv4, 0);
+		if (uSceneIsDynamic)
+			invTransformMatrix[3][1] -= sin(uTime + currentBoxNodeData1.w) * 2.0;
 
 		// transform ray into shape's object space
 		rObjOrigin = vec3( invTransformMatrix * vec4(rayOrigin, 1.0) );
 		rObjDirection = vec3( invTransformMatrix * vec4(rayDirection, 0.0) );
 
-		if (sd4.x == 0.0)
-			d = UnitBoxIntersect(rObjOrigin, rObjDirection, n);
-		else if (sd4.x == 1.0)
+		// this demo contains spheres only
+		// if (sd4.x == 0.0)
+		// 	d = UnitBoxIntersect(rObjOrigin, rObjDirection, n);
+		// else if (sd4.x == 1.0)
 			d = UnitSphereIntersect(rObjOrigin, rObjDirection, n);
-		else if (sd4.x == 2.0)
-			d = UnitCylinderIntersect(rObjOrigin, rObjDirection, n);
-		else if (sd4.x == 3.0)
-			d = UnitConeIntersect(rObjOrigin, rObjDirection, n);
-		else if (sd4.x == 4.0)
-			d = UnitParaboloidIntersect(rObjOrigin, rObjDirection, n);
+		// else if (sd4.x == 2.0)
+		// 	d = UnitCylinderIntersect(rObjOrigin, rObjDirection, n);
+		// else if (sd4.x == 3.0)
+		// 	d = UnitConeIntersect(rObjOrigin, rObjDirection, n);
+		// else if (sd4.x == 4.0)
+		// 	d = UnitParaboloidIntersect(rObjOrigin, rObjDirection, n);
 		
 		if (d > 0.0 && d < t)
 		{
@@ -473,4 +476,128 @@ void SetupScene(void)
 }
 
 
-#include <pathtracing_main>
+//#include <pathtracing_main>
+
+// tentFilter from Peter Shirley's 'Realistic Ray Tracing (2nd Edition)' book, pg. 60
+float tentFilter(float x) // input: x: a random float(0.0 to 1.0), output: a filtered float (-1.0 to +1.0)
+{
+	return (x < 0.5) ? sqrt(2.0 * x) - 1.0 : 1.0 - sqrt(2.0 - (2.0 * x));
+}
+
+void main( void )
+{
+	vec3 camRight   = vec3( uCameraMatrix[0][0],  uCameraMatrix[0][1],  uCameraMatrix[0][2]);
+	vec3 camUp      = vec3( uCameraMatrix[1][0],  uCameraMatrix[1][1],  uCameraMatrix[1][2]);
+	vec3 camForward = vec3(-uCameraMatrix[2][0], -uCameraMatrix[2][1], -uCameraMatrix[2][2]);
+	// the following is not needed - three.js has a built-in uniform named cameraPosition
+	//vec3 camPos   = vec3( uCameraMatrix[3][0],  uCameraMatrix[3][1],  uCameraMatrix[3][2]);
+
+	// calculate unique seed for rng() function
+	seed = uvec2(uFrameCounter, uFrameCounter + 1.0) * uvec2(gl_FragCoord);
+	// initialize rand() variables
+	randNumber = 0.0; // the final randomly-generated number (range: 0.0 to 1.0)
+	blueNoise = texelFetch(tBlueNoiseTexture, ivec2(mod(floor(gl_FragCoord.xy), 128.0)), 0).r;
+
+	vec2 pixelOffset;
+	
+	if (uSampleCounter < 50.0)
+	{
+		pixelOffset = vec2( tentFilter(rand()), tentFilter(rand()) );
+		pixelOffset *= uCameraIsMoving ? 0.5 : 1.0;
+	}	
+	else pixelOffset = vec2( tentFilter(uRandomVec2.x), tentFilter(uRandomVec2.y) );
+	
+	// we must map pixelPos into the range -1.0 to +1.0: (-1.0,-1.0) is bottom-left screen corner, (1.0,1.0) is top-right
+	vec2 pixelPos = ((gl_FragCoord.xy + vec2(0.5) + pixelOffset) / uResolution) * 2.0 - 1.0;
+
+	vec3 rayDir = uUseOrthographicCamera ? camForward :
+		      normalize( (camRight * pixelPos.x * uULen) + (camUp * pixelPos.y * uVLen) + camForward );
+					       
+	// depth of field
+	vec3 focalPoint = uFocusDistance * rayDir;
+	float randomAngle = rng() * TWO_PI; // pick random point on aperture
+	float randomRadius = rng() * uApertureSize;
+	vec3  randomAperturePos = ((camRight * cos(randomAngle)) + (camUp * sin(randomAngle))) * sqrt(randomRadius);
+	// point on aperture to focal point
+	vec3 finalRayDir = normalize(focalPoint - randomAperturePos);
+
+	rayOrigin = cameraPosition + randomAperturePos;
+	rayOrigin += !uUseOrthographicCamera ? vec3(0) : 
+		     (camRight * pixelPos.x * uULen * 100.0) + (camUp * pixelPos.y * uVLen * 100.0);
+					     
+	rayDirection = finalRayDir;
+	
+
+	SetupScene();
+
+	// Edge Detection - don't want to blur edges where either surface normals change abruptly (i.e. room wall corners), objects overlap each other (i.e. edge of a foreground sphere in front of another sphere right behind it),
+	// or an abrupt color variation on the same smooth surface, even if it has similar surface normals (i.e. checkerboard pattern). Want to keep all of these cases as sharp as possible - no blur filter will be applied.
+	vec3 objectNormal = vec3(0);
+	vec3 objectColor = vec3(0);
+	float objectID = -INFINITY;
+	float pixelSharpness = 0.0;
+
+	// perform path tracing and get resulting pixel color
+	vec4 currentPixel = vec4( vec3(CalculateRadiance(objectNormal, objectColor, objectID, pixelSharpness)), 0.0 );
+
+	// if difference between normals of neighboring pixels is less than the first edge0 threshold, the white edge line effect is considered off (0.0)
+	float edge0 = 0.2; // edge0 is the minimum difference required between normals of neighboring pixels to start becoming a white edge line
+	// any difference between normals of neighboring pixels that is between edge0 and edge1 smoothly ramps up the white edge line brightness (smoothstep 0.0-1.0)
+	float edge1 = 0.6; // once the difference between normals of neighboring pixels is >= this edge1 threshold, the white edge line is considered fully bright (1.0)
+	float difference_Nx = fwidth(objectNormal.x);
+	float difference_Ny = fwidth(objectNormal.y);
+	float difference_Nz = fwidth(objectNormal.z);
+	float normalDifference = smoothstep(edge0, edge1, difference_Nx) + smoothstep(edge0, edge1, difference_Ny) + smoothstep(edge0, edge1, difference_Nz);
+
+	float objectDifference = min(fwidth(objectID), 1.0);
+
+	float colorDifference = (fwidth(objectColor.r) + fwidth(objectColor.g) + fwidth(objectColor.b)) > 0.0 ? 1.0 : 0.0;
+	// white-line debug visualization for normal difference
+	//currentPixel.rgb += (rng() * 1.5) * vec3(normalDifference);
+	// white-line debug visualization for object difference
+	//currentPixel.rgb += (rng() * 1.5) * vec3(objectDifference);
+	// white-line debug visualization for color difference
+	//currentPixel.rgb += (rng() * 1.5) * vec3(colorDifference);
+	// white-line debug visualization for all 3 differences
+	//currentPixel.rgb += (rng() * 1.5) * vec3( clamp(max(normalDifference, max(objectDifference, colorDifference)), 0.0, 1.0) );
+
+	vec4 previousPixel = texelFetch(tPreviousTexture, ivec2(gl_FragCoord.xy), 0);
+
+	
+	if (uFrameCounter == 1.0) // camera just moved after being still
+	{
+		previousPixel.rgb *= (1.0 / uPreviousSampleCount) * 0.5; // essentially previousPixel *= 0.5, like below
+		previousPixel.a = 0.0;
+		currentPixel.rgb *= 0.5;
+	}
+	else if (uCameraIsMoving) // camera is currently moving
+	{
+		previousPixel.rgb *= 0.5; // motion-blur trail amount (old image)
+		previousPixel.a = 0.0;
+		currentPixel.rgb *= 0.5; // brightness of new image (noisy)
+	}
+	else if (uSceneIsDynamic)
+	{
+		previousPixel.rgb *= 0.9; // motion-blur trail amount (old image)
+		currentPixel.rgb *= 0.1; // brightness of new image (noisy)
+	}
+
+	if (colorDifference > 0.0 || normalDifference >= 0.9 || objectDifference >= 1.0)
+		pixelSharpness = 1.0; // 1.0 means an edge pixel
+
+	currentPixel.a = pixelSharpness;
+
+	// Eventually, all edge-containing pixels' .a (alpha channel) values will converge to 1.0, 
+	//   which keeps them from getting blurred by the box-blur filter, thus retaining sharpness over time.
+	if (previousPixel.a == 1.0) // an edge or a light source
+		currentPixel.a = 1.0;
+
+	if (uSceneIsDynamic)
+	{ // for dynamic scenes (to clear out old, dark, sharp pixel trails left behind from moving objects)	
+		if (previousPixel.a == 1.0 && rng() < 0.05)
+			currentPixel.a = 0.0;
+	}
+	
+
+	pc_fragColor = vec4(previousPixel.rgb + currentPixel.rgb, currentPixel.a);
+}
