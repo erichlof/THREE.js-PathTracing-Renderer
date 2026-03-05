f9eda52aec18f2a7e1571e8c6105cc9a8271f164
Erich Loftis <erichlof@swbell.net>
2026-03-04 23:04:24 -0600

    Update Arctic_Circle_Fragment.glsl

--- shaders/Arctic_Circle_Fragment.glsl
+++ shaders/Arctic_Circle_Fragment.glsl
@@ -406,7 +406,7 @@ vec3 CalculateRadiance()
 			vec3 randomSkyVec = randomCosWeightedDirectionInHemisphere(mix(n, up, 0.9));
 			vec3 skyColor = Get_Sky_Color(randomSkyVec);
 			if (dot(randomSkyVec, uSunDirection) > 0.98) skyColor *= 0.01;
-			vec3 sunColor = clamp(Get_Sky_Color(randomDirectionInSpecularLobe(uSunDirection,  0.1)), 0.0, 2.0);
+			vec3 sunColor = clamp(Get_Sky_Color(randomDirectionInSpecularLobe(nl, uSunDirection,  0.1)), 0.0, 2.0);
 			float terrainLayer = clamp( ((x.y + -TERRAIN_LIFT) + (rockNoise * 1000.0) * n.y) / (TERRAIN_HEIGHT * 1.2), 0.0, 1.0 );
 			
 			if (x.y > uWaterLevel && terrainLayer > 0.95 && terrainLayer > 0.9 - n.y)
@@ -442,7 +442,7 @@ vec3 CalculateRadiance()
 			bounceIsSpecular = FALSE;
 				
 			
-			vec3 shadowRayDirection = randomDirectionInSpecularLobe(uSunDirection, 0.4);						
+			vec3 shadowRayDirection = randomDirectionInSpecularLobe(nl, uSunDirection, 0.4);						
 			//if ( isLightSourceVisible(x, n, shadowRayDirection) && x.y > uWaterLevel ) // in direct sunlight
 			if (bounces < 2 && x.y > uWaterLevel && dot(n, shadowRayDirection) > 0.1 && isLightSourceVisible(x, n, shadowRayDirection) ) // in direct sunlight
 			{
